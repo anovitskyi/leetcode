e23e2b5de470677f5f38e6adc19ec24aa47d8ca3
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2019-09-11 16:29:13 +0200

    Find All Duplicates in an Array O(1) space solution

--- src/arrays/Task1.java
+++ src/arrays/Task1.java
@@ -9,9 +9,30 @@ public class Task1 {
 
     public static void main(String[] args) {
         int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
-        System.out.println(findDuplicates(arr));
+        int[] arr2 = {10, 2, 5, 10, 9, 1, 1, 4, 3, 7};
+        System.out.println(findDuplicates2(arr2));
     }
 
+    // O(n) - time
+    // O(1) - space, except list of results
+    private static List<Integer> findDuplicates2(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] < 0) {
+                duplicates.add(Math.abs(nums[i]));
+            } else {
+                nums[index] *= -1;
+            }
+        }
+
+        return duplicates;
+    }
+
+    // O(n) - time
+    // O(n) - space
     private static List<Integer> findDuplicates(int[] nums) {
         boolean[] flags = new boolean[nums.length];
         List<Integer> duplicates = new ArrayList<>();
