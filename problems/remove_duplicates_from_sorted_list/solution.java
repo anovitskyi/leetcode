d0e120ec71f0a2a861220a18d640caf502b57b39
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2019-10-01 05:42:32 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted List (java)

--- problems/remove_duplicates_from_sorted_list/solution.java
+++ problems/remove_duplicates_from_sorted_list/solution.java
@@ -7,7 +7,40 @@
  * }
  */
 class Solution {
+    
     public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        
+        if (current == null) {
+            return head;
+        }
+        
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;   
+            }
+        }
+        return head;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public ListNode deleteDuplicates2(ListNode head) {
         ListNode root = head;
         
         while (head != null && head.next != null) {
