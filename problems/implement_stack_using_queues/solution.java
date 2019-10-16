09f8d05b2ed8e9ff1f88e3ea676ea8035cfa904f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2019-10-16 06:54:58 +0000

    Sync LeetCode submission - Implement Stack using Queues (java)

---
+++ problems/implement_stack_using_queues/solution.java
@@ -0,0 +1,60 @@
+class MyStack {
+
+    private Queue<Integer> queue;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+        queue = new LinkedList<>();
+
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        queue.add(x);
+
+        if (queue.size() <= 1) {
+            return;
+        }
+
+        int counter = queue.size() - 1;
+        while (counter-- > 0) {
+            int tmp = queue.poll();
+            queue.add(tmp);
+        }
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
