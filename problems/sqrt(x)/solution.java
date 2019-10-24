1616b924d9217a54dc626c6f8a8ea569890949f0
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2019-10-24 19:33:46 +0000

    Sync LeetCode submission - Sqrt(x) (java)

--- problems/sqrt(x)/solution.java
+++ problems/sqrt(x)/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 2) {
-            return x;
+        if (x == 0) {
+            return 0;
         } else if (x < 4) {
             return 1;
         }
 
         int left = 0;
-        int right = x / 2;
-        int middle;
+        int right = x;
+        int middle = -1;
         int res = -1;
 
         while (left <= right) {
@@ -17,7 +17,7 @@ class Solution {
 
             if (middle == tmp) {
                 return tmp;
-            } else if (tmp > middle) {
+            } else if (tmp > middle) { 
                 left = middle + 1;
                 res = middle;
             } else {
