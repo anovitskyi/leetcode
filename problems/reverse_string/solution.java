7da4e4afd14aeccfedd3b818b28cb893872a0ffb
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-08-21 21:02:51 +0000

    Sync LeetCode submission - Reverse String (java)

---
+++ problems/reverse_string/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int last = s.length - 1;
+        for (int i = 0; i < s.length / 2; ++i, --last) {
+            char tmp = s[i];
+            s[i] = s[last];
+            s[last] = tmp;
+        }
+    }
+}
