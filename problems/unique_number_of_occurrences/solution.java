57c44124207f67a74b21978507516e0f544938e6
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-10-29 18:50:19 +0000

    Sync LeetCode submission - Unique Number of Occurrences (java)

--- problems/unique_number_of_occurrences/solution.java
+++ problems/unique_number_of_occurrences/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
+        Map<Integer, Integer> map = new HashMap<>(1000);
         for (int num : arr) {
-            int counter = map.getOrDefault(num, 0);
-            map.put(num, counter + 1);
+            int quantity = map.getOrDefault(num, 0);
+            map.put(num, quantity + 1);
         }
         
-        Set<Integer> set = new HashSet<>();
-        for (Integer val : map.values()) {
-            if (!set.add(val)) {
+        Set<Integer> set = new HashSet<>(map.size());
+        for (int quantity : map.values()) {
+            if (!set.add(quantity)) {
                 return false;
             }
         }
-        
         return true;
     }
 }
