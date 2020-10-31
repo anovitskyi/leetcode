1031d89f34188c5eb3cb372e0b8a0eed3769fcd0
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-10-31 16:46:16 +0000

    Sync LeetCode submission - Daily Temperatures (java)

--- problems/daily_temperatures/solution.java
+++ problems/daily_temperatures/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] dailyTemperatures(int[] T) {
         int[] res = new int[T.length];
         
-        for (int i = 0; i < res.length; ++i) {
+        for (int i = res.length - 1; i >= 0; --i) {
             for (int j = i + 1; j < res.length; ++j) {
                 if (T[j] > T[i]) {
                     res[i] = j - i;
