11a16875a9e28ff112c210c67ab0dddd9ed87441
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-11-10 16:30:25 +0000

    Sync LeetCode submission - Flipping an Image (java)

--- problems/flipping_an_image/solution.java
+++ problems/flipping_an_image/solution.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
         for (int i = 0; i < A.length; ++i) {
-            int[] arr = A[i];
-            int size = arr.length;
-            for (int j = 0; j < size / 2; ++j) {
-                arr[j] = 1 - arr[j];
-                arr[size - j - 1] = 1 - arr[size - j - 1];
-                
-                int tmp = arr[j];
-                arr[j] = arr[size - j - 1];
-                arr[size - j - 1] = tmp;
+            int length = A[i].length;
+            for (int j = 0; j < length / 2; ++j) {
+                int tmp = A[i][j];
+                A[i][j] = 1 - A[i][length - 1 - j];
+                A[i][length - 1 - j] = 1 - tmp;
             }
             
-            if (size % 2 == 1) {
-                arr[size / 2] = 1 - arr[size / 2];
+            if (length % 2 == 1) {
+                A[i][length / 2] = 1 - A[i][length / 2];
             }
         }
-        
         return A;
     }
 }
