98faf7654f24f5e939da3773e5aa8c8040ac1560
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-11-15 11:40:34 +0000

    Sync LeetCode submission - Range Sum of BST (java)

--- problems/range_sum_of_bst/solution.java
+++ problems/range_sum_of_bst/solution.java
@@ -4,30 +4,52 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public int rangeSumBST(TreeNode root, int L, int R) {
-        if (root == null) {
-            return 0;
+//     public int rangeSumBST(TreeNode root, int low, int high) {
+//         if (root == null) {
+//             return 0;
+//         }
+        
+//         if (root.val < low) {
+//             return rangeSumBST(root.right, low, high);
+//         } else if (root.val > high) {
+//             return rangeSumBST(root.left, low, high);
+//         }
+        
+        
+//         return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+//     }
+    
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int sum = 0;
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            
+            if (node.val >= low && node.val <= high) {
+                sum += node.val;
+            } 
+            
+            if (node.val >= low && node.left != null) {
+                queue.offer(node.left);
+            } 
+            
+            if (node.val <= high && node.right != null) {
+                queue.offer(node.right);
+            }
         }
         
-        int res = 0;
-        if (root.val >= L && root.val <= R) {
-            res = root.val;
-        }
-        
-        int left = 0;
-        if (root.val > L) {
-            left = rangeSumBST(root.left, L, R);
-        }
-        
-        int right = 0;
-        if (root.val < R) {
-            right = rangeSumBST(root.right, L, R);
-        }
-        
-        return res + left + right;
+        return sum;
     }
 }
