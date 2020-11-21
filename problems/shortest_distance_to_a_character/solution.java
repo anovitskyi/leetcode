3e9ff8a573b33aa64fc3fd34465573f2e5851ee6
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-11-21 08:04:33 +0000

    Sync LeetCode submission - Shortest Distance to a Character (java)

--- problems/shortest_distance_to_a_character/solution.java
+++ problems/shortest_distance_to_a_character/solution.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] shortestToChar(String S, char C) {
-        char[] tab = S.toCharArray();
-        Set<Integer> set = new HashSet<>();
-        
-        for (int i = 0; i < tab.length; ++i) {
-            if (tab[i] == C) {
+        List<Integer> set = new ArrayList<>();
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == C) {
                 set.add(i);
             }
         }
         
-        int[] res = new int[tab.length];
-        for (int i = 0; i < tab.length; ++i) {
-            int min = Integer.MAX_VALUE;
-            for (int index : set) {
-                int diff = Math.abs(i - index);
-                if (diff < min) {
-                    min = diff;
-                }
+        int[] res = new int[S.length()];
+        int right = 0;
+        int left = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == C) {
+                left = right;
+                ++right;
+            } else {
+                int leftDiff = left >= set.size() ? Integer.MAX_VALUE : Math.abs(i - set.get(left));
+                int rightDiff = right >= set.size() ? Integer.MAX_VALUE : Math.abs(i - set.get(right));
+                
+                res[i] = Math.min(leftDiff, rightDiff);
             }
-            res[i] = min;
         }
+        
         return res;
     }
 }
