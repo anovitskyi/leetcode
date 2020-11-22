ca8859c5a04fd342113e0c5c911a884c73e28c84
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-11-22 13:33:27 +0000

    Sync LeetCode submission - Unique Morse Code Words (java)

--- problems/unique_morse_code_words/solution.java
+++ problems/unique_morse_code_words/solution.java
@@ -1,31 +1,19 @@
 class Solution {
-    
-    private static final String[] morse;
-    
-    static {
-        morse = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    }
+    private static final String[] letters = new String[] {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
     
     public int uniqueMorseRepresentations(String[] words) {
         Set<String> set = new HashSet<>(words.length);
         
         for (String word : words) {
-            String trans = transform(word);
-            set.add(trans);
+            StringBuilder b = new StringBuilder();
+            
+            for (char ch : word.toCharArray()) {
+                b.append(letters[ch - 'a']);
+            }
+            
+            set.add(b.toString());
         }
         
         return set.size();
-    }
-    
-    private String transform(String word) {
-        StringBuilder b = new StringBuilder();
-        char[] tab = word.toLowerCase().toCharArray();
-        
-        for (char ch : tab) {
-            int index = ch - 97;
-            b.append(morse[index]);
-        }
-        
-        return b.toString();
     }
 }
