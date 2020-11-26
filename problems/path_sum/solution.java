39db2d3683accf32400dedd95e2fca7195ff6b9c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-11-26 18:49:53 +0000

    Sync LeetCode submission - Path Sum (java)

---
+++ problems/path_sum/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        
+        sum = sum - root.val;
+        return (root.left != null && hasPathSum(root.left, sum)) ||
+            (root.right != null && hasPathSum(root.right, sum));
+    }
+    
+}
