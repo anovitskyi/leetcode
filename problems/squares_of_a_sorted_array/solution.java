82f662e5a44dd738fe9fbcedb842994a241aa2bb
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-12-15 17:27:09 +0000

    Sync LeetCode submission - Squares of a Sorted Array (java)

--- problems/squares_of_a_sorted_array/solution.java
+++ problems/squares_of_a_sorted_array/solution.java
@@ -1,50 +1,37 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        int index = 0;
-        for (int i = 0; i < A.length; ++i) {
-            if (A[i] >= 0) {
-                index = i;
-                break;
+    public int[] sortedSquares(int[] nums) {
+        int lastNegative = -1;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] < 0) {
+                lastNegative = i;
             }
+            
+            nums[i] = nums[i] * nums[i];
         }
-    
         
-        int[] negative = getSquares(A, 0, index);
-        int[] positive = getSquares(A, index, A.length);
+        if (lastNegative == -1) {
+            return nums;
+        }
         
-        int n = negative.length - 1;
-        int p = 0;
-        
-        int[] res = new int[A.length];
-        int i;
-        for (i = 0; i < A.length && n >= 0 && p < positive.length; ++i) {
-            if (negative[n] <= positive[p]) {
-                res[i] = negative[n--];
+        int firstPositive = lastNegative + 1;
+        int[] copy = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            if (lastNegative < 0) {
+                copy[i] = nums[firstPositive++];
+                continue;
+            }
+            
+            if (firstPositive >= nums.length) {
+                copy[i] = nums[lastNegative--];
+                continue;
+            }
+            
+            if (nums[lastNegative] < nums[firstPositive]) {
+                copy[i] = nums[lastNegative--];
             } else {
-                res[i] = positive[p++];
+                copy[i] = nums[firstPositive++];
             }
         }
-        
-        if (n >= 0) {
-            for (; n >= 0; ++i) {
-                res[i] = negative[n--];
-            }
-        } else if (p < positive.length) {
-            for (; p < positive.length; ++i) {
-                res[i] = positive[p++];
-            }
-        }
-        
-        return res;
-    }
-    
-    private static int[] getSquares(int[] arr, int start, int finish) {
-        int[] res = new int[finish - start];
-        
-        for (int i = start; i < finish; ++i) {
-            res[i - start] = arr[i] * arr[i];
-        }
-        
-        return res;
+        return copy;
     }
 }
