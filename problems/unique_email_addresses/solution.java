2a201999b8bcc75a46dce919de9bcc20eefbfd8e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2020-12-31 13:12:24 +0000

    Sync LeetCode submission - Unique Email Addresses (java)

--- problems/unique_email_addresses/solution.java
+++ problems/unique_email_addresses/solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        Set<String> set = new HashSet<>();
-        for (String email : emails) {
-            String tab[] = email.split("@");
-            tab[0] = tab[0].replaceAll("\\+.*|\\.", "");
-            
-            set.add(tab[0] + "@" + tab[1]);
+        Set<String> set = new HashSet<>(emails.length);
         
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            int indexOfPlus = parts[0].indexOf('+');
+            if (indexOfPlus > 0) {
+                parts[0] = parts[0].substring(0, indexOfPlus);
+            }
+            parts[0] = parts[0].replaceAll("\\.", "");
+            set.add(parts[0] + "@" + parts[1]);
         }
         
         return set.size();
