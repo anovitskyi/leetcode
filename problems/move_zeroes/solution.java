d4262770fb9cec608cf2192ddd788beb9def0cc5
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-01-02 21:52:08 +0000

    Sync LeetCode submission - Move Zeroes (java)

--- problems/move_zeroes/solution.java
+++ problems/move_zeroes/solution.java
@@ -1,33 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-//         int counter = 0;
-//         for (int i = 0; i < nums.length; ++i) {
-//             if (nums[i] != 0) {
-//                 nums[counter++] = nums[i];
-//             }
-//         }
-        
-//         for (; counter < nums.length; ++counter) {
-//             nums[counter] = 0;
-//         }
-        
-        int i=0, j=0;
-        while(i < nums.length){
-            if(nums[i] == 0) {
-                j = i;
-
-                while(j < nums.length && nums[j] == 0){
-                    j++;
-                }
-
-                if(j == nums.length) return;
-
-                nums[i] = nums[j];
-                nums[j] = 0;
-                
-                j = i;
-            } else {
-                ++i;
+        int write = 0;
+        for (int read = 0; read < nums.length; ++read) {
+            if (nums[read] != 0) {
+                int tmp = nums[read];
+                nums[read] = 0;
+                nums[write] = tmp;
+                ++write;
             }
         }
     }
