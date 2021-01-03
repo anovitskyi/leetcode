99ab4dba48c3710b368f37888d6b7e84dca1f1bd
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-01-03 17:33:24 +0000

    Sync LeetCode submission - Merge Sorted Array (java)

--- problems/merge_sorted_array/solution.java
+++ problems/merge_sorted_array/solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) {
-            return;
-        }
-
-        --m; --n;
+        --m;
+        --n;
         for (int i = nums1.length - 1; i >= 0; --i) {
-            if (m < 0) {
-                nums1[i] = nums2[n--];
-            } else if (n < 0) {
-                nums1[i] = nums1[m--];
+            int elem = -1;
+            if (n < 0) {
+                elem = nums1[m--];
+            } else if (m < 0) {
+                elem = nums2[n--];  
             } else if (nums1[m] >= nums2[n]) {
-                nums1[i] = nums1[m];
-                --m;
+                elem = nums1[m--];
             } else {
-                nums1[i] = nums2[n];
-                --n;
+                elem = nums2[n--];
             }
+            
+            nums1[i] = elem;
         }
     }
 }
+// -10 -9 -8 -5 0 0 0 0 | 4
+// 100 101 102 104 | 4
