ba3b994352033d17c28a44523cc977cfe8495387
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-01-05 11:39:02 +0000

    Sync LeetCode submission - Split a String in Balanced Strings (java)

--- problems/split_a_string_in_balanced_strings/solution.java
+++ problems/split_a_string_in_balanced_strings/solution.java
@@ -1,21 +1,13 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int res = 0;
-        
         int counter = 0;
-        char[] tab = s.toCharArray();
-        for (int i = 0; i < tab.length; ++i) {
-            if (tab[i] == 'L') {
-                ++counter;
-            } else {
-                --counter;
-            }
-            
+        int result = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            counter += s.charAt(i) == 'L' ? -1 : 1;
             if (counter == 0) {
-                ++res;
+                ++result;
             }
         }
-        
-        return res;
+        return result;
     }
 }
