418499c2d9c8755b5cd02d2969b8fca424d913a2
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-01-17 11:34:46 +0000

    Sync LeetCode submission - Add Two Numbers II (java)

--- problems/add_two_numbers_ii/solution.java
+++ problems/add_two_numbers_ii/solution.java
@@ -1,55 +1,43 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null || l1.val == 0) {
-            return l2;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { 
+        Stack<Integer> first = convertToStack(l1);
+        Stack<Integer> second = convertToStack(l2);
+        
+        ListNode prev = null;
+        int carrying = 0;
+        while (!first.isEmpty() || !second.isEmpty()) {
+            int sum = carrying;
+            if (!first.isEmpty()) {
+                sum += first.pop();
+            }
+            
+            if (!second.isEmpty()) {
+                sum += second.pop();
+            }
+            
+            carrying = sum / 10;
+            ListNode tmp = new ListNode(sum % 10);
+            tmp.next = prev;
+            prev = tmp;
         }
         
-        if (l2 == null || l2.val == 0) {
-            return l1;
+        if (carrying != 0) {
+            ListNode tmp = new ListNode(carrying);
+            tmp.next = prev;
+            prev = tmp;
         }
         
-        Stack<Integer> first = new Stack<>();
-        Stack<Integer> second = new Stack<>();
-        
-        while (l1 != null) {
-            first.push(l1.val);
-            l1 = l1.next;
-        }
-        
-        while (l2 != null) {
-            second.push(l2.val);
-            l2 = l2.next;
-        }
-        
-        ListNode root = null;
-        int prev = 0;
-        
-        while (!first.empty() || !second.empty()) {
-            int fValue = first.empty() ? 0 : first.pop();
-            int sValue = second.empty() ? 0 : second.pop();
-            int val = fValue + sValue + prev;
-            prev = val / 10;
-            ListNode node = new ListNode(val % 10);
-            node.next = root;
-            root = node;
-        }
-        
-        if (prev != 0) {
-            ListNode node = new ListNode(prev);
-            node.next = root;
-            root = node;
-        }
-        
-        return root;
+        return prev;
     }
+    
+    private Stack<Integer> convertToStack(ListNode node) {
+        Stack<Integer> stack = new Stack<>();
+        while (node != null) {
+            stack.push(node.val);
+            node = node.next;
+        }
+        return stack;
+    }
+    
+    
 }
