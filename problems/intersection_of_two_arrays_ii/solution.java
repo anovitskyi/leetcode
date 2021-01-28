e864a73efdeb869133d3cc9dae0360c108be384d
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-01-28 23:12:22 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (java)

--- problems/intersection_of_two_arrays_ii/solution.java
+++ problems/intersection_of_two_arrays_ii/solution.java
@@ -1,26 +1,29 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>(nums1.length);
-        for (int num : nums1) {
-            int q = map.getOrDefault(num, 0);
-            map.put(num, q + 1);
-        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         
-        List<Integer> list = new LinkedList<>();
-        for (int num : nums2) {
-            int q = map.getOrDefault(num, 0);
-            if (q > 0) {
-                list.add(num);
-                map.put(num, q - 1);
+        List<Integer> result = new ArrayList<>();
+        int n1 = 0;
+        int n2 = 0;
+        while (true) {
+            if (n1 >= nums1.length || n2 >= nums2.length) {
+                int[] res = new int[result.size()];
+                for (int i = 0; i < result.size(); ++i) {
+                    res[i] = result.get(i);
+                }
+                return res;
+            }
+            
+            if (nums1[n1] == nums2[n2]) {
+                result.add(nums1[n1]);
+                ++n1;
+                ++n2;
+            } else if (nums1[n1] > nums2[n2]) {
+                ++n2;
+            } else if (nums2[n2] > nums1[n1]) {
+                ++n1;
             }
         }
-        
-        int[] res = new int[list.size()];
-        int counter = 0;
-        for (int num : list) {
-            res[counter++] = num;
-        }
-        return res;
-        
     }
 }
