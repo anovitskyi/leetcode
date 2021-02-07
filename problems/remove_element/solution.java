695f3f47159be718d6a5be43358dd8ed1471b519
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-02-07 15:48:37 +0000

    Sync LeetCode submission - Remove Element (java)

--- problems/remove_element/solution.java
+++ problems/remove_element/solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int write = 0;
-        for (int read = 0; read < nums.length; ++read) {
-            if (nums[read] != val) { // false
-                nums[write] = nums[read];
-                ++write;
+        int write = -1;
+        int read = 0;
+        
+        while (read < nums.length) {
+            if (nums[read] != val) {
+                nums[++write] = nums[read];
             }
+            ++read;
         }
-        return write;
+        
+        return write + 1; 
     }
 }
