73187e4f39827d897cccfcdb844441f39931d420
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-02-08 19:54:11 +0000

    Sync LeetCode submission - Implement strStr() (java)

--- problems/implement_strstr()/solution.java
+++ problems/implement_strstr()/solution.java
@@ -1,25 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if ("".equals(needle)) {
-            return 0;
-        }
-
-        char[] chars = haystack.toCharArray();
-        char[] needles = needle.toCharArray();
-
-        for (int i = 0; i < chars.length; ++i) {
-            point:
-            if (chars[i] == needles[0] && i + needles.length <= chars.length) {
-                for (int m = 1, n = i + 1; m < needles.length; ++m, ++n) {
-                    if (chars[n] != needles[m]) {
-                        break point;
-                    }
-                }
-                return i;
-            }
-        }
-
-        return -1;
-
+        return haystack.indexOf(needle);
     }
 }
