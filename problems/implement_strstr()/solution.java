1c54c7b2708669fd1862bbf933db50b538e5e73a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-02-18 22:40:13 +0000

    Sync LeetCode submission - Implement strStr() (java)

--- problems/implement_strstr()/solution.java
+++ problems/implement_strstr()/solution.java
@@ -1,28 +1,40 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
-            return 0;
-        }
-        
-        if (haystack.length() < needle.length()) {
+        if (needle.length() > haystack.length()) {
             return -1;
         }
         
-        for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
-            boolean found = true;
-            for (int j = 0; j < needle.length(); ++j) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    found = false;
-                    break;
-                }
+        int start = 0;
+        int end = needle.length();
+        int patternHash = hash(needle, end);
+        int actualHash = hash(haystack, end);
+        
+        while (true) {
+            if (patternHash == actualHash && haystack.substring(start, end).equals(needle)) {
+                return start;
             }
             
-            if (found) {
-                return i;
+            if (end >= haystack.length()) {
+                return -1;
             }
+            
+            actualHash -= (Math.pow(26, 0) * (haystack.charAt(start) - 'a' + 1));
+            actualHash /= 26;
+            actualHash += (Math.pow(26, needle.length() - 1) * (haystack.charAt(end) - 'a' + 1));
+            
+            ++start;
+            ++end;
         }
-        
-        return -1;
     }
     
+    private int hash(String text, int end) {
+        int result = 0;
+        
+        for (int i = 0; i < end; ++i) {
+            int letter = text.charAt(i) - 'a' + 1;
+            result += (Math.pow(26, i) * letter);
+        }
+        
+        return result;
+    }
 }
