3ed3b34bd60331db317e429bea7945a348f6fca9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-03-23 07:48:57 +0000

    Sync LeetCode submission - Build an Array With Stack Operations (java)

--- problems/build_an_array_with_stack_operations/solution.java
+++ problems/build_an_array_with_stack_operations/solution.java
@@ -1,18 +1,17 @@
 class Solution {
-    
     public List<String> buildArray(int[] target, int n) {
         List<String> result = new LinkedList<>();
+        int targetIndex = 0;
         
-        int pointer = 0;
-        for (int i = 1; i <= n && pointer < target.length; ++i) {
+        for (int i = 1; i <= target[target.length - 1]; ++i) {
             result.add("Push");
-            
-            if (target[pointer] == i) {
-                ++pointer;
-            } else {
+            if (i != target[targetIndex]) {
                 result.add("Pop");
+            } else {
+                ++targetIndex;
             }
         }
+        
         
         return result;
     }
