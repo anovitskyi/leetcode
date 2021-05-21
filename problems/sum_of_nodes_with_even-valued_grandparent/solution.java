6e9fc999d4abde160ac92274764f03f39a3db393
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-05-21 07:37:48 +0000

    Sync LeetCode submission - Sum of Nodes with Even-Valued Grandparent (java)

--- problems/sum_of_nodes_with_even-valued_grandparent/solution.java
+++ problems/sum_of_nodes_with_even-valued_grandparent/solution.java
@@ -1,63 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
-        int val = 0;
-        if (root == null) {
-            return val;
-        }
-        
-        if (root.val % 2 == 0) {
-            val += getValues(root);
-        }
-        
-        if (root.left != null) {
-            val += sumEvenGrandparent(root.left);
-        }
-        
-        if (root.right != null) {
-            val += sumEvenGrandparent(root.right);
-        }
-        
-        return val;
+        return sum(root, 1, 1);
     }
     
-    private int getValues(TreeNode node) {
-        int sum = 0;
-        
-        if (node.left != null) {
-            TreeNode left = node.left;
-            if (left.left != null) {
-                sum += left.left.val;
-            }
-            
-            if (left.right != null) {
-                sum += left.right.val;
-            }
+    private int sum(TreeNode node, int parent, int grand) {
+        if (node == null) {
+            return 0;
         }
         
-        if (node.right != null) {
-            TreeNode right = node.right;
-            if (right.left != null) {
-                sum += right.left.val;
-            }
-            
-            if (right.right != null) {
-                sum += right.right.val;
-            }
+        int sum = sum(node.left, node.val, parent) + sum(node.right, node.val, parent);
+        
+        if (grand % 2 == 0) {
+            sum += node.val;
         }
         
         return sum;
