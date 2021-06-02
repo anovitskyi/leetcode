b7918925b68a2b42802de74da459ec3df89ee83e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-06-02 12:25:39 +0000

    Sync LeetCode submission - Encode and Decode TinyURL (java)

--- problems/encode_and_decode_tinyurl/solution.java
+++ problems/encode_and_decode_tinyurl/solution.java
@@ -1,20 +1,15 @@
-import java.util.UUID;
-
 public class Codec {
     
     private final Map<String, String> map = new HashMap<>();
-
-    // Encodes a URL to a shortened URL.
+    
     public String encode(String longUrl) {
-        return longUrl;
+        int hash = longUrl.hashCode();
+        String strHash = Integer.toString(hash);
+        map.put(strHash, longUrl);
+        return strHash;
     }
 
-    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        return map.get(shortUrl);
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(url));
