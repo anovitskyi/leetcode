b158ecff5d269024b9fbc131c22691429ab5930b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-06-04 13:43:38 +0000

    Sync LeetCode submission - Deepest Leaves Sum (java)

--- problems/deepest_leaves_sum/solution.java
+++ problems/deepest_leaves_sum/solution.java
@@ -1,27 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
+        int result = 0;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int sum = 0;
+        queue.add(root);
         
         while (!queue.isEmpty()) {
             int size = queue.size();
-            sum = 0;
-            
+            result = 0;
             while (size-- > 0) {
                 TreeNode node = queue.poll();
-                sum += node.val;
+                result += node.val;
                 
                 if (node.left != null) {
-                    queue.offer(node.left);
+                    queue.add(node.left);
                 }
                 
                 if (node.right != null) {
-                    queue.offer(node.right);
+                    queue.add(node.right);
                 }
             }
         }
         
-        return sum;
+        return result;
     }
 }
