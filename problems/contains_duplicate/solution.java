5ab05449143ad0d625ab9f6408e9cad3b7d51475
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-06-05 18:01:11 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- problems/contains_duplicate/solution.java
+++ problems/contains_duplicate/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>(nums.length);
-        
-        for (int i = 0; i < nums.length; ++i) {
-            if (map.put(nums[i], nums[i]) != null) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
-            } 
+            }
         }
         
         return false;
