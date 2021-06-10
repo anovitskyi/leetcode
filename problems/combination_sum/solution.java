cebf65c5c6cb8aa620b6477ad97c32692c68faee
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-06-10 15:36:11 +0000

    Sync LeetCode submission - Combination Sum (java)

--- problems/combination_sum/solution.java
+++ problems/combination_sum/solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new LinkedList<>();
-        backtrack(candidates, target, new LinkedList<>(), result, 0);
+        backtrack(candidates, result, 0, target, new LinkedList<>());
         return result;
     }
     
-    private void backtrack(int[] candidates, int target, List<Integer> current, List<List<Integer>> result, int start) {
+    private void backtrack(int[] candidates, List<List<Integer>> result, int currIndex, int target, List<Integer> currList) {
         if (target == 0) {
-            result.add(current);
+            result.add(currList);
             return;
         }
-        
-        for (int i = start; i < candidates.length; ++i) {
-            if (target - candidates[i] < 0) {
-                continue;
-            }
             
-            List<Integer> copy = new LinkedList<>(current);
-            copy.add(candidates[i]);
-            backtrack(candidates, target - candidates[i], copy, result, i);
+        for (int i = currIndex; i < candidates.length; ++i) {
+            if (target - candidates[i] >= 0) {
+                List<Integer> copy = new LinkedList<>(currList);
+                copy.add(candidates[i]);
+                backtrack(candidates, result, i, target - candidates[i], copy);
+            }
         }
     }
 }
