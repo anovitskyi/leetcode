8531a4cbe67819b25d61e5e2aac083a7cb846aec
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-06-24 20:59:28 +0000

    Sync LeetCode submission - Reverse Linked List (java)

--- problems/reverse_linked_list/solution.java
+++ problems/reverse_linked_list/solution.java
@@ -1,59 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-//     public ListNode reverseList(ListNode head) {
-//         ListNode prev = null;
-//         while (head != null) {
-//             ListNode tmp = head.next;
-//             head.next = prev;
-//             prev = head;
-//             head = tmp;
-//         }
-        
-//         return prev;
-//     }
-    
-//     public ListNode reverseList(ListNode head) {
-//         if (head == null || head.next == null) {
-//             return head;
-//         }
-        
-//         ListNode h = reverseList(head.next);
-//         ListNode tmp = head.next;
-//         head.next = tmp.next;
-//         tmp.next = head;
-//         return h;
-//     }
-    
-    public ListNode reverseList(ListNode root) {  // 1
-        if(root == null) {
-            return null;
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
-
-        return dfs(null, root); // 1
+        
+        ListNode result = reverseList(head.next);
+        ListNode next = head.next;
+        next.next = head;
+        head.next = null;
+        return result;
     }
-
-        private ListNode dfs(ListNode parent, ListNode n) { // null 1;  
-            ListNode res = null;
-                    if(n.next != null) {
-                        res = dfs(n, n.next); // 5 4 3 2 
-                } else { // 
-                    n.next = parent;
-                    return n;
-                }
-
-                n.next = parent;
-                return res;
-
-
-        }
-
 }
