13159828a1be3b589def202748942e30c84746b9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-07-27 18:40:30 +0000

    Sync LeetCode submission - 3Sum Closest (java)

--- problems/3sum_closest/solution.java
+++ problems/3sum_closest/solution.java
@@ -4,27 +4,38 @@ class Solution {
         int result = Integer.MAX_VALUE - Math.abs(target);
         
         for (int i = 0; i < nums.length - 2; ++i) {
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (Math.abs(sum - target) < Math.abs(result - target)) {
-                    result = sum;
-                }
-                
-                if (sum == target) {
-                    return result;
-                } else if (sum < target) {
-                    ++left;
-                } else {
-                    --right;
-                }
+            int sum = findSum(nums, target - nums[i], i + 1, nums.length - 1) + nums[i];
+            if (Math.abs(target - result) > Math.abs(target - sum)) {
+                result = sum;
             }
         }
         
         return result;
     }
     
+    private int findSum(int[] nums, int target, int left, int right) {
+        int result = Integer.MAX_VALUE - Math.abs(target);
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            
+            if (sum == target) {
+                return sum;
+            } else if (sum < target) {
+                ++left;
+            } else {
+                --right;
+            }
+            
+            if (Math.abs(target - result) > Math.abs(target - sum)) {
+                result = sum;
+            }
+        }
+        
+        return result;
+    }
 }
+
+
+// -4 -1 1 2     1
+// -1  
