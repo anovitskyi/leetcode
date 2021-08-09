72188d9ca95bd4c551e2f9857f4dbd1ec8512c44
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-08-09 15:01:47 +0000

    Sync LeetCode submission - Find All Duplicates in an Array (java)

--- problems/find_all_duplicates_in_an_array/solution.java
+++ problems/find_all_duplicates_in_an_array/solution.java
@@ -1,17 +1,23 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        boolean[] tab = new boolean[nums.length];
+        List<Integer> result = new LinkedList<>();
         
-        List<Integer> dups = new ArrayList<>();
         for (int i = 0; i < nums.length; ++i) {
-            int index = nums[i] - 1;
-            if (tab[index]) {
-                dups.add(nums[i]);
+            int num = Math.abs(nums[i]);
+            int index = num - 1;
+            
+            if (nums[index] < 0) {
+                result.add(num);
             } else {
-                tab[index] = true;
+                nums[index] = 0 - nums[index];
             }
         }
         
-        return dups;
+        return result;
     }
 }
+
+
+// 1. Bruteforce - O(n^2). Find duplicates for each element in nested loop
+// 2. Sort array and the find duplicates in single iteration - O(logn)
+// 3. Mark indicies as visited by making them negative. When negative element will be found, that means it was previously visited and is a duplicate - O(n)
