34556ea4008f1705609ec49039f264d85e3a280f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-08-09 15:56:10 +0000

    Sync LeetCode submission - Find All Numbers Disappeared in an Array (java)

--- problems/find_all_numbers_disappeared_in_an_array/solution.java
+++ problems/find_all_numbers_disappeared_in_an_array/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for (int i = 0; i < nums.length; ++i) {
-            int indexToUpdate = nums[i] - 1;
-            
-            while (nums[indexToUpdate] - 1 != indexToUpdate) {
-                int copy = nums[indexToUpdate] - 1;
-                nums[indexToUpdate] = indexToUpdate + 1;
-                indexToUpdate = copy;
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = 0 - nums[index];
             }
         }
         
         List<Integer> result = new LinkedList<>();
         for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != i + 1) {
+            if (nums[i] > 0) {
                 result.add(i + 1);
             }
         }
         return result;
     }
 }
+
+
+// 1. Bruteforce - O(n^2). Manually check each number in range [1, n] whether it appears in array nums
+// 2. Sort array nums - O(nlogn). Then manually check each number in range [1, n] using binary search.
+// 3. Store elements in boolean array. Then return only "false" (not populated) indicies.
+// 4. Mark each index as mark, by making it negative. Then add to result only positive indexes - O(n)
