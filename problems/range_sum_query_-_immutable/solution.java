5c7de81cfc4b1bc78f053167b76499a520b70438
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-08-11 13:31:34 +0000

    Sync LeetCode submission - Range Sum Query - Immutable (java)

--- problems/range_sum_query_-_immutable/solution.java
+++ problems/range_sum_query_-_immutable/solution.java
@@ -1,20 +1,18 @@
 class NumArray {
     
-    private int[] arr;
+    private final int[] prefixSum;
 
     public NumArray(int[] nums) {
+        prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
         for (int i = 1; i < nums.length; ++i) {
-            nums[i] = nums[i] + nums[i - 1];
+            prefixSum[i] = nums[i] + prefixSum[i - 1];
         }
-        
-        arr = nums;
     }
     
-    public int sumRange(int i, int j) {
-        if (i == 0) {
-            return arr[j];
-        }
-        
-        return arr[j] - arr[i - 1];
+    public int sumRange(int left, int right) {
+        int leftSum = left - 1 < 0 ? 0 : prefixSum[left - 1];
+        int rightSum = prefixSum[right];
+        return rightSum - leftSum;
     }
 }
