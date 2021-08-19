9d7e8204a3cbd662d5c73fa8387cd5b4c969fe2a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-08-19 20:53:52 +0000

    Sync LeetCode submission - Product of Array Except Self (java)

--- problems/product_of_array_except_self/solution.java
+++ problems/product_of_array_except_self/solution.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int prod = 1;
-        int zeros = 0;
-        int zeroIndex = -1;
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        
+        left[0] = nums[0];
+        right[nums.length - 1] = nums[nums.length - 1];
+        for (int i = 1; i < nums.length; ++i) {
+            left[i] = left[i - 1] * nums[i];
+            right[nums.length - 1 - i] = right[nums.length - i] * nums[nums.length - 1 - i];
+        }
         
         for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] == 0) {
-                ++zeros;
-                zeroIndex = i;
-            } else {
-                prod *= nums[i];
-            }
+            int l = i - 1 < 0 ? 1 : left[i - 1];
+            int r = i + 1 >= nums.length ? 1 : right[i + 1];
+            
+            nums[i] = l * r;
         }
         
-        int[] output = new int[nums.length];
-        if (zeros == 1) {
-            output[zeroIndex] = prod;    
-        } else if (zeros == 0) {
-            for (int i = 0; i < output.length; ++i) {
-                output[i] = prod / nums[i];
-            }
-        }
-        
-        return output;
+        return nums;
     }
 }
