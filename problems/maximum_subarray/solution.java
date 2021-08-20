50f1c5abfb9be5ac35bf0e1ea35776ac7e10d3dd
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-08-20 13:24:43 +0000

    Sync LeetCode submission - Maximum Subarray (java)

--- problems/maximum_subarray/solution.java
+++ problems/maximum_subarray/solution.java
@@ -7,7 +7,9 @@ class Solution {
             if (sum < 0) {
                 sum = 0;
             }
+            
             sum += nums[i];
+            
             if (sum > max) {
                 max = sum;
             }
@@ -16,3 +18,11 @@ class Solution {
         return max;
     }
 }
+
+// [-3,4,-1,5,-10,6,-10] -> 8
+
+// 1. Bruteforce - O(n^2) time, O(1) space
+// Two nested loops. First loop keeps index of start element. Second loop calculates elements sum starting and checks whether the sum if maximum.
+
+
+// 2. Iterate array only once. Keep current sum and maximum sum. Befor adding element to sum, check whether actual sum is negative and set current sum to zero. Every time check whether current sum is maximum. Time - O(n), space - O(1)
