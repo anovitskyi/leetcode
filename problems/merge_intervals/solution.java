692247c0a9f69492f3ed34a9ae1a447364e63078
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-09-01 17:34:37 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,39 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 1) {
-            return intervals;
-        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new LinkedList<>();
+        result.add(intervals[0]);
+        int[] last = intervals[0];
         
-        sortArray(intervals);
-        
-        Stack<int[]> stack = new Stack<>();
-        stack.push(intervals[0]);
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][0] <= stack.peek()[1]) {
-                if (intervals[i][1] > stack.peek()[1]) {
-                    stack.peek()[1] = intervals[i][1];
+            if (intervals[i][0] <= last[1]) {
+                if (intervals[i][1] > last[1]) {
+                    last[1] = intervals[i][1];
                 }
             } else {
-                stack.push(intervals[i]);
-            }
+                result.add(intervals[i]);
+                last = intervals[i];
+            }    
         }
         
-        int[][] result = new int[stack.size()][2];
-        for (int i = stack.size() - 1; i >= 0; --i) {
-            result[i] = stack.pop();
-        }
-        return result;
-    }
-    
-    private void sortArray(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> {
-            int first = Integer.compare(a[0], b[0]);
-            
-            if (first == 0) {
-                return Integer.compare(a[1], b[1]);
-            }
-            
-            return first;
-        });
+        return result.toArray(new int[result.size()][2]);
     }
 }
