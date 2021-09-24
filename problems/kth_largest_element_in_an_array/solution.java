a317520b35bd61a25cc44dae01a0f5791f6c0984
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-09-24 16:31:53 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (java)

--- problems/kth_largest_element_in_an_array/solution.java
+++ problems/kth_largest_element_in_an_array/solution.java
@@ -1,13 +1,17 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        quickSelect(nums, k - 1, 0, nums.length - 1);
-        return nums[k - 1];
+        return quickSelect(nums, k - 1, 0, nums.length - 1);
     }
     
-    private void quickSelect(int[] nums, int k, int left, int right) {
+    private int quickSelect(int[] nums, int k, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        
+        int pivot = nums[right];
         int last = left;
         for (int i = left; i < right; ++i) {
-            if (nums[i] > nums[right]) {
+            if (nums[i] > pivot) {
                 int tmp = nums[i];
                 nums[i] = nums[last];
                 nums[last] = tmp;
@@ -15,18 +19,16 @@ class Solution {
             }
         }
         
-        if (nums[last] < nums[right]) {
-            int tmp = nums[last];
-            nums[last] = nums[right];
-            nums[right] = tmp;
-        }
+        int tmp = nums[last];
+        nums[last] = nums[right];
+        nums[right] = tmp;
         
         if (last == k) {
-            return;
-        } else if (last > k && left < last - 1) {
-            quickSelect(nums, k, left, last - 1);
-        } else if (last + 1 < right) {
-            quickSelect(nums, k, last + 1, right);
+            return nums[k];
+        } else if (last > k) {
+            return quickSelect(nums, k, left, last - 1);
+        } else {
+            return quickSelect(nums, k, left + 1, right);
         }
     }
 }
