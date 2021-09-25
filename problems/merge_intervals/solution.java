a9d8cd5df4b6297644f6a2cb71a83b1e0a2f94b1
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-09-25 10:14:36 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,21 +1,26 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        List<int[]> result = new LinkedList<>();
-        result.add(intervals[0]);
-        int[] last = intervals[0];
+        Arrays.sort(intervals, (a, b) -> {
+            int first = Integer.compare(a[0], b[0]);
+            
+            if (first == 0) {
+                return Integer.compare(a[1], b[1]);
+            }
+            
+            return first;
+        });
         
+        List<int[]> result = new LinkedList<>();
+        int[] last = intervals[0];
+        result.add(intervals[0]);
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][0] <= last[1]) {
-                if (intervals[i][1] > last[1]) {
-                    last[1] = intervals[i][1];
-                }
-            } else {
+            if (intervals[i][0] > last[1]) {
                 result.add(intervals[i]);
                 last = intervals[i];
-            }    
+            } else if (intervals[i][1] > last[1]) {
+                last[1] = intervals[i][1];
+            }
         }
-        
         return result.toArray(new int[result.size()][2]);
     }
 }
