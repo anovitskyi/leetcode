b30b6f22591643726d26ab9b09a9b10b8fefa3d3
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-09-28 07:46:37 +0000

    Sync LeetCode submission - Intersection of Two Arrays (java)

--- problems/intersection_of_two_arrays/solution.java
+++ problems/intersection_of_two_arrays/solution.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for (int num : nums1) {
-            map.put(num, 1);
+            set.add(num);
         }
         
-        int size = 0;
+        Set<Integer> result = new HashSet<>();
         for (int num : nums2) {
-            int q = map.getOrDefault(num, 0);
-            if (q > 0) {
-                map.put(num, q + 1);
-                if (q == 1) {
-                    ++size;   
-                }
+            if (set.contains(num)) {
+                result.add(num);
             }
-            
         }
         
-        int[] result = new int[size];
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                result[--size] = entry.getKey();
-            }
+        int[] arr = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            arr[i++] = num;
         }
-        return result;
+        return arr;
     }
 }
