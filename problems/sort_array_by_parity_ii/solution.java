742d540edce2e416f47fbe279738b8913117d3e2
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-09-30 09:32:05 +0000

    Sync LeetCode submission - Sort Array By Parity II (java)

--- problems/sort_array_by_parity_ii/solution.java
+++ problems/sort_array_by_parity_ii/solution.java
@@ -1,19 +1,29 @@
 class Solution {
-    public int[] sortArrayByParityII(int[] A) {
-        int odd = 0;
-        int even = 1;
-        int[] res = new int[A.length];
+    public int[] sortArrayByParityII(int[] nums) {
+        int even = 0;
+        int odd = 1;
         
-        for (int i = 0; i < A.length; ++i) {
-            if (A[i] % 2 == 0) {
-                res[odd] = A[i];
-                odd += 2;
-            } else {
-                res[even] = A[i];
+        while (even < nums.length && odd < nums.length) {
+            if (nums[even] % 2 != 0 && nums[odd] % 2 != 1) {
+                int tmp = nums[even];
+                nums[even] = nums[odd];
+                nums[odd] = tmp;
+            }
+            
+            if (nums[even] % 2 == 0) {
                 even += 2;
+            }
+            
+            if (nums[odd] % 2 == 1) {
+                odd += 2;
             }
         }
         
-        return res;
+        return nums;
     }
 }
+
+// [2,3,0,1,4,1,0,3,4,3]
+//  0 1 2 3 4 5 6 7 8 9
+// even = 10
+// odd = 9
