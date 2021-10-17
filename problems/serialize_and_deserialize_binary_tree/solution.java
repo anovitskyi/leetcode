c0de915fbd66b974b12ce4d503ef932891a16d48
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-10-17 10:01:04 +0000

    Sync LeetCode submission - Serialize and Deserialize Binary Tree (java)

--- problems/serialize_and_deserialize_binary_tree/solution.java
+++ problems/serialize_and_deserialize_binary_tree/solution.java
@@ -1,22 +1,48 @@
-import java.util.UUID;
-
 public class Codec {
-    
-    private static final Map<String, TreeNode> cache = new HashMap<>();
 
+    private static final String sep = ",";
+    private static final String nullVal = "null";
+    
+    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        String id = UUID.randomUUID().toString();
-        cache.put(id, root);
-        return id;
-    }
-
-    public TreeNode deserialize(String id) {
-        return cache.get(id);
+        StringBuilder builder = new StringBuilder();
+        dfs(root, builder);
+        return builder.toString();
     }
     
-}
+    private void dfs(TreeNode node, StringBuilder builder) {
+        if (node == null) {
+            builder.append(nullVal);
+            return;
+        }
+        
+        builder.append(node.val);
+        
+        builder.append(sep);
+        dfs(node.left, builder);
+        
+        builder.append(sep);
+        dfs(node.right, builder);
+    }
 
-// Your Codec object will be instantiated and called as such:
-// Codec ser = new Codec();
-// Codec deser = new Codec();
-// TreeNode ans = deser.deserialize(ser.serialize(root));
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] tab = data.split(sep);
+        return rdfs(tab, new int[] {0});
+    }
+    
+    private TreeNode rdfs(String[] tab, int[] index) {
+        if (tab[index[0]].equals(nullVal)) {
+            return null;
+        }
+        
+        TreeNode node = new TreeNode(Integer.parseInt(tab[index[0]]));
+        ++index[0];
+        node.left = rdfs(tab, index);
+        ++index[0];
+        node.right = rdfs(tab, index);
+        return node;
+    }
+    
+    // 1,2,null,null,3,4,null,null,5,null,null
+}
