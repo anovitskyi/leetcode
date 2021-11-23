34067b9172812a3492aab4335b711bd440cfa9db
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-11-23 20:56:02 +0000

    Sync LeetCode submission - Sort Colors (java)

--- problems/sort_colors/solution.java
+++ problems/sort_colors/solution.java
@@ -1,5 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int white = 0;
+        int blue = 0;
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 0) {
+                nums[index++] = 0;
+            } else if (nums[i] == 1) {
+                ++white;
+            } else if (nums[i] == 2) {
+                ++blue;
+            }
+        }
+        
+        while (white-- > 0) {
+            nums[index++] = 1;
+        }
+        
+        while (blue-- > 0) {
+            nums[index++] = 2;
+        }
     }
 }
