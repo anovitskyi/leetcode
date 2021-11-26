21fc7af9cfde8243c802b18b3fad0508c91b4d7f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-11-26 17:59:28 +0000

    Sync LeetCode submission - Generate Parentheses (java)

--- problems/generate_parentheses/solution.java
+++ problems/generate_parentheses/solution.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new LinkedList<>();
-        backtrack(result, n, n, "");
+        String curr = "";
+        
+        backtrack(result, curr, n, n);
+        
         return result;
     }
     
-    private void backtrack(List<String> result, int left, int right, String current) {
-        if (left == 0 && right == 0) {
-            result.add(current);
+    private void backtrack(List<String> result, String curr, int open, int close) {
+        if (open == 0 && close == 0) {
+            result.add(curr);
             return;
         }
         
-        if (left < 0 || right < 0 || left > right) {
-            return;
+        if (open > 0) {
+          backtrack(result, curr + "(", open - 1, close);    
         }
         
-        backtrack(result, left - 1, right, current + "(");
-        backtrack(result, left, right - 1, current + ")");
+        if (close > open) {
+          backtrack(result, curr + ")", open, close - 1);  
+        }
     }
 }
