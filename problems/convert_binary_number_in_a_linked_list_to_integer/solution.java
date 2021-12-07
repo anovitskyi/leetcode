4939bb70094cd832c29ab734da430891279e755c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-12-07 16:47:16 +0000

    Sync LeetCode submission - Convert Binary Number in a Linked List to Integer (java)

--- problems/convert_binary_number_in_a_linked_list_to_integer/solution.java
+++ problems/convert_binary_number_in_a_linked_list_to_integer/solution.java
@@ -1,27 +1,12 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        ListNode sizeNode = head;
-        int size = 0;
-        while (sizeNode != null) {
-            ++size;
-            sizeNode = sizeNode.next;
-        }
+        int result = 0;
         
-        int res = 0;
         while (head != null) {
-            res += ((int) Math.pow(2, --size)) * head.val;
+            result = (result << 1) | head.val;
             head = head.next;
         }
-        return res;
+        
+        return result;
     }
 }
