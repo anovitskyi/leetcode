39ce10e2d61560ceca2bec2b0f4671b507df67bc
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-12-13 16:51:06 +0000

    Sync LeetCode submission - Consecutive Characters (java)

--- problems/consecutive_characters/solution.java
+++ problems/consecutive_characters/solution.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int maxPower(String s) { // "abbcccddddeeeeedcba", length = 19
-        int max = Integer.MIN_VALUE; // -1
-        
-        for (int i = 0; i < s.length(); ) { // 0 < 19;
-            int length = i + 1; // 1
-            while (length < s.length() && s.charAt(i) == s.charAt(length)) {
-                ++length;
+    public int maxPower(String s) {
+        int result = 1;
+        int curr = 1;
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                ++curr;
+            } else {
+                result = Math.max(result, curr);
+                curr = 1;
             }
-            
-            max = Math.max(max, length - i);
-            i = length;
         }
-        
-        return max;
+        return Math.max(result, curr);
     }
 }
