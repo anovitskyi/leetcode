187134459590425f2dc63511cd0dab659dc97088
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-12-15 11:24:25 +0000

    Sync LeetCode submission - Range Sum of BST (java)

--- problems/range_sum_of_bst/solution.java
+++ problems/range_sum_of_bst/solution.java
@@ -1,55 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-//     public int rangeSumBST(TreeNode root, int low, int high) {
-//         if (root == null) {
-//             return 0;
-//         }
-        
-//         if (root.val < low) {
-//             return rangeSumBST(root.right, low, high);
-//         } else if (root.val > high) {
-//             return rangeSumBST(root.left, low, high);
-//         }
-        
-        
-//         return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
-//     }
-    
     public int rangeSumBST(TreeNode root, int low, int high) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int sum = 0;
-        
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            
-            if (node.val >= low && node.val <= high) {
-                sum += node.val;
-            } 
-            
-            if (node.val >= low && node.left != null) {
-                queue.offer(node.left);
-            } 
-            
-            if (node.val <= high && node.right != null) {
-                queue.offer(node.right);
-            }
+        if (root == null) {
+            return 0;
         }
         
-        return sum;
+        return (root.val >= low && root.val <= high ? root.val : 0) 
+            + rangeSumBST(root.left, low, high) 
+            + rangeSumBST(root.right, low, high);
     }
 }
