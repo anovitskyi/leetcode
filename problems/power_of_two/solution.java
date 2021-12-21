75216cf18ec8215255a9fd9c5d44ce72a3a809d0
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2021-12-21 16:49:12 +0000

    Sync LeetCode submission - Power of Two (java)

--- problems/power_of_two/solution.java
+++ problems/power_of_two/solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        for (int i = 1; i > 0 && i <= n; i = i << 1) {
-            if (i == n) {
-                return true;
-            }
+        if (n < 1) {
+            return false;
         }
         
-        return false;
+        return (n & (n - 1)) == 0;
     }
 }
-
-// log 2 n  = x
