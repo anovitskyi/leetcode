af11149c6d4168410e817f0113c67e4cb3d15738
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-01-17 16:30:21 +0000

    Sync LeetCode submission - Word Pattern (java)

--- problems/word_pattern/solution.java
+++ problems/word_pattern/solution.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         Map<Character, String> map = new HashMap<>();
-        Set<String> cache = new HashSet<>();
         String[] words = s.split(" ");
         
         if (pattern.length() != words.length) {
             return false;
         }
         
+        Set<String> visitedWords = new HashSet<>();
         for (int i = 0; i < pattern.length(); ++i) {
-            char letter = pattern.charAt(i);
+            char key = pattern.charAt(i);
             
-            if (!map.containsKey(letter)) {
-                if (cache.contains(words[i])) {
+            if (!map.containsKey(key)) {
+                if (visitedWords.contains(words[i])) {
                     return false;
                 }
                 
-                cache.add(words[i]);
-                map.put(letter, words[i]);
-            } else if (!words[i].equals(map.get(letter))) {
-                return false;
+                map.put(key, words[i]);
+                visitedWords.add(words[i]);
+            } else if (!map.get(key).equals(words[i])) {
+                    return false;
             }
+            
         }
-        
         return true;
     }
 }
