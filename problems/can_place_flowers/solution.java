98e352ac3b99513f52e16987d4e8f66f9062178a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-01-18 21:45:53 +0000

    Sync LeetCode submission - Can Place Flowers (java)

--- problems/can_place_flowers/solution.java
+++ problems/can_place_flowers/solution.java
@@ -1,23 +1,16 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n <= 0) {
+        if (n == 0) {
             return true;
         }
         
         for (int i = 0; i < flowerbed.length; ++i) {
-            if (flowerbed[i] == 1) {
-                continue;
+            if ((i - 1 < 0 || flowerbed[i - 1] == 0) && (i + 1 >= flowerbed.length || flowerbed[i + 1] == 0) && flowerbed[i] == 0) {
+                --n;
+                flowerbed[i] = 1;
             }
             
-            int left = i - 1 >= 0 ? flowerbed[i - 1] : 0;
-            int right = i + 1 < flowerbed.length ? flowerbed[i + 1] : 0;
-            
-            if (left == 1 || right == 1) {
-                continue;
-            }
-            
-            flowerbed[i] = 1;
-            if (--n == 0) {
+            if (n == 0) {
                 return true;
             }
         }
