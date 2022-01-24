cd83e9eb91000b44a7b67a300a61752f22fea83b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-01-24 22:40:55 +0000

    Sync LeetCode submission - Summary Ranges (java)

--- problems/summary_ranges/solution.java
+++ problems/summary_ranges/solution.java
@@ -2,22 +2,21 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new LinkedList<>();
         
-        int start = 0;
-        for (int i = 1; i < nums.length; ++i) {
-            if (nums[i] - nums[i - 1] != 1) {
-                if (i - 1 == start) {
-                    result.add(nums[start] + "");
-                } else {
-                    result.add(nums[start] + "->" + nums[i - 1]);
-                }
-                start = i;
+        for (int i = 0; i < nums.length; ++i) {
+            int left = i;
+            int right = i;
+            
+            while (right + 1 < nums.length && nums[right + 1] - nums[right] == 1) {
+                ++right;
             }
-        }
-        
-        if (start < nums.length - 1) {
-            result.add(nums[start] + "->" + nums[nums.length - 1]);
-        } else if (start == nums.length - 1) {
-            result.add(nums[start] + "");
+            
+            if (left == right) {
+                result.add(nums[left] + "");
+            } else {
+                result.add(nums[left] + "->" + nums[right]);
+            }
+            
+            i = right;
         }
         
         return result;
