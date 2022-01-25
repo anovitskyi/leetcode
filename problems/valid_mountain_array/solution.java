ad291b5c1d4ea9f6e60a651cfd31ffb0b9617ab8
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-01-25 23:57:04 +0000

    Sync LeetCode submission - Valid Mountain Array (java)

--- problems/valid_mountain_array/solution.java
+++ problems/valid_mountain_array/solution.java
@@ -1,24 +1,28 @@
 class Solution {
-    public boolean validMountainArray(int[] arr) { // 0 7 5
+    public boolean validMountainArray(int[] arr) {
         if (arr.length < 3) {
             return false;
         }
         
-        boolean isIncreasing = true;
-        for (int i = 0; i < arr.length - 1; ++i) {
-            if (arr[i] == arr[i + 1]) {
+        int counter = 0;
+        for (int i = 1; i < arr.length - 1; ++i) {
+            if (arr[i - 1] == arr[i] || arr[i + 1] == arr[i]) {
                 return false;
             }
             
-            if ((arr[i] < arr[i + 1]) != isIncreasing) {
-                if (isIncreasing == true && i > 0) {
-                    isIncreasing = false;
-                    continue;
-                }
+            if (arr[i - 1] > arr[i] && arr[i] < arr[i + 1]) {
                 return false;
             }
+            
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                if (counter > 0) {
+                    return false;
+                }
+                ++counter;
+            }
+            
         }
         
-        return isIncreasing == false;
+        return counter == 1;
     }
 }
