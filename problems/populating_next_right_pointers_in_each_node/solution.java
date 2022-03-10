4f7cef4346f162a00858630ffae61a4d34bd75f4
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-03-10 20:24:45 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node (java)

--- problems/populating_next_right_pointers_in_each_node/solution.java
+++ problems/populating_next_right_pointers_in_each_node/solution.java
@@ -1,23 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
 class Solution {
-    public Node connect(Node root) {
-        if (root == null) {
-            return root;
+    
+    public Node connect(Node node) {
+        if (node == null || node.left == null) {
+            return node;
         }
         
-        connect(root.left, root.right);
+        node.left.next = node.right;
+        if (node.next != null) {
+            node.right.next = node.next.left;
+        }
         
-        return root;
+        connect(node.left);
+        connect(node.right);
+        return node;
     }
     
-    private void connect(Node left, Node right) {
-        if (left == null) {
-            return;
-        }
-        
-        left.next = right;
-        
-        connect(left.left, left.right);
-        connect(right.left, right.right);
-        connect(left.right, right.left);
-    }
 }
