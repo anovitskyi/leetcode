4c0b0f49cd1ef28cd0943b278afb0cd39a9aee97
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-03-16 18:25:04 +0000

    Sync LeetCode submission - Count Binary Substrings (java)

--- problems/count_binary_substrings/solution.java
+++ problems/count_binary_substrings/solution.java
@@ -1,27 +1,20 @@
-class Solution {
+class Solution {                                
     public int countBinarySubstrings(String s) {
-        int result = 0;
-        
-        for (int i = 1; i < s.length(); ++i) {
-            if (s.charAt(i) != s.charAt(i - 1)) {
-                char initLeft = s.charAt(i - 1);
-                char initRight = s.charAt(i);
-                int left = i - 1;
-                
-                while (s.charAt(left) == initLeft && s.charAt(i) == initRight) {
-                    ++result;
-                    --left;
-                    ++i;
-                    
-                    if (left < 0 || i >= s.length()) {
-                        break;
-                    }
-                }
-                
-                --i;
-            }
+        if (s.length() <= 1) {
+            return 0;
         }
         
-        return result;
+        int result = 0;
+        int count = 1;
+        int prev = 0;
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                result += Math.min(prev, count);
+                prev = count;
+                count = 0;
+            }
+            ++count;
+        }
+        return result + Math.min(prev, count);
     }
 }
