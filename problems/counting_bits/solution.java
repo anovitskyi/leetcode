12a72f3f74bb7daa1000ab0bdb00de078b9c58da
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-03-24 12:40:02 +0000

    Sync LeetCode submission - Counting Bits (java)

--- problems/counting_bits/solution.java
+++ problems/counting_bits/solution.java
@@ -1,18 +1,39 @@
 class Solution {
-    public int[] countBits(int num) {
-        int[] result = new int[num + 1];
-        for (int i = 0; i <= num; ++i) {
-            result[i] = count(i);
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        if (n > 0) {
+            result[1] = 1;   
         }
+        
+        for (int i = 2; i <= n; ++i) {
+            result[i] = result[i / 2] + result[i % 2];
+        }
+        
         return result;
     }
-    
-    private int count(int num) {
-        int res = 0;
-        while (num > 0) {
-            res += num % 2;
-            num /= 2;
-        }
-        return res;
-    }
 }
+
+/**
+
+0: 0 -> 0
+
+1: 1 -> 1
+
+2: 10 -> 1
+3: 11 -> 2
+
+4: 100 -> 1
+5: 101 -> 2
+6: 110 -> 2
+7: 111 -> 3
+
+8: 1000 -> 1
+9: 1001 -> 2
+10: 1010 -> 2
+11: 1011 -> 3
+12: 1100 -> 2
+13: 1101 -> 3
+14: 1110 -> 3
+15: 1111 -> 4
+
+**/
