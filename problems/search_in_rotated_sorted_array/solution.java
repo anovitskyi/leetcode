b0dda22eb8f3ed4710c33f6770759b5c495f4e48
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-03-30 20:38:26 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- problems/search_in_rotated_sorted_array/solution.java
+++ problems/search_in_rotated_sorted_array/solution.java
@@ -1,49 +1,46 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums[0] <= nums[nums.length - 1]) {
-            return findTarget(nums, target, 0, nums.length - 1);
-        }
-        
-        int pivot = findPivotIndex(nums);
-        if (nums[0] <= target) {
-            return findTarget(nums, target, 0, pivot - 1);
-        } else {
-            return findTarget(nums, target, pivot, nums.length - 1);
-        }
-    }
-    
-    private int findPivotIndex(int[] nums) {
+        int pivot = findPivot(nums, 0, nums.length - 1);
         int left = 0;
         int right = nums.length - 1;
-        int middle = 0;
         
-        while (left <= right) {
-            middle = (right - left) / 2 + left;
-            
-            if (middle + 1 < nums.length && nums[middle] > nums[middle + 1]) {
-                return middle + 1;
-            } else if (middle - 1 >= 0 && nums[middle] < nums[middle - 1]) {
-                return middle;
-            } else if (nums[middle] > nums[0]) {
-                left = middle + 1;
-            } else if (nums[middle] < nums[0]) {
-                right = middle - 1;
+        if (pivot != right) {
+            if (target <= nums[right]) {
+                left = pivot + 1;
+            } else {
+                right = pivot;
             }
         }
         
-        return -1;
+        return findNumber(nums, target, left, right);
     }
     
-    private int findTarget(int[] nums, int target, int left, int right) {
+    private int findPivot(int[] nums, int left, int right) {
+        int result = right;
         while (left <= right) {
-            int middle = (right - left) / 2 + left;
+            int mid = ((right - left) / 2) + left;
             
-            if (nums[middle] == target) {
-                return middle;
-            } else if (nums[middle] > target) {
-                right = middle - 1;
+            if (mid < result && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] < nums[left]) {
+                right = mid - 1;
             } else {
-                left = middle + 1;
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+    
+    private int findNumber(int[] nums, int num, int left, int right) {
+        while (left <= right) {
+            int mid = ((right - left) / 2) + left;
+            
+            if (nums[mid] == num) {
+                return mid;
+            } else if (nums[mid] > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         
