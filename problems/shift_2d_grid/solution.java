a68e66347084eb7c6786f9c7bebefad67174d27b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-11 11:26:11 +0000

    Sync LeetCode submission - Shift 2D Grid (java)

--- problems/shift_2d_grid/solution.java
+++ problems/shift_2d_grid/solution.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         List<List<Integer>> result = new LinkedList<>();
-        
         int size = grid.length * grid[0].length;
+        k %= size;
         
         for (int i = 0; i < grid.length; ++i) {
-            List<Integer> list = new LinkedList<>();
-            
+            List<Integer> row = new LinkedList<>();
             for (int j = 0; j < grid[i].length; ++j) {
-                
-                int elem = i * grid[i].length + j - k;
-                int index = elem % size;
-                if (index < 0) {
-                    index += size;
-                }
-                list.add(grid[index / grid[i].length][index % grid[i].length]);
-                
+                row.add(getPrevElement(grid, i, j, size, k));
             }
-            
-            result.add(list);
+            result.add(row);
         }
         
         return result;
+    }
+    
+    private int getPrevElement(int[][] grid, int i, int j, int size, int k) {
+        int absoluteIndex = ((i * grid[i].length + j - k) + size) % size;
+        int row = absoluteIndex / grid[i].length;
+        int column = absoluteIndex % grid[i].length;
+        
+        return grid[row][column];
     }
 }
