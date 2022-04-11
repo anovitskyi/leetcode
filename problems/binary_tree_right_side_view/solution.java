1cf2356be11ca0d87bf75d7aba66c73160665333
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-11 11:06:21 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

--- problems/binary_tree_right_side_view/solution.java
+++ problems/binary_tree_right_side_view/solution.java
@@ -1,27 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new LinkedList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        if (root != null) {
-            queue.add(root);
+        return rightSideViewDFS(root);
+    }
+    
+    public List<Integer> rightSideViewDFS(TreeNode root) {
+        return dfs(root, new LinkedList<>(), 0);
+    }
+    
+    private List<Integer> dfs(TreeNode node, List<Integer> result, int level) {
+        if (node == null) {
+            return result;
         }
         
+        if (result.size() == level) {
+            result.add(node.val);
+        }
+        
+        dfs(node.right, result, level + 1);
+        dfs(node.left, result, level + 1);
+        
+        return result;
+    }
+    
+    public List<Integer> rightSideViewBFS(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
         while (!queue.isEmpty()) {
+            result.add(queue.peek().val);
+            
             int size = queue.size();
-            TreeNode node = null;
             while (size-- > 0) {
-                node = queue.poll();
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
+                TreeNode node = queue.poll();
                 
                 if (node.right != null) {
                     queue.offer(node.right);
                 }
+                
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
             }
-            result.add(node.val);
         }
-         
+        
         return result;
     }
 }
