6fa94358f93ad0f9b4f28bc55271c4b7cd649879
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-16 15:45:40 +0000

    Sync LeetCode submission - Find Center of Star Graph (java)

--- problems/find_center_of_star_graph/solution.java
+++ problems/find_center_of_star_graph/solution.java
@@ -1,9 +1,20 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
-            return edges[0][0];
-        } else {
-            return edges[0][1];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < edges.length; ++i) {
+            for (int j = 0; j < edges[i].length; ++j) {
+                int counter = map.getOrDefault(edges[i][j], 0) + 1;
+                map.put(edges[i][j], counter);
+            }
         }
+        
+        int result = 1;
+        for (int key : map.keySet()) {
+            if (map.get(key) > map.get(result)) {
+                result = key;
+            }
+        }
+        return result;
     }
 }
