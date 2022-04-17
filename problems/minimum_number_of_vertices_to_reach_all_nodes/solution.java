c6cfe9e04ad0b06c0e23c48ed113aef4ad3f0338
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-17 18:19:03 +0000

    Sync LeetCode submission - Minimum Number of Vertices to Reach All Nodes (java)

--- problems/minimum_number_of_vertices_to_reach_all_nodes/solution.java
+++ problems/minimum_number_of_vertices_to_reach_all_nodes/solution.java
@@ -1,21 +1,16 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        boolean[] visited = new boolean[n];
-        
+        int[] nodes = new int[n];
         for (List<Integer> edge : edges) {
-            visited[edge.get(1)] = true; 
+            ++nodes[edge.get(1)];
         }
         
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                result.add(i);
+        List<Integer> result = new LinkedList<>();
+        for (int node = 0; node < n; ++node) {
+            if (nodes[node] == 0) {
+                result.add(node);
             }
         }
-        
         return result;
     }
 }
-
-// [false, true, true, false, true, true]
-//   0      1       2     3       4      5
