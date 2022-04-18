443b8f96b85809f9a28efd5fc6a655dd0fc50dc6
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-18 12:25:18 +0000

    Sync LeetCode submission - Unique Paths (java)

--- problems/unique_paths/solution.java
+++ problems/unique_paths/solution.java
@@ -2,31 +2,18 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] matrix = new int[m][n];
         matrix[0][0] = 1;
-        dp(matrix, m - 1, n - 1);
-        return matrix[m - 1][n - 1];
+        return findPaths(matrix, m - 1, n - 1);
     }
     
-    private void dp(int[][] matrix, int x, int y) {
-        if (x < 0 || y < 0 || (x == 0 && y == 0)) {
-            return;
+    private int findPaths(int[][] matrix, int row, int column) {
+        if (row < 0 || column < 0) {
+            return 0;
         }
         
-        int prevX = 0;
-        if (x - 1 >= 0) {
-            if (matrix[x - 1][y] == 0) {
-                dp(matrix, x - 1, y);
-            }
-            prevX = matrix[x - 1][y];
+        if (matrix[row][column] == 0) {
+            matrix[row][column] = findPaths(matrix, row - 1, column) + findPaths(matrix, row, column - 1);
         }
         
-        int prevY = 0;
-        if (y - 1 >= 0) {
-            if (matrix[x][y - 1] == 0) {
-                dp(matrix, x, y - 1);
-            }
-            prevY = matrix[x][y - 1];
-        }
-        
-        matrix[x][y] = prevX + prevY;
+        return matrix[row][column];
     }
 }
