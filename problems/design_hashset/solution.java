042af8d90b6013843f9748c11d0c20ebc97cee63
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-21 07:33:57 +0000

    Sync LeetCode submission - Design HashSet (java)

--- problems/design_hashset/solution.java
+++ problems/design_hashset/solution.java
@@ -1,22 +1,24 @@
 class MyHashSet {
     
-    private boolean[] array = new boolean[1_000_001];
-
-    /** Initialize your data structure here. */
-    public MyHashSet() {
-        
-    }
+    private final boolean[] elements = new boolean[1_000_001];
     
     public void add(int key) {
-        array[key] = true;
+        elements[key] = true;
     }
     
     public void remove(int key) {
-        array[key] = false;
+        elements[key] = false;
     }
     
-    /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        return array[key];
+        return elements[key];
     }
 }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
