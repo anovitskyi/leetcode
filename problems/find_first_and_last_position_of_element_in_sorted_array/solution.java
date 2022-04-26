6a8e8eb52243362689b76fbd4f9678900e73d6f3
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-26 13:11:33 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (java)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
@@ -1,54 +1,33 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = findLeft(nums, target);
-        int right = findRight(nums, target);
-        
         return new int[] {
-            left, right
+            binarySearch(nums, target, -1),
+            binarySearch(nums, target, 1)
         };
     }
     
-    private int findLeft(int[] nums, int target) {
+    private int binarySearch(int[] nums, int target, int direction) {
+        int result = -1;
         int left = 0;
         int right = nums.length - 1;
         
-        int mid = -1;
         while (left <= right) {
-            mid = ((right - left) / 2) + left;
+            int middle = left + (right - left) / 2;
             
-            if (target > nums[mid]) {
-                left = mid + 1;
-            } else if (target < nums[mid]) {
-                right = mid - 1;
-            } else if (mid == 0 || nums[mid - 1] != target) {
-                return mid;
+            if (nums[middle] == target) {
+                result = middle;
+                if (direction == -1) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else if (nums[middle] > target) {
+                right = middle - 1;
             } else {
-                right = mid - 1;
+                left = middle + 1;
             }
         }
         
-        return -1;
-    }
-    
-    private int findRight(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        int mid = -1;
-        while (left <= right) {
-            mid = ((right - left) / 2) + left;
-            
-            if (target > nums[mid]) {
-                left = mid + 1;
-            } else if (target < nums[mid]) {
-                right = mid - 1;
-            } else if (mid == nums.length - 1 || nums[mid + 1] != target) {
-                return mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        return -1;
+        return result;
     }
 }
