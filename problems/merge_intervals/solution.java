37c7a3a19f2f3fa7570809befdd2a1306428491f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-04-27 08:15:18 +0000

    Sync LeetCode submission - Merge Intervals (java)

--- problems/merge_intervals/solution.java
+++ problems/merge_intervals/solution.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length < 2) {
-            return intervals;
-        }
-        
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        List<int[]> resultList = new ArrayList<>();
-        resultList.add(intervals[0]);
-        for (int i = 1; i < intervals.length; ++i) {
-            int[] prevIntervals = resultList.get(resultList.size() - 1);
-            if (intervals[i][0] > prevIntervals[1]) {
-                resultList.add(intervals[i]);
-                continue;
+        Arrays.sort(intervals, (a, b) -> {
+            int diff = a[0] - b[0];
+            
+            if (diff == 0) {
+                return a[1] - b[1];
             }
             
-            //prevIntervals[0] = Math.min(prevIntervals[0], intervals[i][0]);
-            prevIntervals[1] = Math.max(prevIntervals[1], intervals[i][1]);
+            return diff;
+        });
+        
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+        for (int i = 1; i < intervals.length; ++i) {
+            int[] prev = list.get(list.size() - 1);
+            int[] curr = intervals[i];
+            
+            if (prev[1] >= curr[0]) {
+                prev[1] = Math.max(prev[1], curr[1]);
+            } else {
+                list.add(curr);
+            }
         }
         
-        int[][] result = new int[resultList.size()][2];
-        for (int i = 0; i < resultList.size(); ++i) {
-            result[i] = resultList.get(i);
+        int[][] result = new int[list.size()][2];
+        for (int i = 0; i < result.length; ++i) {
+            result[i] = list.get(i);
         }
         return result;
     }
