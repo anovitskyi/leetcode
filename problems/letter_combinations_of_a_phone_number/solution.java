8df74b82e4ed2212dc36872d15476935017a26c8
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-09 10:32:33 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- problems/letter_combinations_of_a_phone_number/solution.java
+++ problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,37 +1,38 @@
 class Solution {
     
-    private final Map<Character, String> map = new HashMap<>(8);
+    private static final Map<Character, List<Character>> digitMap = new HashMap<>();
     
-    {
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+    static {
+        digitMap.put('2', List.of('a', 'b', 'c'));
+        digitMap.put('3', List.of('d', 'e', 'f'));
+        digitMap.put('4', List.of('g', 'h', 'i'));
+        digitMap.put('5', List.of('j', 'k', 'l'));
+        digitMap.put('6', List.of('m', 'n', 'o'));
+        digitMap.put('7', List.of('p', 'q', 'r', 's'));
+        digitMap.put('8', List.of('t', 'u', 'v'));
+        digitMap.put('9', List.of('w', 'x', 'y', 'z'));
     }
     
     public List<String> letterCombinations(String digits) {
         List<String> result = new LinkedList<>();
-        
-        if (digits.length() > 0) {
-            backtrack("", digits, result);   
+        if (digits.isEmpty()) {
+            return result;
         }
         
+        backtrack(result, "", digits, 0);
         return result;
     }
     
-    private void backtrack(String word, String digits, List<String> result) {
-        if (word.length() >= digits.length()) {
-            result.add(word);
+    private void backtrack(List<String> result, String curr, String digits, int digitIndex) {
+        if (curr.length() == digits.length()) {
+            result.add(curr);
             return;
         }
         
-        String arr = map.get(digits.charAt(word.length()));
-        for (char ch : arr.toCharArray()) {
-            backtrack(word + ch, digits, result);
+        List<Character> mappedLetters = digitMap.get(digits.charAt(digitIndex));
+        for (char d : mappedLetters) {
+            backtrack(result, curr + d, digits, digitIndex + 1);
         }
     }
+    
 }
