d973e32a1e53286095a6f7edfae1c759b1ad72dd
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-13 11:00:46 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- problems/longest_substring_without_repeating_characters/solution.java
+++ problems/longest_substring_without_repeating_characters/solution.java
@@ -1,20 +1,64 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstringSlidingWindow(String s) {
         Map<Character, Integer> map = new HashMap<>();
+        
         int left = 0;
         int right = 0;
         int result = 0;
         
         while (right < s.length()) {
-            char ch = s.charAt(right);
-            if (map.containsKey(ch)) {
-                left = Math.max(left, map.get(ch) + 1);
+            char rightChar = s.charAt(right);
+            
+            if (left > right) {
+                ++right;
+            } else if (map.getOrDefault(rightChar, 0) == 0) {
+                map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
+                ++right;
+            } else {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                result = Math.max(result, right - left);
+                ++left;
             }
-            map.put(ch, right);
-            ++right;
-            result = Math.max(result, right - left);
         }
-         
+        
+        return Math.max(result, right - left);
+    }
+    
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        int left = 0;
+        int right = 0;
+        int result = 0;
+        
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+            
+            if (map.containsKey(rightChar)) {
+                left = Math.max(left, map.get(rightChar) + 1);
+            }
+            
+            map.put(rightChar, right);
+            ++right;
+            result = Math.max(right - left, result);
+        }
+        
         return Math.max(result, right - left);
     }
 }
+
+/*
+
+        |
+"abcabcbb"
+         |
+ 
+ result = 3
+ [
+    a: 0
+    b: 1
+    c: 0
+ ]
+
+*/
