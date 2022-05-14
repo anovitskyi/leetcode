6855001afcf23baf6248ddeb65908cd37d67057b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-14 08:35:30 +0000

    Sync LeetCode submission - Search Insert Position (java)

--- problems/search_insert_position/solution.java
+++ problems/search_insert_position/solution.java
@@ -2,16 +2,17 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        int result = 0;
         
         while (left <= right) {
             int middle = left + (right - left) / 2;
             
             if (nums[middle] == target) {
                 return middle;
-            } else if (nums[middle] > target) {
-                right = middle - 1;
-            } else {
+            } else if (nums[middle] < target) {
                 left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
         
