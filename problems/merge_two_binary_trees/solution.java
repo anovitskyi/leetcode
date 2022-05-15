c3ffa64973139750112aaa8325a05d781bf58814
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-15 08:22:21 +0000

    Sync LeetCode submission - Merge Two Binary Trees (java)

--- problems/merge_two_binary_trees/solution.java
+++ problems/merge_two_binary_trees/solution.java
@@ -4,23 +4,32 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null) {
-            return t2;
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return null;
         }
         
-        if (t2 == null) {
-            return t1;
+        if (root1 == null) {
+            return root2;
         }
         
-        t1.val += t2.val;
-        t1.left = mergeTrees(t1.left, t2.left);
-        t1.right = mergeTrees(t1.right, t2.right);
+        if (root2 == null) {
+            return root1;
+        }
         
-        return t1;
+        TreeNode root = new TreeNode(root1.val + root2.val);
+        root.left = mergeTrees(root1.left, root2.left);
+        root.right = mergeTrees(root1.right, root2.right);
+        return root;
     }
 }
