3d48c46762cd385852241cee150bc203e6263f22
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-19 06:57:20 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- problems/climbing_stairs/solution.java
+++ problems/climbing_stairs/solution.java
@@ -2,20 +2,15 @@ class Solution {
     
     private static final int[] cache = new int[46];
     
+    static {
+        cache[1] = 1;
+        cache[2] = 2;
+    }
+    
     public int climbStairs(int n) {
-        if (n <= 1) {
-            return 1;
+        if (cache[n] == 0) {
+            cache[n] = climbStairs(n - 1) + climbStairs(n - 2);   
         }
-        
-        if (cache[n - 1] == 0) {
-            cache[n - 1] = climbStairs(n - 1);
-        }
-        
-        if (cache[n - 2] == 0) {
-            cache[n - 2] = climbStairs(n - 2);
-        }
-        
-        cache[n] = cache[n - 1] + cache[n - 2];
         return cache[n];
     }
 }
