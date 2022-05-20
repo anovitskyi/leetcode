70c38b8047aa21558207d1ff8cf8e571d6f2b94a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-20 14:24:03 +0000

    Sync LeetCode submission - 01 Matrix (java)

---
+++ problems/01_matrix/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for (int i = 0; i < mat.length; ++i) {
+            for (int j = 0; j < mat[i].length; ++j) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                } else {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int x = pos[0];
+            int y = pos[1];
+            
+            if (x - 1 >= 0 && mat[x - 1][y] > 0 && mat[x - 1][y] > mat[x][y] + 1) {
+                mat[x - 1][y] = mat[x][y] + 1;
+                queue.offer(new int[] {x - 1, y});
+            }
+            
+            if (x + 1 < mat.length && mat[x + 1][y] > 0 && mat[x + 1][y] > mat[x][y] + 1) {
+                mat[x + 1][y] = mat[x][y] + 1;
+                queue.offer(new int[] {x + 1, y});
+            }
+            
+            if (y - 1 >= 0 && mat[x][y - 1] > 0 && mat[x][y - 1] > mat[x][y] + 1) {
+                mat[x][y - 1] = mat[x][y] + 1;
+                queue.offer(new int[] {x, y - 1});
+            }
+            
+            if (y + 1 < mat[x].length && mat[x][y + 1] > 0 && mat[x][y + 1] > mat[x][y] + 1) {
+                mat[x][y + 1] = mat[x][y] + 1;
+                queue.offer(new int[] {x, y + 1});
+            }
+        }
+        return mat;
+    }
+    
+}
