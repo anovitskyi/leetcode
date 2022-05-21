cfa30bedd4222b42312571a984fff2e85c2898db
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-05-21 13:22:49 +0000

    Sync LeetCode submission - House Robber (java)

--- problems/house_robber/solution.java
+++ problems/house_robber/solution.java
@@ -1,22 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        
         if (nums.length == 1) {
             return nums[0];
         }
         
-        int prevprev = nums[0];
-        int prev = Math.max(prevprev, nums[1]);
-        
+        nums[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < nums.length; ++i) {
-            int max = Math.max(prev, nums[i] + prevprev);
-            prevprev = prev;
-            prev = max;
+            nums[i] = Math.max(nums[i - 1], nums[i - 2] + nums[i]);
         }
         
-        return prev;
+        return Math.max(nums[nums.length - 1], nums[nums.length - 2]);
     }
 }
