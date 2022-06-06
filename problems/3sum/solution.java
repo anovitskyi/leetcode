37649e9523b381920659acfce9c27d09bd72e2c6
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-06 07:16:41 +0000

    Sync LeetCode submission - 3Sum (java)

--- problems/3sum/solution.java
+++ problems/3sum/solution.java
@@ -1,48 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new LinkedList<>();
-        if (nums.length < 3) {
-            return result;
-        }
         Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
         
         for (int i = 0; i < nums.length - 2; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             
-            int left = i + 1;
-            int right = nums.length - 1;
-            int negative = 0 - nums[i];
-            while (left < right) {
-                if (left > i + 1 && nums[left] == nums[left - 1]) {
-                    ++left;
-                    continue;
-                }
-                
-                if (right < nums.length - 1 && nums[right] == nums[right + 1]) {
-                    --right;
-                    continue;
-                }
-                
-                int sum = nums[left] + nums[right];
-                if (sum == negative) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    ++left;
-                    --right;
-                } else if (sum > negative) {
-                    --right;
-                } else {
-                    ++left;
-                }
-            }
+            findSum(nums, i, result);
         }
         
         return result;
     }
+    
+    private void findSum(int[] nums, int start, List<List<Integer>> result) {
+        int left = start + 1;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            
+            if (sum + nums[start] == 0) {
+                result.add(Arrays.asList(nums[start], nums[left], nums[right]));
+                do {
+                    ++left;
+                } while (left < right && nums[left] == nums[left - 1]);
+                
+                do {
+                    --right;
+                } while (left < right && nums[right] == nums[right + 1]);
+            } else if (sum + nums[start] > 0) {
+                --right;
+            } else {
+                ++left;
+            }
+        }
+    }
 }
-
-// [-1,0,1,2,-1,-4]
-// [-4,-1,-1,0,1,2]
-
-// [-2,0,0,2,2]
