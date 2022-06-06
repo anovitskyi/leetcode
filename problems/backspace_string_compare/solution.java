3fffd5c04bb875ade5918bc6e541c507ab48c436
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-06 07:55:44 +0000

    Sync LeetCode submission - Backspace String Compare (java)

--- problems/backspace_string_compare/solution.java
+++ problems/backspace_string_compare/solution.java
@@ -1,47 +1,36 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int sPointer = s.length() - 1;
-        int tPointer = t.length() - 1;
-        int skipS = 0;
-        int skipT = 0;
-
-        while (sPointer >= 0 || tPointer >= 0) {
-            while (sPointer >= 0) {
-                if (s.charAt(sPointer) == '#') {
-                    skipS++; 
-                    sPointer--;
-                } else if (skipS > 0) {
-                    skipS--; 
-                    sPointer--;
-                } else {
-                    break;
-                }
-            }
-            
-            while (tPointer >= 0) {
-                if (t.charAt(tPointer) == '#') {
-                    skipT++; 
-                    tPointer--;
-                } else if (skipT > 0) {
-                    skipT--; 
-                    tPointer--;
-                } else {
-                    break;
-                }
-            }
-            
-            if (sPointer >= 0 && tPointer >= 0 && s.charAt(sPointer) != t.charAt(tPointer)) {
+        Stack<Character> sStack = convertToStack(s);
+        Stack<Character> tStack = convertToStack(t);
+        
+        if (sStack.size() != tStack.size()) {
+            return false;
+        }
+        
+        while (!sStack.isEmpty()) {
+            if (sStack.pop() != tStack.pop()) {
                 return false;
             }
-            
-            if ((sPointer >= 0) != (tPointer >= 0)) {
-                return false;
-            }
-            
-            sPointer--;
-            tPointer--;
         }
         
         return true;
+    }
+    
+    private Stack<Character> convertToStack(String word) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            
+            if (ch == '#') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        
+        return stack;
     }
 }
