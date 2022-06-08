57ec5f9c69128ae2d01a6de672f5c34d47fd6dfc
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-08 07:37:44 +0000

    Sync LeetCode submission - 01 Matrix (java)

--- problems/01_matrix/solution.java
+++ problems/01_matrix/solution.java
@@ -3,10 +3,10 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < mat.length; ++i) {
             for (int j = 0; j < mat[i].length; ++j) {
-                if (mat[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
+                if (mat[i][j] == 1) {
+                    mat[i][j] = Integer.MAX_VALUE;
                 } else {
-                    mat[i][j] *= -1;
+                    queue.offer(new int[] {i, j});
                 }
             }
         }
@@ -16,22 +16,22 @@ class Solution {
             int x = pos[0];
             int y = pos[1];
             
-            if (x - 1 >= 0 && (mat[x - 1][y] == -1 || mat[x][y] + 1 < mat[x - 1][y])) {
+            if (x - 1 >= 0 && mat[x - 1][y] > mat[x][y] + 1) {
                 mat[x - 1][y] = mat[x][y] + 1;
                 queue.offer(new int[] {x - 1, y});
             }
             
-            if (x + 1 < mat.length && (mat[x + 1][y] == -1 || mat[x][y] + 1 < mat[x + 1][y])) {
+            if (x + 1 < mat.length && mat[x + 1][y] > mat[x][y] + 1) {
                 mat[x + 1][y] = mat[x][y] + 1;
                 queue.offer(new int[] {x + 1, y});
             }
             
-            if (y - 1 >= 0 && (mat[x][y - 1] == -1 || mat[x][y] + 1 < mat[x][y - 1])) {
+            if (y - 1 >= 0 && mat[x][y - 1] > mat[x][y] + 1) {
                 mat[x][y - 1] = mat[x][y] + 1;
                 queue.offer(new int[] {x, y - 1});
             }
             
-            if (y + 1 < mat[x].length && (mat[x][y + 1] == -1 || mat[x][y] + 1 < mat[x][y + 1])) {
+            if (y + 1 < mat[x].length && mat[x][y + 1] > mat[x][y] + 1) {
                 mat[x][y + 1] = mat[x][y] + 1;
                 queue.offer(new int[] {x, y + 1});
             }
@@ -40,13 +40,3 @@ class Solution {
         return mat;
     }
 }
-
-/*
-
-    0 1 1 1
-    1 1 1 0
-    
-    0  1  2  1
-    1  2  1  0
-
-*/
