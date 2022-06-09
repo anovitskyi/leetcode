678dcd920ee40a3284ca0596a8534d852a02329b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-09 07:21:33 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node II (java)

--- problems/populating_next_right_pointers_in_each_node_ii/solution.java
+++ problems/populating_next_right_pointers_in_each_node_ii/solution.java
@@ -1,59 +1,38 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
 
 class Solution {
-    public Node connect(Node root) {
-        if (root != null) {
-            List<Node> list = new ArrayList<>();
-            list.add(root);
-            connect(list);
-        }
-        return root;
-    }
-    
-    private void connect(List<Node> list) {
-        if (list.isEmpty()) {
-            return;
-        }
-        
-        List<Node> nextList = new ArrayList<>();
-        
-        Node prev = new Node();
-        for (int i = 0; i < list.size(); ++i) {
-            Node curr = list.get(i);
-            
-            if (curr.left != null) {
-                nextList.add(curr.left);
-            }
-            
-            if (curr.right != null) {
-                nextList.add(curr.right);
-            }
-            
-            prev.next = curr;
-            prev = curr;
-        }
-        
-        connect(nextList);
-    }
+   public Node connect(Node root) {
+		Node curr = root ;
+		Node head = null , tail = null ;
+		while(curr!=null){
+			if(curr.left!=null){
+				if(head == null)
+					head = tail = curr.left ;
+
+				else{
+					tail.next = curr.left ;
+					tail = tail.next ;
+				}
+			}
+
+			if(curr.right!=null){
+				if(head == null)
+					head = tail = curr.right ;
+
+				else{
+					tail.next = curr.right ;
+					tail = tail.next ;
+				}
+			}
+
+			curr = curr.next ;
+
+			if(curr == null){
+				curr = head ;
+				head = tail = null ;
+			}
+		}
+
+		return root ;
+
+	}
 }
