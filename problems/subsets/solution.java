e595c6808f32b339d4b21e78e725c12bafa94386
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-11 09:05:28 +0000

    Sync LeetCode submission - Subsets (java)

--- problems/subsets/solution.java
+++ problems/subsets/solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new LinkedList<>();
-        backtrack(result, new LinkedList<>(), nums, 0);
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
     
-    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] nums, int start) {
+    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] nums, int index) {
+        if (index > nums.length) {
+            return;
+        }
+        
         result.add(curr);
         
-        for (int i = start; i < nums.length; ++i) {
-            List<Integer> copy = new LinkedList<>(curr);
-            copy.add(nums[i]);
+        for (int i = index; i < nums.length; ++i) {
+            List<Integer> copy = new ArrayList<>(curr);
+            curr.add(nums[i]);
             backtrack(result, copy, nums, i + 1);
         }
     }
