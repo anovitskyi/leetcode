ac960ede9e9b4c60c3c295332651cd7ecdaf6515
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-12 07:56:52 +0000

    Sync LeetCode submission - Subsets II (java)

--- problems/subsets_ii/solution.java
+++ problems/subsets_ii/solution.java
@@ -1,20 +1,27 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new LinkedList<>();
-        backtrack(nums, 0, result, new LinkedList<>());
+        
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, 0, result, new ArrayList<>());
         return result;
     }
     
     private void backtrack(int[] nums, int index, List<List<Integer>> result, List<Integer> curr) {
+        if (index > nums.length) {
+            return;
+        }
+        
         result.add(curr);
         
         for (int i = index; i < nums.length; ++i) {
-            if (i == index || nums[i - 1] != nums[i]) {
-                List<Integer> copy = new LinkedList<>(curr);
-                copy.add(nums[i]);
-                backtrack(nums, i + 1, result, copy);
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
             }
+            
+            List<Integer> copy = new ArrayList<>(curr);
+            copy.add(nums[i]);
+            backtrack(nums, i + 1, result, copy);
         }
     }
 }
