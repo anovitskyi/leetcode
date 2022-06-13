4912f1f35f3198dd7904601d997662e87b20a1f9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-13 07:02:38 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- problems/letter_combinations_of_a_phone_number/solution.java
+++ problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,38 +1,36 @@
 class Solution {
     
-    private static final Map<Character, List<Character>> digitMap = new HashMap<>();
+    private static final Map<Character, String> map = new HashMap<>();
     
     static {
-        digitMap.put('2', List.of('a', 'b', 'c'));
-        digitMap.put('3', List.of('d', 'e', 'f'));
-        digitMap.put('4', List.of('g', 'h', 'i'));
-        digitMap.put('5', List.of('j', 'k', 'l'));
-        digitMap.put('6', List.of('m', 'n', 'o'));
-        digitMap.put('7', List.of('p', 'q', 'r', 's'));
-        digitMap.put('8', List.of('t', 'u', 'v'));
-        digitMap.put('9', List.of('w', 'x', 'y', 'z'));
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
     }
     
     public List<String> letterCombinations(String digits) {
-        List<String> result = new LinkedList<>();
+        List<String> result = new ArrayList<>();
         if (digits.isEmpty()) {
             return result;
         }
-        
         backtrack(result, "", digits, 0);
         return result;
     }
     
     private void backtrack(List<String> result, String curr, String digits, int digitIndex) {
-        if (curr.length() == digits.length()) {
+        if (digitIndex >= digits.length()) {
             result.add(curr);
             return;
         }
         
-        List<Character> mappedLetters = digitMap.get(digits.charAt(digitIndex));
-        for (char d : mappedLetters) {
-            backtrack(result, curr + d, digits, digitIndex + 1);
+        String letters = map.get(digits.charAt(digitIndex));
+        for (int i = 0; i < letters.length(); ++i) {
+            backtrack(result, curr + letters.charAt(i), digits, digitIndex + 1);
         }
     }
-    
 }
