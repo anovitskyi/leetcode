cc74e3b3056dd421f1f2b338ae458a76a53646c5
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-26 10:16:02 +0000

    Sync LeetCode submission - 01 Matrix (java)

--- problems/01_matrix/solution.java
+++ problems/01_matrix/solution.java
@@ -3,10 +3,10 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < mat.length; ++i) {
             for (int j = 0; j < mat[i].length; ++j) {
-                if (mat[i][j] == 1) {
-                    mat[i][j] = Integer.MAX_VALUE;
-                } else {
+                if (mat[i][j] == 0) {
                     queue.offer(new int[] {i, j});
+                } else {
+                    mat[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
@@ -15,28 +15,29 @@ class Solution {
             int[] pos = queue.poll();
             int x = pos[0];
             int y = pos[1];
-            
+                
             if (x - 1 >= 0 && mat[x - 1][y] > mat[x][y] + 1) {
                 mat[x - 1][y] = mat[x][y] + 1;
                 queue.offer(new int[] {x - 1, y});
             }
-            
-            if (x + 1 < mat.length && mat[x + 1][y] > mat[x][y] + 1) {
-                mat[x + 1][y] = mat[x][y] + 1;
-                queue.offer(new int[] {x + 1, y});
-            }
-            
+                
             if (y - 1 >= 0 && mat[x][y - 1] > mat[x][y] + 1) {
                 mat[x][y - 1] = mat[x][y] + 1;
                 queue.offer(new int[] {x, y - 1});
             }
-            
+                
+            if (x + 1 < mat.length && mat[x + 1][y] > mat[x][y] + 1) {
+                mat[x + 1][y] = mat[x][y] + 1;
+                queue.offer(new int[] {x + 1, y});
+            }
+                
             if (y + 1 < mat[x].length && mat[x][y + 1] > mat[x][y] + 1) {
                 mat[x][y + 1] = mat[x][y] + 1;
                 queue.offer(new int[] {x, y + 1});
-            }
+            }    
         }
         
         return mat;
     }
+
 }
