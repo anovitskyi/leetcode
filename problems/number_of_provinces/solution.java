87842523c1c3625f8d98c55e35bf13a27cca6687
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-06-29 08:17:32 +0000

    Sync LeetCode submission - Number of Provinces (java)

--- problems/number_of_provinces/solution.java
+++ problems/number_of_provinces/solution.java
@@ -1,6 +1,5 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        Map<Integer, List<Integer>> graph = convertToGraph(isConnected);
         Set<Integer> visited = new HashSet<>();
         int result = 0;
         
@@ -9,45 +8,31 @@ class Solution {
                 continue;
             }
             
-            visitCity(graph, i, visited);
+            dfs(i, isConnected, visited);
             ++result;
         }
         
         return result;
     }
     
-    private void visitCity(Map<Integer, List<Integer>> graph, int city, Set<Integer> visited) {
-        if (visited.contains(city)) {
-            return;
-        }
+    private void dfs(int start, int[][] isConnected, Set<Integer> visited) {
+        int[] neighbors = isConnected[start];
         
-        visited.add(city);
-        
-        for (int neighbour : graph.get(city)) {
-            visitCity(graph, neighbour, visited);
-        }
-    }
-    
-    private Map<Integer, List<Integer>> convertToGraph(int[][] isConnected) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        for (int i = 0; i < isConnected.length; ++i) {
-            List<Integer> neighbours = new ArrayList<>();
-            map.put(i, neighbours);
-            
-            for (int j = 0; j < isConnected[i].length; ++j) {
-                if (i == j) {
-                    continue;
-                }
-                
-                if (isConnected[i][j] == 0) {
-                    continue;
-                }
-                
-                neighbours.add(j);
+        for (int i = 0; i < neighbors.length; ++i) {
+            if (i == start) {
+                continue;
             }
+            
+            if (neighbors[i] == 0) {
+                continue;
+            }
+            
+            if (visited.contains(i)) {
+                continue;
+            }
+            
+            visited.add(i);
+            dfs(i, isConnected, visited);
         }
-        
-        return map;
     }
 }
