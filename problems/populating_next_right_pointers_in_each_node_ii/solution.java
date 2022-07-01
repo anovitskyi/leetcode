5dd9ec859335a7592fd99061974cd9320d660a3b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-01 06:18:22 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node II (java)

--- problems/populating_next_right_pointers_in_each_node_ii/solution.java
+++ problems/populating_next_right_pointers_in_each_node_ii/solution.java
@@ -1,38 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
 
 class Solution {
-   public Node connect(Node root) {
-		Node curr = root ;
-		Node head = null , tail = null ;
-		while(curr!=null){
-			if(curr.left!=null){
-				if(head == null)
-					head = tail = curr.left ;
-
-				else{
-					tail.next = curr.left ;
-					tail = tail.next ;
-				}
-			}
-
-			if(curr.right!=null){
-				if(head == null)
-					head = tail = curr.right ;
-
-				else{
-					tail.next = curr.right ;
-					tail = tail.next ;
-				}
-			}
-
-			curr = curr.next ;
-
-			if(curr == null){
-				curr = head ;
-				head = tail = null ;
-			}
-		}
-
-		return root ;
-
-	}
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        
+        if (root.left != null && root.right != null) {
+            root.left.next = root.right;
+        }
+        
+        if (root.next != null && (root.left != null || root.right != null)) {
+            Node tmp = root.next;
+            while (tmp != null && tmp.left == null && tmp.right == null) {
+                tmp = tmp.next;
+            }
+            
+            if (tmp != null) {
+                Node source = root.right != null ? root.right : root.left;
+                Node target = tmp.left != null ? tmp.left : tmp.right;
+                source.next = target; 
+            }
+        }
+        
+        connect(root.right);
+        connect(root.left);
+        
+        return root;
+    }
 }
