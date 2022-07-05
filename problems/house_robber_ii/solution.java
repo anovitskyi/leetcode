5ae11aaa2601d3eaedfb8f45e9c88d4dd15c690e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-05 08:05:33 +0000

    Sync LeetCode submission - House Robber II (java)

--- problems/house_robber_ii/solution.java
+++ problems/house_robber_ii/solution.java
@@ -4,23 +4,30 @@ class Solution {
             return nums[0];
         }
         
-        if (nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        
-        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+        return Math.max(findMaxAmount(nums, 0, nums.length - 2), findMaxAmount(nums, 1, nums.length - 1));
     }
     
-    private int rob(int[] nums, int left, int right) {
-        int prevprev = nums[left];
-        int prev = Math.max(prevprev, nums[left + 1]);
+    private int findMaxAmount(int[] nums, int start, int end) {
+        int prevprev = 0;
+        int prev = nums[start];
         
-        for (int i = left + 2; i <= right; ++i) {
-            int tmp = Math.max(prev, nums[i] + prevprev);
-            prevprev = prev;
-            prev = tmp;
+        for (int i = start + 1; i <= end; ++i) {
+            int tmp = prev;
+            prev = Math.max(nums[i] + prevprev, prev);
+            prevprev = tmp;
         }
         
         return prev;
     }
 }
+
+/*
+     |
+[1,2,3,1]
+
+prevprev = 2
+prev = 4
+tmp = 2
+
+
+*/
