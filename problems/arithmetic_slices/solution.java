0f24136c01e65e45419d19cbb353b5f4f68ed195
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-06 05:47:20 +0000

    Sync LeetCode submission - Arithmetic Slices (java)

--- problems/arithmetic_slices/solution.java
+++ problems/arithmetic_slices/solution.java
@@ -1,17 +1,22 @@
 class Solution {
-    
     public int numberOfArithmeticSlices(int[] nums) {
-        int prev = 0;
-        int sum = 0;
+        int[] dp = new int[nums.length];
         for (int i = 2; i < nums.length; ++i) {
             if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-                prev = prev + 1;
-                sum += prev;
-            } else {
-                prev = 0;
+                dp[i] += 1 + dp[i - 1];
             }
         }
-        return sum;
+        
+        int result = 0;
+        for (int i = 0; i < dp.length; ++i) {
+            result += dp[i];
+        }
+        return result;
     }
-    
 }
+
+/*
+
+    
+
+*/
