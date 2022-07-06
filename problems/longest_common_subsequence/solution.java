88740632ad26178386076c280fcc58f0237f68ea
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-06 06:47:10 +0000

    Sync LeetCode submission - Longest Common Subsequence (java)

--- problems/longest_common_subsequence/solution.java
+++ problems/longest_common_subsequence/solution.java
@@ -5,13 +5,25 @@ class Solution {
         for (int i = text1.length() - 1; i >= 0; --i) {
             for (int j = text2.length() - 1; j >= 0; --j) {
                 if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
-                }
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }        
             }
         }
         
         return dp[0][0];
     }
+    
 }
+
+/*
+
+
+     a b c d e
+   a 0 0 0 0 0 0
+   c 0 0 0 0 0 0 
+   e 0 0 0 0 1 0
+     0 0 0 0 0 0
+
+*/
