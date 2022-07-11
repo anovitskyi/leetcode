dc4bebc661e03762fb2c36d267503b69f5b2aa84
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-11 09:25:03 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

--- problems/binary_tree_right_side_view/solution.java
+++ problems/binary_tree_right_side_view/solution.java
@@ -14,56 +14,24 @@
  * }
  */
 class Solution {
-    
     public List<Integer> rightSideView(TreeNode root) {
-        return rightSideViewDFS(root);
+        List<Integer> levels = new ArrayList<>();
+        dfsLevels(root, 0, levels);
+        return levels;
     }
     
-    public List<Integer> rightSideViewDFS(TreeNode root) {
-        return dfs(root, new LinkedList<>(), 0);
-    }
-    
-    private List<Integer> dfs(TreeNode node, List<Integer> result, int level) {
-        if (node == null) {
-            return result;
-        }
-        
-        if (result.size() == level) {
-            result.add(node.val);
-        }
-        
-        dfs(node.right, result, level + 1);
-        dfs(node.left, result, level + 1);
-        
-        return result;
-    }
-    
-    public List<Integer> rightSideViewBFS(TreeNode root) {
-        List<Integer> result = new LinkedList<>();
+    private void dfsLevels(TreeNode root, int currLevel, List<Integer> levels) {
         if (root == null) {
-            return result;
+            return;
         }
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        
-        while (!queue.isEmpty()) {
-            result.add(queue.peek().val);
-            
-            int size = queue.size();
-            while (size-- > 0) {
-                TreeNode node = queue.poll();
-                
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-                
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-            }
+        if (currLevel >= levels.size()) {
+            levels.add(root.val);
         }
         
-        return result;
+        dfsLevels(root.right, currLevel + 1, levels);
+        dfsLevels(root.left, currLevel + 1, levels);
     }
+    
+   
 }
