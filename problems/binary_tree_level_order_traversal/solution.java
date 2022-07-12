8e2e772767a233e380273e0821f944f04c7aba50
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-12 06:35:05 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- problems/binary_tree_level_order_traversal/solution.java
+++ problems/binary_tree_level_order_traversal/solution.java
@@ -1,26 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new LinkedList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        if (root != null) {
-            queue.offer(root);   
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
         }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
         
         while (!queue.isEmpty()) {
             int size = queue.size();
-            List<Integer> level = new LinkedList<>();
+            List<Integer> level = new ArrayList<>();
+            
             while (size-- > 0) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
                 
-                if (node.left != null) {
-                    queue.offer(node.left);   
+                if (curr.left != null) {
+                    queue.offer(curr.left);
                 }
                 
-                if (node.right != null) {
-                    queue.offer(node.right);   
+                if (curr.right != null) {
+                    queue.offer(curr.right);
                 }
             }
+            
             result.add(level);
         }
         
