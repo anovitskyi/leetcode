352fb31c613cc7a4a8a46ecfc0ba4b63a5e9aa59
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-12 06:51:44 +0000

    Sync LeetCode submission - Get Maximum in Generated Array (java)

--- problems/get_maximum_in_generated_array/solution.java
+++ problems/get_maximum_in_generated_array/solution.java
@@ -8,28 +8,21 @@ class Solution {
             return 1;
         }
         
-        int[] memo = new int[n + 1];
-        memo[1] = 1;
-        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
         int result = 0;
+        
         for (int i = 2; i <= n; ++i) {
-            memo[i] = memo[i / 2];
+            dp[i] = dp[i / 2];
             if (i % 2 == 1) {
-                memo[i] += memo[i / 2 + 1];
+                dp[i] += dp[i / 2 + 1];   
             }
             
-            result = Math.max(result, memo[i]);
+            if (dp[i] > result) {
+                result = dp[i];
+            }
         }
+        
         return result;
     }
-}   
-
-// n = 4
-
-/**
-
-  n = 20
-  i = 10
-  arr = [0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, ]
-
-**/
+}
