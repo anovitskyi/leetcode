444d0617ea6ef03d6eeb2ca9c8ebc75672d0a8b0
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-15 06:54:52 +0000

    Sync LeetCode submission - Number of Islands (java)

--- problems/number_of_islands/solution.java
+++ problems/number_of_islands/solution.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
                 if (grid[i][j] == '1') {
+                    visitIsland(grid, i, j);
                     ++result;
-                    visitIslandBfs(grid, i, j);
                 }
             }
         }
@@ -14,7 +14,7 @@ class Solution {
         return result;
     }
     
-    private void visitIslandDfs(char[][] grid, int x, int y) {
+    private void visitIsland(char[][] grid, int x, int y) {
         if (x < 0 || x >= grid.length) {
             return;
         }
@@ -23,45 +23,15 @@ class Solution {
             return;
         }
         
-        if (grid[x][y] == '0') {
+        if (grid[x][y] != '1') {
             return;
         }
         
         grid[x][y] = '0';
         
-        visitIslandDfs(grid, x - 1, y);
-        visitIslandDfs(grid, x + 1, y);
-        visitIslandDfs(grid, x, y - 1);
-        visitIslandDfs(grid, x, y + 1);
-    }
-    
-    private void visitIslandBfs(char[][] grid, int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j});
-        
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int x = pos[0];
-            int y = pos[1];
-            
-            if (x < 0 || x >= grid.length) {
-                continue;
-            }
-            
-            if (y < 0 || y >= grid[x].length) {
-                continue;
-            }
-            
-            if (grid[x][y] == '0') {
-                continue;
-            }
-            
-            grid[x][y] = '0';
-            
-            queue.offer(new int[] {x - 1, y});
-            queue.offer(new int[] {x + 1, y});
-            queue.offer(new int[] {x, y - 1});
-            queue.offer(new int[] {x, y + 1});
-        }
+        visitIsland(grid, x - 1, y);
+        visitIsland(grid, x + 1, y);
+        visitIsland(grid, x, y - 1);
+        visitIsland(grid, x, y + 1);
     }
 }
