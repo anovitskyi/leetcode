b32c33ae0385f74a258b809db2e64029a0dc065e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-19 06:03:05 +0000

    Sync LeetCode submission - Two Sum (java)

--- problems/two_sum/solution.java
+++ problems/two_sum/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < nums.length; ++i) {
             int diff = target - nums[i];
             
@@ -8,7 +9,7 @@ class Solution {
                 return new int[] {
                     i, map.get(diff)
                 };
-            }
+            } 
             
             map.put(nums[i], i);
         }
