64ae75d750f127b0999eb75dff63bd0afe1f1c63
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-20 05:57:36 +0000

    Sync LeetCode submission - Decode String (java)

--- problems/decode_string/solution.java
+++ problems/decode_string/solution.java
@@ -1,45 +1,23 @@
 class Solution {
     public String decodeString(String s) {
-        return decode(s, 0, s.length());
-    }
-    
-    private String decode(String s, int start, int end) {
-        StringBuilder b = new StringBuilder();
         
-        for (int i = start; i < end; ++i) {
-            char ch = s.charAt(i);
-            if (ch >= 'a' && ch <= 'z') {
-                b.append(ch);
-            } else if (ch >= '1' && ch <= '9') {
-                int counter = 0;
-                do {
-                    counter = counter * 10 + (ch - '0');
-                    ch = s.charAt(++i);
-                } while (ch >= '0' && ch <= '9');
-                
-                int st = i + 1;
-                int brackets = 0;
-                while (++i < end) {
-                    ch = s.charAt(i);
-                    if (ch == '[') {
-                        ++brackets;
-                    } else if (ch == ']') {
-                        if (brackets == 0) {
-                            break;
-                        } else {
-                            --brackets;
-                        }
-                    }
-                }
-                
-                
-                String copy = decode(s, st, i);
-                for (int j = 0; j < counter; ++j) {
-                    b.append(copy);
-                }
+        int lastIndex = s.lastIndexOf('[');
+        if(lastIndex<0) return s;
+        int temp=lastIndex-1;
+        String repeatString="";
+        while(temp>=0){
+            if(Character.isDigit(s.charAt(temp))){
+                temp--;
             }
+            else break;
         }
         
-        return b.toString();
+        int repeatNum=Integer.parseInt(s.substring(temp+1,lastIndex));
+        
+        String rpStr=s.substring(lastIndex+1, s.indexOf(']',lastIndex));
+        rpStr=rpStr.repeat(repeatNum);
+        s=s.replace((s.substring(temp+1,s.indexOf(']',temp+1)+1)),rpStr);
+        return decodeString(s);      
+        
     }
 }
