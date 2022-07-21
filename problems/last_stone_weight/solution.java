9b68626b4ac73e5d2a593a2c9193953e4a2f496b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-21 14:21:44 +0000

    Sync LeetCode submission - Last Stone Weight (java)

--- problems/last_stone_weight/solution.java
+++ problems/last_stone_weight/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         Queue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
-        for (int weight : stones) {
-            queue.offer(weight);
+        for (int stone : stones) {
+            queue.offer(stone);
         }
         
         while (queue.size() > 1) {
             int diff = queue.poll() - queue.poll();
+            
             if (diff != 0) {
                 queue.offer(diff);
             }
@@ -15,12 +16,3 @@ class Solution {
         return queue.isEmpty() ? 0 : queue.poll();
     }
 }
-
-/*
-
-8 7 4 2 1 1
-4 2 1 1 1
-2 1 1 1
-1 1 1
-1
-*/
