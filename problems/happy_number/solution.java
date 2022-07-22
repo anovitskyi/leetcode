19a7529abf2dfa3e41c496d96bec4237e0bbda5a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-22 06:21:17 +0000

    Sync LeetCode submission - Happy Number (java)

--- problems/happy_number/solution.java
+++ problems/happy_number/solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> prev = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         
-        while (true) {
-            if (n == 1) {
-                return true;
+        while (n != 1) {
+            if (visited.contains(n)) {
+                return false;
             }
+            visited.add(n);
             
             int tmp = 0;
             while (n > 0) {
                 int last = n % 10;
-                tmp = tmp + (last * last);
+                tmp += (last * last);
                 n = n / 10;
             }
-            
-            if (!prev.add(tmp)) {
-                return false;
-            }
-            
             n = tmp;
         }
+        
+        return true;
     }
 }
