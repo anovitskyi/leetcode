3db922e904b3f2ed2edbe7a93714c39a36573db9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-27 06:35:05 +0000

    Sync LeetCode submission - Invert Binary Tree (java)

--- problems/invert_binary_tree/solution.java
+++ problems/invert_binary_tree/solution.java
@@ -19,10 +19,6 @@ class Solution {
             return root;
         }
         
-        if (root.left == null && root.right == null) {
-            return root;
-        }
-        
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
