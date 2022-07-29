614f3261e20c786ac408a00d0fa08526e6fce2cd
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-29 06:23:01 +0000

    Sync LeetCode submission - Search a 2D Matrix (java)

--- problems/search_a_2d_matrix/solution.java
+++ problems/search_a_2d_matrix/solution.java
@@ -1,39 +1,47 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int top = 0;
-        int bottom = matrix.length - 1;
-        
-        while (top <= bottom) {
-            int middle = top + (bottom - top) / 2;
-            
-            if (matrix[middle][0] <= target && target <= matrix[middle][matrix[middle].length - 1]) {
-                return elementExists(matrix[middle], target);
-            } else if (matrix[middle][0] > target) {
-                bottom = middle - 1;
-            } else {
-                top = middle + 1;
-            }
+        int row = findRow(matrix, target, 0, matrix.length - 1);
+        if (row < 0) {
+            return false;
         }
         
-        return false;
+        int col = findCol(matrix[row], target, 0, matrix[row].length - 1);
+        if (col < 0) {
+            return false;
+        }
+        
+        return true;
     }
     
-    private boolean elementExists(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
+    private int findRow(int[][] matrix, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
             
-            if (nums[middle] == target) {
-                return true;
-            } else if (nums[middle] > target) {
-                right = middle - 1;
+            if (matrix[mid][0] <= target && target <= matrix[mid][matrix[mid].length - 1]) {
+                return mid;
+            } else if (target < matrix[mid][0]) {
+                end = mid - 1;
             } else {
-                left = middle + 1;
+                start = mid + 1;
             }
         }
         
-        return false;
+        return -1;
+    }
+    
+    private int findCol(int[] row, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (row[mid] == target) {
+                return mid;
+            } else if (row[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return -1;
     }
 }
