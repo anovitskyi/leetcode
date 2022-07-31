45b4eaa1021a8394feb34fc5184ee3a5bcb96e74
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-07-31 06:21:43 +0000

    Sync LeetCode submission - Rotting Oranges (java)

--- problems/rotting_oranges/solution.java
+++ problems/rotting_oranges/solution.java
@@ -1,94 +1,66 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int result = 0;
+        Queue<int[]> rottenOranges = new LinkedList<>();
         int freshOranges = 0;
-        Queue<int[]> queue = new LinkedList<>();
         
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == 0) {
+                    continue;
+                } else if (grid[i][j] == 1) {
                     ++freshOranges;
-                    if (isSurroundedByRottenOrange(grid, i, j)) {
-                        queue.offer(new int[] {i, j});   
-                    }
+                } else {
+                    rottenOranges.offer(new int[] {i, j});
                 }
             }
         }
         
-        int result = 0;
-        while (!queue.isEmpty()) {
-            int beforeChange = freshOranges;
+        while (freshOranges > 0 && !rottenOranges.isEmpty()) {
+            int before = freshOranges;
+            int size = rottenOranges.size();
             
-            int size = queue.size();
             while (size-- > 0) {
-                int[] pos = queue.poll();
-                int x = pos[0];
-                int y = pos[1];
-                
-                if (grid[x][y] != 1) {
-                    continue;
-                }
-                
-                grid[x][y] = 2;
-                --freshOranges;
+                int[] poll = rottenOranges.poll();
+                int x = poll[0];
+                int y = poll[1];
                 
                 if (x - 1 >= 0 && grid[x - 1][y] == 1) {
-                    queue.offer(new int[] {x - 1, y});
-                }
-                
-                if (y - 1 >= 0 && grid[x][y - 1] == 1) {
-                    queue.offer(new int[] {x, y - 1});
+                    --freshOranges;
+                    grid[x - 1][y] = 2;
+                    rottenOranges.offer(new int[] {x - 1, y});
                 }
                 
                 if (x + 1 < grid.length && grid[x + 1][y] == 1) {
-                    queue.offer(new int[] {x + 1, y});
+                    --freshOranges;
+                    grid[x + 1][y] = 2;
+                    rottenOranges.offer(new int[] {x + 1, y});
+                }
+                
+                if (y - 1 >= 0 && grid[x][y - 1] == 1) {
+                    --freshOranges;
+                    grid[x][y - 1] = 2;
+                    rottenOranges.offer(new int[] {x, y - 1});
                 }
                 
                 if (y + 1 < grid[x].length && grid[x][y + 1] == 1) {
-                    queue.offer(new int[] {x, y + 1});
+                    --freshOranges;
+                    grid[x][y + 1] = 2;
+                    rottenOranges.offer(new int[] {x, y + 1});
                 }
             }
             
-            if (freshOranges < beforeChange) {
-                ++result;   
-            } else {
-                break;
+            if (before == freshOranges) {
+                return -1;
             }
+            
+            ++result;
         }
         
-        if (freshOranges != 0) {
+        if (freshOranges > 0 && rottenOranges.isEmpty()) {
             return -1;
         }
+        
         return result;
     }
-    
-    private boolean isSurroundedByRottenOrange(int[][] grid, int i, int j) {
-        if (i - 1 >= 0 && grid[i - 1][j] == 2) {
-            return true;
-        }
-        
-        if (j - 1 >= 0 && grid[i][j - 1] == 2) {
-            return true;
-        }
-        
-        if (i + 1 < grid.length && grid[i + 1][j] == 2) {
-            return true;
-        }
-        
-        if (j + 1 < grid[i].length && grid[i][j + 1] == 2) {
-            return true;
-        }
-        
-        return false;
-    }
 }
-
-/*
-
-
-[[2,1,1],[1,1,0],[0,1,1]]
-
- 2 2 1
- 2 1 0
- 0 1 1
-
-*/
