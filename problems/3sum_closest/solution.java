a82570f7112376f52b2374e7f2bf4710ed7dde1d
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-07 16:20:03 +0000

    Sync LeetCode submission - 3Sum Closest (java)

--- problems/3sum_closest/solution.java
+++ problems/3sum_closest/solution.java
@@ -1,41 +1,38 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int result = Integer.MAX_VALUE - Math.abs(target);
         
+        int min = Integer.MAX_VALUE - Math.abs(target);
         for (int i = 0; i < nums.length - 2; ++i) {
-            int sum = findSum(nums, target - nums[i], i + 1, nums.length - 1) + nums[i];
-            if (Math.abs(target - result) > Math.abs(target - sum)) {
-                result = sum;
+            int currSum = findSum(nums, i + 1, nums.length - 1, target - nums[i]) + nums[i];
+            
+            if (Math.abs(target - currSum) < Math.abs(target - min)) {
+                min = currSum;
             }
         }
         
-        return result;
+        return min;
     }
     
-    private int findSum(int[] nums, int target, int left, int right) {
+    private int findSum(int[] nums, int left, int right, int target) {
         int result = Integer.MAX_VALUE - Math.abs(target);
         
         while (left < right) {
             int sum = nums[left] + nums[right];
             
-            if (sum == target) {
-                return sum;
-            } else if (sum < target) {
-                ++left;
-            } else {
-                --right;
+            if (Math.abs(target - sum) < Math.abs(target - result)) {
+                result = sum;
             }
             
-            if (Math.abs(target - result) > Math.abs(target - sum)) {
-                result = sum;
+            if (sum == target) {
+                break;
+            } else if (sum > target) {
+                --right;
+            } else {
+                ++left;
             }
         }
         
         return result;
     }
 }
-
-
-// -4 -1 1 2     1
-// -1  
