e48a0042fae401baeadea1dd92f7baaf32dc52a9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-07 22:59:58 +0000

    Sync LeetCode submission - Same Tree (java)

--- problems/same_tree/solution.java
+++ problems/same_tree/solution.java
@@ -4,18 +4,29 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null) {
-            return q == null;
+        if (p == null && q == null) {
+            return true;
         }
         
-        return q != null 
-            && p.val == q.val 
-            && isSameTree(p.left, q.left) 
-            && isSameTree(p.right, q.right);
+        if (p == null || q == null) {
+            return false;
+        }
+        
+        if (p.val != q.val) {
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
