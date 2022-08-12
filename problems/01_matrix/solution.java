2664ff5e8fca2911b3bd323f3c108b97ab8ca482
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-12 08:00:31 +0000

    Sync LeetCode submission - 01 Matrix (java)

--- problems/01_matrix/solution.java
+++ problems/01_matrix/solution.java
@@ -15,29 +15,28 @@ class Solution {
             int[] pos = queue.poll();
             int x = pos[0];
             int y = pos[1];
-                
+            
             if (x - 1 >= 0 && mat[x - 1][y] > mat[x][y] + 1) {
                 mat[x - 1][y] = mat[x][y] + 1;
                 queue.offer(new int[] {x - 1, y});
             }
-                
-            if (y - 1 >= 0 && mat[x][y - 1] > mat[x][y] + 1) {
-                mat[x][y - 1] = mat[x][y] + 1;
-                queue.offer(new int[] {x, y - 1});
-            }
-                
+            
             if (x + 1 < mat.length && mat[x + 1][y] > mat[x][y] + 1) {
                 mat[x + 1][y] = mat[x][y] + 1;
                 queue.offer(new int[] {x + 1, y});
             }
-                
+            
+            if (y - 1 >= 0 && mat[x][y - 1] > mat[x][y] + 1) {
+                mat[x][y - 1] = mat[x][y] + 1;
+                queue.offer(new int[] {x, y - 1});
+            }
+            
             if (y + 1 < mat[x].length && mat[x][y + 1] > mat[x][y] + 1) {
                 mat[x][y + 1] = mat[x][y] + 1;
                 queue.offer(new int[] {x, y + 1});
-            }    
+            }
         }
         
         return mat;
     }
-
 }
