a3a2f436ceac8cdf4011931acaaf5074dc94329a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-14 09:44:05 +0000

    Sync LeetCode submission - Missing Number (java)

--- problems/missing_number/solution.java
+++ problems/missing_number/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int expectedSum = nums.length;
         int actualSum = 0;
-        int expectedSum = 0;
         
         for (int i = 0; i < nums.length; ++i) {
+            expectedSum += i;
             actualSum += nums[i];
-            expectedSum += i + 1;
         }
         
         return expectedSum - actualSum;
