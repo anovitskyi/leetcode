cc97e4e7cc3a5c30f011bc41d15c7632e7cd8f7f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-19 07:08:09 +0000

    Sync LeetCode submission - Find Peak Element (java)

--- problems/find_peak_element/solution.java
+++ problems/find_peak_element/solution.java
@@ -5,11 +5,12 @@ class Solution {
         
         while (left <= right) {
             int middle = left + (right - left) / 2;
+            
             long curr = nums[middle];
             long prev = middle - 1 >= 0 ? nums[middle - 1] : Long.MIN_VALUE;
             long next = middle + 1 < nums.length ? nums[middle + 1] : Long.MIN_VALUE;
             
-            if (prev < curr && curr > next) {
+            if (prev < curr && next < curr) {
                 return middle;
             } else if (prev > curr) {
                 right = middle - 1;
