b966b33e19064be168c8a2e4aa3c8970b605f390
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-26 12:20:42 +0000

    Sync LeetCode submission - House Robber II (java)

--- problems/house_robber_ii/solution.java
+++ problems/house_robber_ii/solution.java
@@ -1,40 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
+        int left = 0;
+        int right = nums.length - 1;
         
-        return Math.max(findMaxSum(nums, 0, nums.length - 2), findMaxSum(nums, 1, nums.length - 1));
-    }
-    
-    private int findMaxSum(int[] nums, int left, int right) {
         if (left == right) {
             return nums[left];
         }
         
-        if (right - left == 1) {
-            return Math.max(nums[left], nums[left + 1]);
+        return Math.max(rob(nums, left, right - 1), rob(nums, left + 1, right));
+    }
+    
+    private int rob(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
         }
         
         int prevprev = nums[left];
-        int prev = Math.max(nums[left], nums[left + 1]);
-        for (int i = left + 2; i <= right; ++i) {
+        int prev = Math.max(nums[left], nums[++left]);
+        
+        while (++left <= right) {
             int tmp = prev;
-            prev = Math.max(prev, nums[i] + prevprev);
+            prev = Math.max(nums[left] + prevprev, prev);
             prevprev = tmp;
         }
         return prev;
     }
 }
-
-/*
-    
-    [1,2,3,1,5,4,3,7]
-    
-    
-     2 3 3 8 8 11 15 
-    [2,3,1,5,4,3,7] => 
-    ||
-    [1,2,3,1,5,4,3]
-
-*/
