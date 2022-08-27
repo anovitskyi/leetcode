1ccc59b393f02b0bb9460a891c563379c7ab0a41
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-27 08:25:49 +0000

    Sync LeetCode submission - Flood Fill (java)

--- problems/flood_fill/solution.java
+++ problems/flood_fill/solution.java
@@ -1,29 +1,40 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] != color) {
-            fill(image, sr, sc, image[sr][sc], color);
+        if (image[sr][sc] == color) {
+            return image;
+        }
+        
+        int initColor = image[sr][sc];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {sr, sc});
+        
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int x = pos[0];
+            int y = pos[1];
+            
+            if (image[x][y] != initColor) {
+                continue;
+            }
+            image[x][y] = color;
+            
+            if (x - 1 >= 0 && image[x - 1][y] == initColor) {
+                queue.offer(new int[] {x - 1, y});
+            }
+            
+            if (x + 1 < image.length && image[x + 1][y] == initColor) {
+                queue.offer(new int[] {x + 1, y});
+            }
+            
+            if (y - 1 >= 0 && image[x][y - 1] == initColor) {
+                queue.offer(new int[] {x, y - 1});
+            }
+            
+            if (y + 1 < image[x].length && image[x][y + 1] == initColor) {
+                queue.offer(new int[] {x, y + 1});
+            }
         }
         
         return image;
-    }
-    
-     private void fill(int[][] image, int sr, int sc, int originalColor, int newColor) {
-        if (sr < 0 || sr >= image.length) {
-            return;
-        }
-        
-        if (sc < 0 || sc >= image[sr].length) {
-            return;
-        }
-         
-        if (image[sr][sc] != originalColor) {
-             return;
-        }
-        
-        image[sr][sc] = newColor;
-        fill(image, sr - 1, sc, originalColor, newColor);
-        fill(image, sr + 1, sc, originalColor, newColor);
-        fill(image, sr, sc - 1, originalColor, newColor);
-        fill(image, sr, sc + 1, originalColor, newColor);
     }
 }
