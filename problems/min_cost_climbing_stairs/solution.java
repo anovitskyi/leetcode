9510db09d2784b5270fd7b73f0f071973322e745
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-27 07:37:55 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (java)

--- problems/min_cost_climbing_stairs/solution.java
+++ problems/min_cost_climbing_stairs/solution.java
@@ -1,11 +1,9 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int length = cost.length;
-        
-        for (int i = 2; i < length; ++i) {
-            cost[i] = cost[i] + Math.min(cost[i - 2], cost[i - 1]);
+        for (int i = 2; i < cost.length; ++i) {
+            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
         }
         
-        return Math.min(cost[length - 1], cost[length - 2]);
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
     }
 }
