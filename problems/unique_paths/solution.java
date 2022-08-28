d83c94d46f675553871e50026e96e0bc576528b8
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-28 08:57:36 +0000

    Sync LeetCode submission - Unique Paths (java)

--- problems/unique_paths/solution.java
+++ problems/unique_paths/solution.java
@@ -2,28 +2,25 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
-        traverse(dp, m - 1, n - 1);
-        return dp[m - 1][n - 1];
+        return calculatePaths(dp, m - 1, n - 1);
     }
     
-    private void traverse(int[][] dp, int x, int y) {
-        if (x < 0 || y < 0 || dp[x][y] != 0) {
-            return;
+    private int calculatePaths(int[][] dp, int x, int y) {
+        if (x < 0 || x >= dp.length) {
+            return 0;
         }
         
-        traverse(dp, x - 1, y);
-        traverse(dp, x, y - 1);
-        
-        int up = 0;
-        if (x - 1 >= 0) {
-            up = dp[x - 1][y];
+        if (y < 0 || y >= dp[x].length) {
+            return 0;
         }
         
-        int left = 0;
-        if (y - 1 >= 0) {
-            left = dp[x][y - 1];
+        if (dp[x][y] == 0) {
+            int left = calculatePaths(dp, x, y - 1);
+            int up = calculatePaths(dp, x - 1, y);
+        
+            dp[x][y] = left + up;
         }
         
-        dp[x][y] = left + up;
+        return dp[x][y];
     }
 }
