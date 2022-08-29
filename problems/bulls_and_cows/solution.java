b7dc7bf9d5c51edb5f877faf8b2174fac8d47176
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-29 07:18:41 +0000

    Sync LeetCode submission - Bulls and Cows (java)

--- problems/bulls_and_cows/solution.java
+++ problems/bulls_and_cows/solution.java
@@ -3,39 +3,27 @@ class Solution {
         int bulls = 0;
         int cows = 0;
         
-        int[] map = new int[10];
+        Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < secret.length(); ++i) {
-            int secretCh = secret.charAt(i) - '0';
-            int guessCh = guess.charAt(i) - '0';
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
             
-            if (secretCh == guessCh) {
+            if (s == g) {
                 ++bulls;
                 continue;
             }
             
-            ++map[secretCh];
-            if (map[secretCh] <= 0) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+            if (map.get(s) <= 0) {
                 ++cows;
             }
             
-            --map[guessCh];
-            if (map[guessCh] >= 0) {
+            map.put(g, map.getOrDefault(g, 0) - 1);
+            if (map.get(g) >= 0) {
                 ++cows;
             }
         }
-    
+        
         return bulls + "A" + cows + "B";
     }
 }
-
-/*
-        |
-    "1122"
-    "2211"
-    
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-    bulls = 0;
-    cows = 0;
-
-*/
