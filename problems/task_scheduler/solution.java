0aa68542fca24c7569d9402228e5398adfd70b7d
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-30 08:07:49 +0000

    Sync LeetCode submission - Task Scheduler (java)

--- problems/task_scheduler/solution.java
+++ problems/task_scheduler/solution.java
@@ -1,34 +1,22 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> map = new HashMap<>();
+        Queue<Integer> queue = new PriorityQueue<>();
+        
         for (char task : tasks) {
-            map.put(task, map.getOrDefault(task, 0) + 1);
+            int newTime = map.getOrDefault(task, 0 - n  - 1) + n + 1;
+            map.put(task, newTime);
+            queue.offer(newTime);
         }
         
-        int time = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        Queue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
-        for (int quantity : map.values()) {
-            heap.offer(quantity);
-        }
-        
-        while (!heap.isEmpty() || !queue.isEmpty()) {
-            if (!heap.isEmpty()) {
-                int poll = heap.poll();
-                if (poll > 1) {
-                    queue.offer(new int[] {poll - 1, time + n});   
-                }
+        int result = 0;
+        while (!queue.isEmpty()) {
+            if (queue.peek() <= result) {
+                queue.poll();
             }
             
-            if (!queue.isEmpty()) {
-                if (queue.peek()[1] <= time) {
-                    heap.offer(queue.poll()[0]);
-                }
-            }
-            
-            ++time;
+            ++result;
         }
-            
-        return time;
-    }
+        return result;
+    }   
 }
