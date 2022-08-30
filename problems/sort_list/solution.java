243f56236c33f317cf8bd1ba6358c40cfd146b81
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-08-30 07:03:53 +0000

    Sync LeetCode submission - Sort List (java)

--- problems/sort_list/solution.java
+++ problems/sort_list/solution.java
@@ -1,15 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        
-        int size = getListSize(head);
-        return sort(head, size);
+        return mergeSort(head, getSize(head));
     }
     
-    private ListNode sort(ListNode head, int size) {
-        if (size == 1) {
+    private ListNode mergeSort(ListNode head, int size) {
+        if (size <= 1) {
             return head;
         }
         
@@ -20,44 +25,43 @@ class Solution {
             right = right.next;
         }
         
-        left = sort(left, mid);
-        right = sort(right, size - mid);
-        return merge(left, mid, right, size - mid);
-    }
-    
-    private ListNode merge(ListNode left, int leftSize, ListNode right, int rightSize) {
+        left = mergeSort(left, mid);
+        right = mergeSort(right, size - mid);
+        
         ListNode result = new ListNode();
         ListNode curr = result;
-        
+        int leftSize = mid;
+        int rightSize = size - mid;
         while (leftSize > 0 && rightSize > 0) {
             if (left.val < right.val) {
                 curr.next = left;
-                --leftSize;
                 left = left.next;
+                --leftSize;
             } else {
                 curr.next = right;
-                --rightSize;
                 right = right.next;
+                --rightSize;
             }
             
             curr = curr.next;
         }
         
-        while (leftSize-- > 0) {
+        while (leftSize > 0) {
             curr.next = left;
             left = left.next;
+            --leftSize;
             curr = curr.next;
         }
-        
+
         curr.next = right;
         return result.next;
     }
     
-    private int getListSize(ListNode head) {
+    private int getSize(ListNode head) {
         int size = 0;
         while (head != null) {
-            head = head.next;
             ++size;
+            head = head.next;
         }
         return size;
     }
