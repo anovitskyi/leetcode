cdabf02e21a465f4d2ab6f02e762527617d8e4e7
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-01 06:34:53 +0000

    Sync LeetCode submission - Course Schedule II (java)

--- problems/course_schedule_ii/solution.java
+++ problems/course_schedule_ii/solution.java
@@ -1,57 +1,42 @@
 class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) { 
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] prereq : prerequisites) {
-            graph.computeIfAbsent(prereq[0], x -> new ArrayList<>()).add(prereq[1]);
-        } 
+        for (int[] pair : prerequisites) {
+            graph.computeIfAbsent(pair[0], x -> new ArrayList<>()).add(pair[1]);
+        }
         
         Set<Integer> result = new LinkedHashSet<>(numCourses);
         for (int i = 0; i < numCourses; ++i) {
-            boolean isPath = dfs(graph, i, result, new HashSet<>()); 
-            if (!isPath) {
-                return new int[0];
+            if (!dfs(graph, result, i, new HashSet<>())) {
+               return new int[0]; 
             }
         }
         
-        int[] r = new int[numCourses];
+        int[] res = new int[numCourses];
         int i = 0;
-        for (int el : result) {
-            r[i++] = el;
+        for (int num : result) {
+            res[i++] = num;
         }
-        return r;
+        return res;
     }
     
-    private boolean dfs(Map<Integer, List<Integer>> graph, int edge, Set<Integer> result, Set<Integer> path) {
-        if (result.contains(edge)) {
+    private boolean dfs(Map<Integer, List<Integer>> graph, Set<Integer> result, int currCourse, Set<Integer> path) {
+        if (result.contains(currCourse)) {
             return true;
         }
         
-        if (path.contains(edge)) {
+        if (path.contains(currCourse)) {
             return false;
         }
-        path.add(edge);
+        path.add(currCourse);
         
-        
-        for (int neighbour : graph.getOrDefault(edge, new ArrayList<>())) {
-            boolean isPath = dfs(graph, neighbour, result, path);
-            if (!isPath) {
+        for (int nextCourse : graph.getOrDefault(currCourse, Collections.emptyList())) {
+            if (!dfs(graph, result, nextCourse, path)) {
                 return false;
             }
         }
         
-        result.add(edge);
+        result.add(currCourse);
         return true;
     }
 }
-
-/*
-
-    [[0,1],[1,0]]
-    
-    0 -> [1]
-    1 -> [0]
-    
-    0 <-> 1
-    
-
-*/
