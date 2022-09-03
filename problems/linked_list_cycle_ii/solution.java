66eca0b7150cb08a14dc99150558dd69a8bdc9f3
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-03 13:11:47 +0000

    Sync LeetCode submission - Linked List Cycle II (java)

--- problems/linked_list_cycle_ii/solution.java
+++ problems/linked_list_cycle_ii/solution.java
@@ -11,14 +11,14 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
         
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
             
-            if (fast == slow) {
+            if (slow == fast) {
                 break;
             }
         }
@@ -27,11 +27,11 @@ public class Solution {
             return null;
         }
         
-        fast = head;
-        while (fast != slow) {
-            fast = fast.next;
+        while (head != slow) {
+            head = head.next;
             slow = slow.next;
         }
-        return slow;
+        
+        return head;
     }
 }
