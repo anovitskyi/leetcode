192b9a24a32650bd53228926ceb6d008131c65aa
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-04 13:13:33 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (java)

--- problems/longest_repeating_character_replacement/solution.java
+++ problems/longest_repeating_character_replacement/solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int[] tab = new int[26];
-        int result = 0;
         int left = 0;
         int right = 0;
+        int result = 0;
         
         while (right < s.length()) {
             ++tab[s.charAt(right) - 'A'];
             ++right;
             
             int max = 0;
-            for (int counter : tab) {
-                if (counter > max) {
-                    max = counter;
+            for (int i = 0; i < tab.length; ++i) {
+                if (tab[i] > max) {
+                    max = tab[i];
                 }
             }
             
             if (right - left - max > k) {
                 --tab[s.charAt(left) - 'A'];
-                ++left;
+                ++left;   
             }
             
             result = Math.max(result, right - left);
