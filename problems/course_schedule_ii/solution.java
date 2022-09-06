d55d1090df8badfc2225db3f61161f9fe2970d38
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-06 08:05:52 +0000

    Sync LeetCode submission - Course Schedule II (java)

--- problems/course_schedule_ii/solution.java
+++ problems/course_schedule_ii/solution.java
@@ -1,42 +1,42 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] pair : prerequisites) {
-            graph.computeIfAbsent(pair[0], x -> new ArrayList<>()).add(pair[1]);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] p : prerequisites) {
+            map.computeIfAbsent(p[0], x -> new ArrayList<>()).add(p[1]);
         }
         
         Set<Integer> result = new LinkedHashSet<>(numCourses);
         for (int i = 0; i < numCourses; ++i) {
-            if (!dfs(graph, result, i, new HashSet<>())) {
-               return new int[0]; 
+            if (!dfs(map, i, result, new HashSet<>())) {
+                return new int[0];
             }
         }
         
-        int[] res = new int[numCourses];
+        int[] r = new int[numCourses];
         int i = 0;
         for (int num : result) {
-            res[i++] = num;
+            r[i++] = num;
         }
-        return res;
+        return r;
     }
     
-    private boolean dfs(Map<Integer, List<Integer>> graph, Set<Integer> result, int currCourse, Set<Integer> path) {
-        if (result.contains(currCourse)) {
+    private boolean dfs(Map<Integer, List<Integer>> map, int curr, Set<Integer> result, Set<Integer> path) {
+        if (result.contains(curr)) {
             return true;
         }
         
-        if (path.contains(currCourse)) {
+        if (path.contains(curr)) {
             return false;
         }
-        path.add(currCourse);
+        path.add(curr);
         
-        for (int nextCourse : graph.getOrDefault(currCourse, Collections.emptyList())) {
-            if (!dfs(graph, result, nextCourse, path)) {
+        for (int next : map.getOrDefault(curr, Collections.emptyList())) {
+            if (!dfs(map, next, result, path)) {
                 return false;
             }
         }
         
-        result.add(currCourse);
+        result.add(curr);
         return true;
     }
 }
