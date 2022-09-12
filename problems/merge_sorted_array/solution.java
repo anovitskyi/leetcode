ea79c0fe8b787a389dfe6a0aa84e82b65973183c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-12 07:05:01 +0000

    Sync LeetCode submission - Merge Sorted Array (java)

--- problems/merge_sorted_array/solution.java
+++ problems/merge_sorted_array/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        --m; --n;
-        
+        --m;
+        --n;
         for (int i = nums1.length - 1; i >= 0; --i) {
             if (m < 0) {
-                nums1[i] = nums2[n--];
+                nums1[i] = nums2[n];
+                --n;
             } else if (n < 0) {
-                nums1[i] = nums1[m--];
-            } else if (nums1[m] >= nums2[n]) {
-                nums1[i] = nums1[m--];
+                nums1[i] = nums1[m];
+                --m;
+            } else if (nums1[m] > nums2[n]) {
+                nums1[i] = nums1[m];
+                --m;
             } else {
-                nums1[i] = nums2[n--];
+                nums1[i] = nums2[n];
+                --n;
             }
         }
     }
 }
-
-// 1. Bruteforce solution. O(n) - time and O(n) space. Using two pointers (one for nums1 and another for nums2) on each iteration find minimum element and put it in result array
-
-// 2. Using two pointer store max element on each iteration in the end of nums1 array
