3263e01da1d9c4b01e48574f04fcfab6c95e8d52
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-12 07:00:33 +0000

    Sync LeetCode submission - Two Sum (java)

--- problems/two_sum/solution.java
+++ problems/two_sum/solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        
         for (int i = 0; i < nums.length; ++i) {
-            int diff = target - nums[i];
-            
-            if (map.containsKey(diff)) {
+            if (map.containsKey(target - nums[i])) {
                 return new int[] {
-                    i, map.get(diff)
+                    map.get(target - nums[i]), i
                 };
-            } 
+            }
             
             map.put(nums[i], i);
         }
