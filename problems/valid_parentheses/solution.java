b7e9382d1e72db2838cbb5048c889e064e0ed745
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-19 07:25:34 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- problems/valid_parentheses/solution.java
+++ problems/valid_parentheses/solution.java
@@ -1,27 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        
         for (int i = 0; i < s.length(); ++i) {
-            if (isOpenBracket(s.charAt(i))) {
-                stack.push(s.charAt(i));
-            } else if (stack.isEmpty()) {
-                return false;
-            } else if (isCloseBracket(stack.peek(), s.charAt(i))) {
-                stack.pop();
-            } else {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+                continue;
+            }
+            
+            if (stack.isEmpty()) {
                 return false;
             }
+            
+            char peek = stack.peek();
+            if ((ch == ')' && peek != '(') || (ch == ']' && peek != '[') || (ch == '}' && peek != '{')) {
+                return false;
+            }
+            stack.pop();
         }
         
         return stack.isEmpty();
-    }
-    
-    private boolean isOpenBracket(char ch) {
-        return ch == '(' || ch == '[' || ch == '{';
-    }
-    
-    private boolean isCloseBracket(char peek, char ch) {
-        return (ch == ']' && peek == '[') || (ch == '}' && peek == '{') || (ch == ')' && peek == '(');
     }
 }
