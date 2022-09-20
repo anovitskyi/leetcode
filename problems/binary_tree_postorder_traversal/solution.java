c083c2e6a6610d72f6658da088717dda117c0cd9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-20 10:33:19 +0000

    Sync LeetCode submission - Binary Tree Postorder Traversal (java)

--- problems/binary_tree_postorder_traversal/solution.java
+++ problems/binary_tree_postorder_traversal/solution.java
@@ -1,6 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
         }
@@ -11,23 +26,21 @@ class Solution {
         while (!stack.isEmpty()) {
             TreeNode top = stack.peek();
             
-            if (top.left == null && top.right == null) {
-                result.add(stack.pop().val);
+            if (top.left != null) {
+                stack.push(top.left);
+                top.left = null;
+                continue;
             }
             
             if (top.right != null) {
                 stack.push(top.right);
                 top.right = null;
+                continue;
             }
             
-            if (top.left != null) {
-                stack.push(top.left);
-                top.left = null;
-            }
+            result.add(stack.pop().val);
         }
         
         return result;
     }
-    
-    
 }
