bc013ffd1f8e2d23692c719a31dae6f07fc220f4
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-22 08:24:35 +0000

    Sync LeetCode submission - Path Sum (java)

--- problems/path_sum/solution.java
+++ problems/path_sum/solution.java
@@ -1,29 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
             return false;
         }
         
-        int sum = targetSum - root.val;
-        if (root.left == null && root.right == null) {
-            return sum == 0;
-        } 
+        int newSum = targetSum - root.val;
+        if (root.left == null && root.right == null && newSum == 0) {
+            return true;
+        }
         
-        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        return hasPathSum(root.left, newSum) || hasPathSum(root.right, newSum);
     }
 }
