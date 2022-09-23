370fb6870378d2d593160c08874d988521991236
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-23 06:48:34 +0000

    Sync LeetCode submission - Search in a Binary Search Tree (java)

--- problems/search_in_a_binary_search_tree/solution.java
+++ problems/search_in_a_binary_search_tree/solution.java
@@ -1,36 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == null || node.val == val) {
-                return node;
-            }
-            
-            if (val < node.val) {
-                queue.add(node.left);
-            } else {
-                queue.add(node.right);
-            }
+        if (root == null || root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
         }
-        
-        return null;
     }
 }
