bad8d057cbea849fe9d65a326a91f127bcfcff85
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-09-24 10:11:05 +0000

    Sync LeetCode submission - Path Sum II (java)

--- problems/path_sum_ii/solution.java
+++ problems/path_sum_ii/solution.java
@@ -1,31 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> result = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        
-        if (root != null) {
-            backtrack(root, result, curr, targetSum);
-        }
-        
+        traverse(root, result, curr, targetSum);
         return result;
     }
     
-    private void backtrack(TreeNode node, List<List<Integer>> result, List<Integer> curr, int target) {
-        curr.add(node.val);
-        target -= node.val;
-        
-        if (node.left == null && node.right == null && target == 0) {
-            result.add(new ArrayList<>(curr));
-        } 
-        
-        if (node.left != null) {
-            backtrack(node.left, result, curr, target);
-        } 
-        
-        if (node.right != null) {
-            backtrack(node.right, result, curr, target);
+    private void traverse(TreeNode root, List<List<Integer>> result, List<Integer> curr, int target) {
+        if (root == null) {
+            return;
         }
         
+        if (root.left == null && root.right == null) {
+            if (target == root.val) {
+                List<Integer> copy = new ArrayList<>(curr);
+                copy.add(root.val);
+                result.add(copy);
+            }
+            return;
+        }
+        
+        curr.add(root.val);
+        traverse(root.left, result, curr, target - root.val);
+        traverse(root.right, result, curr, target - root.val);
         curr.remove(curr.size() - 1);
     }
 }
