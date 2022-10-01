20b9162fbc6e4de9570c65fc98dd5488d15f6297
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-10-01 13:00:15 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- problems/palindrome_number/solution.java
+++ problems/palindrome_number/solution.java
@@ -1,18 +1,13 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    public boolean isPalindrome(int x) {  
+        int copy = x;
+        int reverse = 0;
+        
+        while (copy > 0) {
+            reverse = reverse * 10 + copy % 10;
+            copy = copy / 10;
         }
         
-        int origin = x;
-        int copy = 0;
-        
-        while (x != 0) {
-            copy *= 10;
-            copy += x % 10;
-            x /= 10;
-        }
-        
-        return origin == copy;
+        return reverse == x;
     }
 }
