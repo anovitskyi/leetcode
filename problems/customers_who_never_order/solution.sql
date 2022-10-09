3e8ad1647769bd80742dad02a3b53c23ebc2d3dc
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-10-09 09:02:55 +0000

    Sync LeetCode submission - Customers Who Never Order (mysql)

--- problems/customers_who_never_order/solution.sql
+++ problems/customers_who_never_order/solution.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT name AS Customers
-FROM Customers c
-WHERE (SELECT COUNT(customerId) FROM Orders WHERE customerId = c.id) = 0;
+select c.name as Customers
+from Customers c left join Orders o on c.id = o.customerId
+group by c.id
+having count(o.id) = 0 or count(o.id) is null
