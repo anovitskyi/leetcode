2e669116a2117dfd2058b81dcb8e5a371c828d18
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-10-20 10:05:12 +0000

    Sync LeetCode submission - Integer to Roman (java)

--- problems/integer_to_roman/solution.java
+++ problems/integer_to_roman/solution.java
@@ -1,51 +1,46 @@
 class Solution {
     
-    private static final char[] LEVELS = new char[] {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    private static final Map<Integer, Character> map = new HashMap<>();
     
-    public String intToRoman(int num) {
-        StringBuilder builder = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        int level = -2;
-        
-        while (num > 0) {
-            stack.push(num % 10);
-            num /= 10;
-            level += 2;
-        }
-        
-        while (!stack.isEmpty()) {
-            int digit = stack.pop();
-            if (digit != 0) {
-                builder.append(convertIntToRoman(digit, level));
-            }
-            level -= 2;
-        }
-        
-        return builder.toString();
+    static {
+        map.put(1, 'I');
+        map.put(5, 'V');
+        map.put(10, 'X');
+        map.put(50, 'L');
+        map.put(100, 'C');
+        map.put(500, 'D');
+        map.put(1000, 'M');
     }
     
-    private String convertIntToRoman(int digit, int level) {
-        StringBuilder builder = new StringBuilder();
-        
-        if (digit >= 1 && digit <= 3) {
-            while (digit-- > 0) {
-                builder.append(LEVELS[level]);
-            }
-        } else if (digit == 4) {
-            builder.append(LEVELS[level]).append(LEVELS[level + 1]);
-        } else if (digit == 5) {
-            builder.append(LEVELS[level + 1]);
-        } else if (digit >= 5 && digit <= 8) {
-            builder.append(LEVELS[level + 1]);
-            while (digit-- > 5) {
-                builder.append(LEVELS[level]);
-            }
-        } else if (digit == 9) {
-            builder.append(LEVELS[level]).append(LEVELS[level + 2]);
-        } else if (digit == 0) {
-            builder.append(LEVELS[level + 2]);
+    public String intToRoman(int num) {
+        Stack<String> stack = new Stack<>();
+        for (int level = 1; num > 0; level *= 10, num /= 10) {
+            stack.push(convert(num % 10, level));
         }
         
-        return builder.toString();
+        StringBuilder b = new StringBuilder();
+        while (!stack.isEmpty()) {
+            b.append(stack.pop());
+        }
+        return b.toString();
+    }
+    
+    private String convert(int num, int level) {
+        char curr = map.get(level);
+        char half = map.getOrDefault(level * 5, '0');
+        char next = map.getOrDefault(level * 10, '0');
+        
+        switch (num) {
+            case 1: return "" + curr;
+            case 2: return "" + curr + curr;
+            case 3: return "" + curr + curr + curr;
+            case 4: return "" + curr + half;
+            case 5: return "" + half;
+            case 6: return "" + half + curr;
+            case 7: return "" + half + curr + curr;
+            case 8: return "" + half + curr + curr + curr;
+            case 9: return "" + curr + next;
+            default: return "";
+        }
     }
 }
