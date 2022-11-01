35f602b6925d9ab136a7484f43761bdb60946bac
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-11-01 10:25:55 +0000

    Sync LeetCode submission - Where Will the Ball Fall (java)

--- problems/where_will_the_ball_fall/solution.java
+++ problems/where_will_the_ball_fall/solution.java
@@ -3,31 +3,39 @@ class Solution {
         int[] result = new int[grid[0].length];
         
         for (int i = 0; i < result.length; ++i) {
-            result[i] = findLastColumn(grid, 0, i);
+            result[i] = findLastColumn(i, grid);
         }
         
         return result;
     }
     
-    private int findLastColumn(int[][] grid, int x, int y) {
-        while (x < grid.length) {
-            if (grid[x][y] == 1) {
-                if (y + 1 >= grid[0].length || grid[x][y + 1] == -1) {
+    private int findLastColumn(int startColumn, int[][] grid) {
+        int column = startColumn;
+        
+        for (int row = 0; row < grid.length; ++row) {
+            if (grid[row][column] == 1) {
+                if (column == grid[0].length - 1) {
                     return -1;
                 }
                 
-                ++y;
-            } else {
-                if (y == 0 || grid[x][y - 1] == 1) {
+                if (grid[row][column + 1] != 1) {
                     return -1;
                 }
                 
-                --y;
+                ++column;
+            } else if (grid[row][column] == -1) {
+                if (column == 0) {
+                    return -1;
+                }
+                
+                if (grid[row][column - 1] != -1) {
+                    return -1;
+                }
+                
+                --column;
             }
-            
-            ++x;
         }
         
-        return y;
+        return column;
     }
 }
