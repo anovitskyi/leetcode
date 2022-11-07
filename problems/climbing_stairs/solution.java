33365cad839028f8753220e9fabc579621a0ce67
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-11-07 07:49:05 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- problems/climbing_stairs/solution.java
+++ problems/climbing_stairs/solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int prevprev = 0;
-        int prev = 1;
-        
-        for (int i = 1; i <= n; ++i) {
-            int tmp = prev;
-            prev = prev + prevprev;
-            prevprev = tmp;
+        if (n == 1) {
+            return 1;
         }
-        
+
+        int prevprev = 1;
+        int prev = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int sum = prev + prevprev;
+            prevprev = prev;
+            prev = sum;
+        }
+
         return prev;
     }
 }
