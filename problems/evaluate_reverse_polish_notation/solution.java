e59ba4a93d467f0e79141edbfe019d86da56d0a5
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-11-20 16:00:26 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (java)

--- problems/evaluate_reverse_polish_notation/solution.java
+++ problems/evaluate_reverse_polish_notation/solution.java
@@ -1,23 +1,53 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < tokens.length; ++i) {
-            if ("+".equals(tokens[i])) {
-                stack.push(stack.pop() + stack.pop());
-            } else if ("-".equals(tokens[i])) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a - b);
-            } else if ("*".equals(tokens[i])) {
-                stack.push(stack.pop() * stack.pop());
-            } else if ("/".equals(tokens[i])) {
-                int b = stack.pop();
-                int a = stack.pop();
-                stack.push(a / b);
+        Stack<Integer> numbers = new Stack<>();
+
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                numbers.push(convertToNumber(token));
             } else {
-                stack.push(Integer.parseInt(tokens[i]));
+                int second = numbers.pop();
+                int first = numbers.pop();
+                char operation = token.charAt(0);
+
+                int result = eval(first, operation, second);
+
+                numbers.push(result);
             }
         }
-        return stack.pop();
+
+        return numbers.pop();
+    }
+
+    private int convertToNumber(String token) {
+        int index = 0;
+        int result = 0;
+
+        if (token.charAt(0) == '-') {
+            ++index;
+        }
+
+        while (index < token.length()) {
+            int last = token.charAt(index) - '0';
+            result = result * 10 + last;
+            ++index;
+        }
+
+        return token.charAt(0) == '-' ? result * -1 : result;
+    }
+
+    private int eval(int first, char operation, int second) {
+        switch (operation) {
+            case '+': return first + second;
+            case '-': return first - second;
+            case '*': return first * second;
+            case '/': return first / second;
+        }
+
+        return -1;
+    }
+
+    private boolean isNumber(String token) {
+        return Character.isDigit(token.charAt(0)) || (token.length() > 1 && Character.isDigit(token.charAt(1))); 
     }
 }
