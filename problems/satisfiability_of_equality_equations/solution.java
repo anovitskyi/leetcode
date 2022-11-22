42c6e42485c28455074159105bfc6bd0522a481a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-11-22 10:06:11 +0000

    Sync LeetCode submission - Satisfiability of Equality Equations (java)

--- problems/satisfiability_of_equality_equations/solution.java
+++ problems/satisfiability_of_equality_equations/solution.java
@@ -1,56 +1,61 @@
 class Solution {
     public boolean equationsPossible(String[] equations) {
-        UnionFind unionFind = new UnionFind(26);
-        
+        UnionFind uf = new UnionFind(26);
+
         for (String equation : equations) {
-            char[] tab = equation.toCharArray();
-            if (tab[1] == '!') {
+            if (equation.charAt(1) != '=') {
                 continue;
             }
-            
-            unionFind.union(tab[0] - 'a', tab[3] - 'a');
+
+            int first = equation.charAt(0) - 'a';
+            int second = equation.charAt(3) - 'a';
+
+            uf.union(first, second);
         }
-        
+
         for (String equation : equations) {
-            char[] tab = equation.toCharArray();
-            if (tab[1] == '=') {
+            if (equation.charAt(1) == '=') {
                 continue;
             }
-            
-            if (unionFind.find(tab[0] - 'a') == unionFind.find(tab[3] - 'a')) {
+
+            int first = equation.charAt(0) - 'a';
+            int second = equation.charAt(3) - 'a';
+
+            if (uf.findRoot(first) == uf.findRoot(second)) {
                 return false;
             }
         }
-        
+
         return true;
     }
-    
+
     private class UnionFind {
-        private final int[] data;
-        
+        private final int[] elements;
+
         UnionFind(int size) {
-            data = new int[size];
+            elements = new int[size];
             for (int i = 0; i < size; ++i) {
-                data[i] = i;
+                elements[i] = i;
             }
         }
-        
-        void union(int source, int target) {
-            int sourceRoot = find(source);
-            int targetRoot = find(target);
-            
-            for (int i = 0; i < data.length; ++i) {
-                if (data[i] == sourceRoot) {
-                    data[i] = targetRoot;
+
+        void union(int first, int second) {
+            int firstRoot = findRoot(first);
+            int secondRoot = findRoot(second);
+
+            for (int i = 0; i < elements.length; ++i) {
+                if (elements[i] == firstRoot) {
+                    elements[i] = secondRoot;
                 }
             }
         }
-        
-        int find(int target) {
-            while (data[target] != target) {
-                target = data[target];
+
+        int findRoot(int element) {
+            while (elements[element] != element) {
+                element = elements[element];
             }
-            return target;
+
+            return elements[element];
         }
     }
 }
