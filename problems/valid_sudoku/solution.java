b3a260a9755df02abeaca932948a9d622bf2ece3
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-11-23 10:22:17 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- problems/valid_sudoku/solution.java
+++ problems/valid_sudoku/solution.java
@@ -1,131 +1,116 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < board.length; ++i) {
-            if (!isValidRow(board, i)) {
-                return false;
-            }
-        }
-        
-        for (int i = 0; i < board[0].length; ++i) {
-            if (!isValidColumn(board, i)) {
-                return false;
-            }
-        }
-        
-        for (int i = 0; i < 9; ++i) {
-            if (!isValidSquare(board, i)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean isValidRow(char[][] board, int row) {
-        boolean[] visited = new boolean[9];
-        
-        for (int i = 0; i < board[row].length; ++i) {
-            if (!isValidChar(board[row][i], visited)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean isValidColumn(char[][] board, int column) {
-        boolean[] visited = new boolean[9];
-        
-        for (int i = 0; i < board.length; ++i) {
-            if (!isValidChar(board[i][column], visited)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private boolean isValidSquare(char[][] board, int square) {
-        boolean[] visited = new boolean[9];
-        int startRow = square - (square % 3);
-        int endRow = startRow + 3;
-        
-        int startCol = (square % 3) * 3;
-        int endCol = startCol + 3;
-        
-        for (int i = startRow; i < endRow; ++i) {
-            for (int j = startCol; j < endCol; ++j) {
-                if (!isValidChar(board[i][j], visited)) {
+        for (int row = 0; row < 9; ++row) {
+            boolean[] numbers = new boolean[9];
+            for (int col = 0; col < 9; ++col) {
+                char ch = board[row][col];
+                if (ch == '.') {
+                    continue;
+                }
+
+                int num = ch - '0' - 1;
+                if (numbers[num]) {
                     return false;
                 }
+                numbers[num] = true;
             }
         }
-        
-        return true;
-    }
-    
-    private boolean isValidChar(char ch, boolean[] visited) {
-        if (ch == '.') {
-            return true;
+
+        for (int col = 0; col < 9; ++col) {
+            boolean[] numbers = new boolean[9];
+            for (int row = 0; row < 9; ++row) {
+                char ch = board[row][col];
+                if (ch == '.') {
+                    continue;
+                }
+
+                int num = ch - '0' - 1;
+                if (numbers[num]) {
+                    return false;
+                }
+                numbers[num] = true;
+            }
         }
-        
-        if (ch < '1' || ch > '9') {
-            return false;
+
+        for (int block = 0; block < 9; ++block) {
+            boolean[] numbers = new boolean[9];
+            for (int element = 0; element < 9; ++element) {
+                int row = (element / 3) + (3 * (block / 3));
+                int col = (element % 3) + (3 * (block % 3));
+
+                char ch = board[row][col];
+                if (ch == '.') {
+                    continue;
+                }
+
+                int num = ch - '0' - 1;
+                if (numbers[num]) {
+                    return false;
+                }
+                numbers[num] = true;
+            }
         }
-        
-        int num = ch - '0' - 1;
-        if (visited[num]) {
-            return false;
-        }
-        visited[num] = true;
-        
+
         return true;
     }
 }
 
 /*
 
-       0   1   2   3   4   5   6   7   8
-    [[".",".",".",".","5",".",".","1","."], 0
-     [".","4",".","3",".",".",".",".","."], 1
-     [".",".",".",".",".","3",".",".","1"], 2
-     ["8",".",".",".",".",".",".","2","."], 3
-     [".",".","2",".","7",".",".",".","."], 4
-     [".","1","5",".",".",".",".",".","."], 5
-     [".",".",".",".",".","2",".",".","."], 6
-     [".","2",".","9",".",".",".",".","."], 7
-     [".",".","4",".",".",".",".",".","."]] 8
+    block = 0;
+    element = 0;
+    position = [0, 0];
 
-    col
-    0 -> 0
-    1 -> 3
-    2 -> 6
-    3 -> 0
-    4 -> 3
-    5 -> 6
-    6 -> 0
-    7 -> 3
-    8 -> 6
-    
-    row
-    0 -> 0
-    1 -> 0
-    2 -> 0
-    3 -> 3
-    4 -> 3
-    5 -> 3
-    6 -> 6
-    7 -> 6
-    8 -> 6
-    
-       0   1   2   3   4   5   6   7   8
-    [[".","2",".",".",".",".",".",".","."], 0
-     [".",".",".",".",".",".","5",".","1"], 1
-     [".",".",".",".",".",".","8","1","3"], 2
-     ["4",".","9",".",".",".",".",".","."], 3
-     [".",".",".",".",".",".",".",".","."], 4
-     [".",".","2",".",".",".",".",".","."], 5
-     ["7",".","6",".",".",".",".",".","."], 6
-     ["9",".",".",".",".","4",".",".","."], 7
-     [".",".",".",".",".",".",".",".","."]] 8
-*/
+    block = 0;
+    element = 1;
+    position = [0, 1];
+
+    block = 0;
+    element = 2;
+    position = [0, 2];
+
+    block = 0;
+    element = 3;
+    position = [1, 0];
+
+    block = 0;
+    element = 4;
+    position = [1, 1];
+
+    block = 0;
+    element = 5;
+    position = [1, 2];
+
+    block = 0;
+    element = 6;
+    position = [2, 0];
+
+    block = 0;
+    element = 7;
+    position = [2, 1];
+
+    block = 0;
+    element = 8;
+    position = [2, 2];
+
+    block = 1;
+    element = 0;
+    position = [0, 3];
+
+    block = 1;
+    element = 1;
+    position = [0, 4];
+
+    block = 1;
+    element = 2;
+    position = [0, 5]
+
+    block = 1;
+    element = 3;
+    position = [1, 3]
+
+    block = 4;
+    element = 1;
+    position = [3, 4]
+
+ */
