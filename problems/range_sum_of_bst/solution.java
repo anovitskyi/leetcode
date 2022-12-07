273e91789fd2974923111956a97fea7bfd6a9120
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-12-07 08:23:17 +0000

    Sync LeetCode submission - Range Sum of BST (java)

--- problems/range_sum_of_bst/solution.java
+++ problems/range_sum_of_bst/solution.java
@@ -1,12 +1,13 @@
-
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null) {
             return 0;
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
         }
-        
-        return (root.val >= low && root.val <= high ? root.val : 0) 
-            + rangeSumBST(root.left, low, high) 
-            + rangeSumBST(root.right, low, high);
     }
 }
