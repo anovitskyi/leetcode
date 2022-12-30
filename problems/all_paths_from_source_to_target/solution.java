08c0be7add26d594f1a04990667844d13938b49e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2022-12-30 07:16:31 +0000

    Sync LeetCode submission - All Paths From Source to Target (java)

--- problems/all_paths_from_source_to_target/solution.java
+++ problems/all_paths_from_source_to_target/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        curr.add(0);
-        dfs(graph, 0, result, curr);
+        dfs(result, graph, 0, new ArrayList<>());
         return result;
     }
-    
-    private void dfs(int[][] graph, int node, List<List<Integer>> result, List<Integer> curr) {
-        if (node == graph.length - 1) {
-            result.add(new ArrayList<>(curr));
+
+    private void dfs(List<List<Integer>> result, int[][] graph, int index, List<Integer> curr) {
+        curr.add(index);
+
+        if (index == graph.length - 1) {
+            result.add(curr);
             return;
         }
-        
-        for (int neighbour : graph[node]) {
-            curr.add(neighbour);
-            dfs(graph, neighbour, result, curr);
-            curr.remove(curr.size() - 1);
+
+        for (int neighbour : graph[index]) {
+            dfs(result, graph, neighbour, new ArrayList<>(curr));
         }
     }
 }
