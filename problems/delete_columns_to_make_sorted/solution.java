f8269710ea78889ce3826038eafc62554c09b89b
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-01-03 07:59:20 +0000

    Sync LeetCode submission - Delete Columns to Make Sorted (java)

---
+++ problems/delete_columns_to_make_sorted/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int result = 0;
+
+        for (int col = 0; col < strs[0].length(); ++col) {
+            for (int row = 1; row < strs.length; ++row) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    ++result;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
