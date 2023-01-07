75e95403a37d9b87fa8622a11813a67a8a37612f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-01-07 09:22:29 +0000

    Sync LeetCode submission - Gas Station (java)

--- problems/gas_station/solution.java
+++ problems/gas_station/solution.java
@@ -2,21 +2,21 @@ class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int sum = 0;
         int currSum = 0;
-        int result = 0;
-        
-        for (int i = 0; i < gas.length; ++i) { // O(n) time and O(1) space
+        int index = 0;
+
+        for (int i = 0; i < gas.length; ++i) {
             int diff = gas[i] - cost[i];
             sum += diff;
             currSum += diff;
             if (currSum < 0) {
                 currSum = 0;
-                result = i + 1;
+                index = i + 1;
             }
         }
-        
+
         if (sum < 0) {
             return -1;
         }
-        return result;
+        return index;
     }
 }
