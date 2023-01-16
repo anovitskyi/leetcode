c0148788d0e7e7cb3e0d0afe2019794e057a81ec
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-01-16 08:41:04 +0000

    Sync LeetCode submission - Permutation in String (java)

--- problems/permutation_in_string/solution.java
+++ problems/permutation_in_string/solution.java
@@ -3,50 +3,34 @@ class Solution {
         if (s1.length() > s2.length()) {
             return false;
         }
-        
-        Map<Character, Integer> map = new HashMap<>();
+
+        int[] desArr = new int[26];
+        int[] currArr = new int[26];
+
         for (int i = 0; i < s1.length(); ++i) {
-            char ch = s1.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            ++desArr[s1.charAt(i) - 'a'];
+            ++currArr[s2.charAt(i) - 'a'];
         }
-        
-        int foundLetters = 0;
-        for (int i = 0; i < s1.length(); ++i) {
-            char ch = s2.charAt(i);
-            
-            if (map.containsKey(ch)) {
-                int val = map.get(ch);
-                map.put(ch, val - 1);
-                if (val == 1) {
-                    ++foundLetters;
-                }
-            }
-        }
-        
+
         for (int i = s1.length(); i < s2.length(); ++i) {
-            if (foundLetters == map.size()) {
+            if (areSame(desArr, currArr)) {
                 return true;
             }
-            
-            char rightCh = s2.charAt(i);
-            if (map.containsKey(rightCh)) {
-                int val = map.get(rightCh);
-                map.put(rightCh, val - 1);
-                if (val == 1) {
-                    ++foundLetters;
-                }
-            }
-            
-            char leftCh = s2.charAt(i - s1.length());
-            if (map.containsKey(leftCh)) {
-                int val = map.get(leftCh);
-                map.put(leftCh, val + 1);
-                if (val == 0) {
-                    --foundLetters;
-                }
+
+            --currArr[s2.charAt(i - s1.length()) - 'a'];
+            ++currArr[s2.charAt(i) - 'a'];
+        }
+
+        return areSame(desArr, currArr);
+    }
+
+    private boolean areSame(int[] source, int[] target) {
+        for (int i = 0; i < source.length; ++i) {
+            if (source[i] != target[i]) {
+                return false;
             }
         }
-        
-        return foundLetters == map.size();
+
+        return true;
     }
 }
