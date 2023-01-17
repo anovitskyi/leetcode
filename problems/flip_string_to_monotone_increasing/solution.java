dd91d00f086d8d56ea8e956fd7f1bb155750283f
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-01-17 16:39:45 +0000

    Sync LeetCode submission - Flip String to Monotone Increasing (java)

---
+++ problems/flip_string_to_monotone_increasing/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int ans = 0;
+        int noOfFlip = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                ans = Math.min(noOfFlip, ans + 1);
+            } else {
+                noOfFlip++;
+            }
+        }
+
+        return ans;
+    }
+}
