945b9efa0d245037241cca35c4541d68fc6fb0f9
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-01-28 09:50:43 +0000

    Sync LeetCode submission - Binary Search (java)

--- problems/binary_search/solution.java
+++ problems/binary_search/solution.java
@@ -2,19 +2,19 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        
+
         while (left <= right) {
-            int middle = left + (right - left) / 2;
-            
-            if (nums[middle] == target) {
-                return middle;
-            } else if (nums[middle] > target) {
-                right = middle - 1;
+            int mid = (right - left) / 2 + left;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                left = middle + 1;
+                left = mid + 1;
             }
         }
-        
+
         return -1;
     }
 }
