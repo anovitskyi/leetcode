9ed801380e00e9624d45bbaecbbc0dd9384073d0
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-06 07:43:23 +0000

    Sync LeetCode submission - Reorder List (java)

--- problems/reorder_list/solution.java
+++ problems/reorder_list/solution.java
@@ -1,37 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public void reorderList(ListNode head) {
-        Stack<ListNode> stack = getStack(head);
-        int numberOfOperations = stack.size() / 2;
-        ListNode tmp = head;
-    
-        for (int i = 0; i < numberOfOperations; ++i) {
-            ListNode next = tmp.next;
-            ListNode stackNode = stack.pop();
-            
-            stackNode.next = next;
-            tmp.next = stackNode;
-            tmp = next;
-        }
-        
-        tmp.next = null;
-    }
-    
-    private Stack<ListNode> getStack(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
-        while (head != null) {
-            stack.push(head);
-            head = head.next;
-        }        
-        return stack;
+        ListNode stackHead = head;
+        while (stackHead != null) {
+            stack.push(stackHead);
+            stackHead = stackHead.next;
+        }
+        int size = stack.size();
+
+        ListNode copy = head;
+        for (int i = 0; i < size / 2; ++i) {
+            ListNode tmp = copy.next;
+            copy.next = stack.pop();
+            copy = copy.next;
+            copy.next = tmp;
+            copy = copy.next;
+        }
+
+        copy.next = null;
     }
 }
