fb3a2e19d86da7c0cca583cd16b55c82f4038163
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-09 07:19:21 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- problems/add_two_numbers/solution.java
+++ problems/add_two_numbers/solution.java
@@ -1,43 +1,39 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        ListNode curr = result;
-        int carrying = 0;
-        
-        while (l1 != null || l2 != null) {
-            int l1val = 0;
-            if (l1 != null) {
-                l1val = l1.val;
-                l1 = l1.next;
-            }
-            
-            int l2val = 0;
-            if (l2 != null) {
-                l2val = l2.val;
-                l2 = l2.next;
-            }
-            
-            int val = l1val + l2val + carrying;
-            carrying = val / 10;
-            ListNode tmp = new ListNode(val % 10);
-            curr.next = tmp;
-            curr = tmp;
+        ListNode fictiveHead = new ListNode(0);
+        ListNode head = fictiveHead;
+        int carry = 0;
+
+        while (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+            carry = sum / 10;
+
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        
-        if (carrying != 0) {
-            curr.next = new ListNode(carrying);
+
+        while (l1 != null) {
+            int sum = l1.val + carry;
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+            carry = sum / 10;
+            l1 = l1.next;
         }
-        
-        return result.next;
+
+        while (l2 != null) {
+            int sum = l2.val + carry;
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+            carry = sum / 10;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            head.next = new ListNode(carry);
+        }
+
+        return fictiveHead.next;
     }
 }
