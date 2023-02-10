25020d42e04c4995ea9b41d9cc0b34e42836c43c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-10 11:42:09 +0000

    Sync LeetCode submission - LRU Cache (java)

--- problems/lru_cache/solution.java
+++ problems/lru_cache/solution.java
@@ -1,84 +1,83 @@
 class LRUCache {
 
-    int capacity;
-    int size;
-    // head and tail of the double linked list
-    Node head;
-    Node tail;
-    // hashmap, from key to Node
-    Map<Integer, Node> keyToNode;
+    private final int capacity;
+    private final Map<Integer, ListNode> map;
+    private final ListNode head;
+
+    private int size;
+    private ListNode tail;
+
     public LRUCache(int capacity) {
-        this.capacity = capacity;
         this.size = 0;
-        // initialize head and tail to be dummy non-null nodes
-        this.head = new Node(0, 0);
-        this.tail = new Node(0, 0);
-        this.head.next = this.tail;
-        this.tail.prev = this.head;
-        this.keyToNode = new HashMap<>();
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.head = new ListNode();
+        this.tail = this.head;
+    }
+
+    private boolean containsKey(int key) {
+        return map.containsKey(key) && map.get(key) != null;
     }
     
     public int get(int key) {
-        if (this.keyToNode.containsKey(key)) {
-            Node node = this.keyToNode.get(key);
-            deleteNode(node);
-            insertAtHead(node);
-            
-            return node.val;
+        if (!containsKey(key)) {
+            return -1;
         }
-        
-        return -1;
+
+        int val = map.get(key).val;
+        put(key, val);
+        return val;
     }
     
     public void put(int key, int value) {
-        if (this.keyToNode.containsKey(key)) {
-            Node node = this.keyToNode.get(key);
-            deleteNode(node);
+        if (containsKey(key)) {
+            ListNode tmp = map.get(key);
+            tmp.prev.next = tmp.next;
+            if (tmp.next != null) {
+                tmp.next.prev = tmp.prev;
+            }
+            if (tail == tmp) {
+                tail = tail.prev;
+            }
+            --size;
         }
-        else if (this.size == this.capacity) {
-            Node node = this.tail.prev;
-            deleteNode(node);
-            // do not forget to remove the entry from the hashmap
-            this.keyToNode.remove(node.key); 
+
+        ListNode node = new ListNode(key, value);
+        map.put(key, node);
+        node.prev = tail;
+        tail.next = node;
+        tail = tail.next;
+        ++size;
+
+        if (size > capacity) {
+            ListNode tmp = head.next;
+            map.put(tmp.key, null);
+            tmp.prev.next = tmp.next;
+            if (tmp.next != null) {
+                tmp.next.prev = tmp.prev;
+            }
+            if (tail == tmp) {
+                tail = tmp.prev;
+            }
+            --size;
         }
-        Node node = new Node(key, value);
-        insertAtHead(node);
-        this.keyToNode.put(key, node);
     }
-    
-    // function method to delete a node from the doubly linkedlist
-    private void deleteNode(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        this.size--;
-    }
-    
-    
-    // insert a node at the head of the linkedlist
-    private void insertAtHead(Node node) {
-        node.next = this.head.next;
-        this.head.next.prev = node;
-        this.head.next = node;
-        node.prev = this.head;
-        this.size++;
-    }
-    
-    // inner class, node for a doubly-linkedlist
-    private static class Node {
+
+    private static class ListNode {
         int key;
         int val;
-        Node prev;
-        Node next;
-        public Node(int key, int val) {
+        ListNode prev;
+        ListNode next;
+
+        ListNode() {
+            this(0, 0);
+        }
+
+        ListNode(int key, int val) {
             this.key = key;
             this.val = val;
+            this.prev = null;
+            this.next = null;
         }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
