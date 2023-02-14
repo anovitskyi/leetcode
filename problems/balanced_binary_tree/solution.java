ce4c9936d2f98577df964bca70fbbc845b10ca87
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-14 07:29:28 +0000

    Sync LeetCode submission - Balanced Binary Tree (java)

--- problems/balanced_binary_tree/solution.java
+++ problems/balanced_binary_tree/solution.java
@@ -1,42 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) {
             return true;
         }
-        
-        int left = 0;
-        if (root.left != null) {
-            if (!isBalanced(root.left)) {
-                return false;
-            }
-            left = root.left.val;
+
+        if (!isBalanced(root.left) || !isBalanced(root.right)) {
+            return false;
         }
-        
-        int right = 0;
-        if (root.right != null) {
-            if (!isBalanced(root.right)) {
-                return false;
-            }
-            right = root.right.val;
-        }
-        
-        root.val = Math.max(left, right) + 1;
-        
-        return Math.abs(left - right) < 2;
+
+        int left = root.left != null ? root.left.val + 1: 0;
+        int right = root.right != null ? root.right.val + 1: 0;
+        root.val = Math.max(left, right); 
+
+        return Math.abs(left - right) <= 1;
     }
 }
