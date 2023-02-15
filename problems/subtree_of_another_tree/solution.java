6e3b012894770c99da7f2a687729df183f4b688a
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-15 07:25:38 +0000

    Sync LeetCode submission - Subtree of Another Tree (java)

--- problems/subtree_of_another_tree/solution.java
+++ problems/subtree_of_another_tree/solution.java
@@ -1,40 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null || subRoot == null) {
-            return root == subRoot;
+        if (root == null) {
+            return false;
         }
-        
-        if (areEqual(root, subRoot)) {
+
+        if (root.val == subRoot.val && areSame(root, subRoot)) {
             return true;
         }
-        
+
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    
-    private boolean areEqual(TreeNode root, TreeNode subRoot) {
-        if (root == null || subRoot == null) {
-            return root == subRoot;
-        } 
-        
+
+    private boolean areSame(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return subRoot == null;
+        }
+
+        if (subRoot == null) {
+            return false;
+        }
+
         if (root.val != subRoot.val) {
             return false;
         }
-        
-        return areEqual(root.left, subRoot.left) && areEqual(root.right, subRoot.right);
+
+        return areSame(root.left, subRoot.left) && areSame(root.right, subRoot.right);
     }
 }
