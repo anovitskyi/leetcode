57b58760c51a164641a6b1ec34c6627870415f13
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-16 07:04:27 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- problems/binary_tree_level_order_traversal/solution.java
+++ problems/binary_tree_level_order_traversal/solution.java
@@ -1,47 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-            
-            while (size-- > 0) {
-                TreeNode top = queue.poll();
-                level.add(top.val);
-                
-                if (top.left != null) {
-                    queue.offer(top.left);
-                }
-                
-                if (top.right != null) {
-                    queue.offer(top.right);
-                }
-            }
-            
-            result.add(level);
-        }
-        
+        traverse(root, 0, result);
         return result;
+    }
+
+    private void traverse(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+
+        List<Integer> levelList = null;
+        if (level >= result.size()) {
+            levelList = new ArrayList<>();
+            result.add(levelList);
+        } else {
+            levelList = result.get(level);
+        }
+
+        levelList.add(root.val);
+
+        traverse(root.left, level + 1, result);
+        traverse(root.right, level + 1, result);
     }
 }
