7f08fc893da7fe0693db5734c0b6b1b16deb7e4c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-17 07:23:18 +0000

    Sync LeetCode submission - Count Good Nodes in Binary Tree (java)

--- problems/count_good_nodes_in_binary_tree/solution.java
+++ problems/count_good_nodes_in_binary_tree/solution.java
@@ -1,50 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int goodNodes(TreeNode root) {
-        int result = 1;
-        
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        
-        while (!stack.empty()) {
-            TreeNode last = stack.pop();
-            
-            if (last.left != null) {
-                if (last.left.val >= last.val) {
-                    ++result;
-                } else {
-                    last.left.val = last.val;
-                }
-                
-                stack.push(last.left);
-            }
-            
-            if (last.right != null) {
-                if (last.right.val >= last.val) {
-                    ++result;
-                } else {
-                    last.right.val = last.val;
-                }
-                
-                stack.push(last.right);
-            }
+        return countGoodNodes(root, root.val);
+    }
+
+    private int countGoodNodes(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
         }
-        
-        
-        return result;
+
+        int counter = root.val >= max ? 1 : 0;
+        int newMax = Math.max(root.val, max);
+
+        return countGoodNodes(root.left, newMax) + countGoodNodes(root.right, newMax) + counter;
     }
 }
