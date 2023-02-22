3f49639b1a49b2f5ad863a6036a7604dcf7c98dc
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-22 08:16:14 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (java)

--- problems/binary_tree_maximum_path_sum/solution.java
+++ problems/binary_tree_maximum_path_sum/solution.java
@@ -1,27 +1,41 @@
 class Solution {
-    
-    public int maxPathSum(TreeNode root) {
-        int[] sum = { Integer.MIN_VALUE };
-        dfs(root, sum);
-        return sum[0];
-    }
 
-    private int dfs(TreeNode node, int[] sum) {
-        if (node==null) {
-            return 0;
+    public int maxPathSum(TreeNode root) {
+        int traverseResult = traverse(root);
+        int rootVal = root.val;
+
+        return Math.max(rootVal, traverseResult);
+    }
+    
+    public int traverse(TreeNode root) {
+        if (root == null) {
+            return -1001;
         }
 
-        int left = dfs(node.left, sum);
-        int right = dfs(node.right, sum);
+        int leftPath = traverse(root.left);
+        int rightPath = traverse(root.right);
+        int path = root.val;
 
-        int nodeWithMaxChild = Math.max(left, right) + node.val;
-        int nodeWithoutChildren = node.val;
-        int nodeWithChildren = node.val + left + right;
+        if (root.val + leftPath > path) {
+            path = root.val + leftPath;
+        }
+        if (root.val + rightPath > path) {
+            path = root.val + rightPath;
+        }
 
-        int case1 = Math.max(nodeWithMaxChild, nodeWithoutChildren);
-        int case2 = Math.max(case1, nodeWithChildren);
-        
-        sum[0] = Math.max(sum[0], case2);
-        return case1;
+        if (root.val + leftPath + rightPath > root.val) {
+            root.val = root.val + leftPath + rightPath;
+        }
+        if (root.left != null && root.left.val > root.val) {
+            root.val = root.left.val;
+        } 
+        if (root.right != null && root.right.val > root.val) {
+            root.val = root.right.val;
+        }
+        if (path > root.val) {
+            root.val = path;
+        }
+
+        return path;
     }
 }
