edd629d7475674f64636cb3a8c737813cb02945d
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-02-26 15:54:13 +0000

    Sync LeetCode submission - Word Search II (java)

--- problems/word_search_ii/solution.java
+++ problems/word_search_ii/solution.java
@@ -1,60 +1,69 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        Trie root = new Trie();
-        for (String word : words) {
-            Trie curr = root;
-
-            for (int i = 0; i < word.length(); ++i) {
-                curr = curr.children.computeIfAbsent(word.charAt(i), x -> new Trie());
-            }
-
-            curr.word = word;
-        }
+        TrieNode root = convertWordsToTrie(words);
 
         Set<String> set = new HashSet<>();
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[i].length; ++j) {
-                findWord(board, i, j, root, set);
+                checkWord(board, i, j, root, set);
             }
         }
 
-        List<String> result = new ArrayList<>();
-        for (String word : set) {
-            result.add(word);
+        List<String> result = new ArrayList<>(set.size());
+        for (String str : set) {
+            result.add(str);
         }
         return result;
     }
 
-    private void findWord(char[][] board, int i, int j, Trie trie, Set<String> set) {
-        if (i < 0 || i >= board.length) {
+    private void checkWord(char[][] board, int i, int j, TrieNode node, Set<String> set) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] == '\0') {
             return;
         }
 
-        if (j < 0 || j >= board[i].length) {
+        char ch = board[i][j];
+        if (!node.children.containsKey(ch)) {
             return;
         }
 
-        if (!trie.children.containsKey(board[i][j])) {
-            return;
+        node = node.children.get(ch);
+        if (node.isEndOfWord) {
+            set.add(node.word);
         }
 
-        Trie next = trie.children.get(board[i][j]);
-
-        if (next.word != null) {
-            set.add(next.word);
-        }
-
-        char tmp = board[i][j];
-        board[i][j] = '0';
-        findWord(board, i - 1, j, next, set);
-        findWord(board, i + 1, j, next, set);
-        findWord(board, i, j - 1, next, set);
-        findWord(board, i, j + 1, next, set);
-        board[i][j] = tmp;
+        board[i][j] = '\0';
+        checkWord(board, i - 1, j, node, set);
+        checkWord(board, i + 1, j, node, set);
+        checkWord(board, i, j - 1, node, set);
+        checkWord(board, i, j + 1, node, set);
+        board[i][j] = ch;
     }
 
-    private class Trie {
-        final Map<Character, Trie> children = new HashMap<>();
-        String word = null;
+    private TrieNode convertWordsToTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            addWordToTrie(word, root);
+        }
+        return root;
+    }
+
+    private void addWordToTrie(String word, TrieNode node) {
+        if (word == null || word.isEmpty()) {
+            return;
+        }
+
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            node = node.children.computeIfAbsent(ch, x -> new TrieNode());
+        }
+
+        node.isEndOfWord = true;
+        node.word = word;
+    }
+
+    private static class TrieNode {
+        private final Map<Character, TrieNode> children = new HashMap<>();
+        private boolean isEndOfWord = false;
+        private String word = null;
     }
 }
