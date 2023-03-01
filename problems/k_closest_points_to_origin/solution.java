637426220e8420f55348982004fe5cdc4bf3f264
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-01 06:43:44 +0000

    Sync LeetCode submission - K Closest Points to Origin (java)

--- problems/k_closest_points_to_origin/solution.java
+++ problems/k_closest_points_to_origin/solution.java
@@ -1,15 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> Double.compare(distance(a), distance(b)));
-        
+        Queue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            double aDist = Math.sqrt((a[0] * a[0]) + (a[1] * a[1]));
+            double bDist = Math.sqrt((b[0] * b[0]) + (b[1] * b[1]));
+
+            return Double.compare(bDist, aDist);
+        });
+
+        for (int[] point : points) {
+            heap.offer(point);
+
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
         int[][] result = new int[k][2];
-        for (int i = 0; i < k; ++i) {
-            result[i] = points[i];
+        while (k > 0) {
+            result[--k] = heap.poll();
         }
         return result;
-    }
-    
-    private double distance(int[] point) {
-        return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
     }
 }
