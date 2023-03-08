ef3ca2c87b0afa978c5f8af2c60d6c699a526619
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-08 06:55:35 +0000

    Sync LeetCode submission - Permutations (java)

--- problems/permutations/solution.java
+++ problems/permutations/solution.java
@@ -1,24 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        List<Integer> curr = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        
+        backtrack(nums, visited, result, curr);
+        
         return result;
     }
-    
-    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] nums, boolean[] visited) {
+
+    private void backtrack(int[] nums, boolean[] visited, List<List<Integer>> result, List<Integer> curr) {
         if (curr.size() == nums.length) {
             result.add(new ArrayList<>(curr));
             return;
         }
-        
+
         for (int i = 0; i < nums.length; ++i) {
             if (visited[i]) {
                 continue;
             }
-            
+
             visited[i] = true;
             curr.add(nums[i]);
-            backtrack(result, curr, nums, visited);
+            backtrack(nums, visited, result, curr);
             curr.remove(curr.size() - 1);
             visited[i] = false;
         }
