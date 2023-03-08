2709dcd1c331830e3cc4dc13a6ec760bf147b7f7
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-08 07:22:17 +0000

    Sync LeetCode submission - Combination Sum II (java)

--- problems/combination_sum_ii/solution.java
+++ problems/combination_sum_ii/solution.java
@@ -2,26 +2,34 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), candidates, 0, target);
+        List<Integer> curr = new ArrayList<>();
+
+        backtrack(candidates, target, 0, result, curr);
+
         return result;
     }
-    
-    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] candidates, int index, int target) {
+
+    private void backtrack(int[] candidates, int target, int index, List<List<Integer>> result, List<Integer> curr) {
         if (target <= 0) {
             if (target == 0) {
                 result.add(new ArrayList<>(curr));
             }
             return;
         }
-        
+
         for (int i = index; i < candidates.length; ++i) {
             if (i > index && candidates[i] == candidates[i - 1]) {
                 continue;
             }
-            
+
             curr.add(candidates[i]);
-            backtrack(result, curr, candidates, i + 1, target - candidates[i]);
+            backtrack(candidates, target - candidates[i], i + 1, result, curr);
             curr.remove(curr.size() - 1);
         }
     }
 }
+
+/*
+    [10,1,2,7,6,1,5]
+    [1,1,2,5,6,7,10]
+ */
