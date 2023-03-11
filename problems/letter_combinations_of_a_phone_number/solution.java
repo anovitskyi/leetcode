ecf25219f50dae1e7fb0409f65c45e373c425ede
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-11 06:49:59 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- problems/letter_combinations_of_a_phone_number/solution.java
+++ problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,36 +1,39 @@
 class Solution {
-    
-    private static final Map<Character, String> map = new HashMap<>();
-    
-    static {
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
-    }
-    
+    private static final Map<Character, List<Character>> numbers = Map.of(
+        '2', List.of('a', 'b', 'c'),
+        '3', List.of('d', 'e', 'f'),
+        '4', List.of('g', 'h', 'i'),
+        '5', List.of('j', 'k', 'l'),
+        '6', List.of('m', 'n', 'o'),
+        '7', List.of('p', 'q', 'r', 's'),
+        '8', List.of('t', 'u', 'v'),
+        '9', List.of('w', 'x', 'y', 'z')
+    );
+
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
         if (digits.isEmpty()) {
-            return result;
-        }
-        backtrack(result, "", digits, 0);
-        return result;
-    }
-    
-    private void backtrack(List<String> result, String curr, String digits, int digitIndex) {
-        if (digitIndex >= digits.length()) {
-            result.add(curr);
-            return;
+            return Collections.emptyList();
         }
         
-        String letters = map.get(digits.charAt(digitIndex));
-        for (int i = 0; i < letters.length(); ++i) {
-            backtrack(result, curr + letters.charAt(i), digits, digitIndex + 1);
+        List<String> result = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+
+        backtrack(digits, 0, result, builder);
+        
+        return result;
+    }
+
+    private void backtrack(String digits, int index, List<String> result, StringBuilder builder) {
+        if (index >= digits.length()) {
+            result.add(builder.toString());
+            return;
+        }
+
+        List<Character> possibleLetters = numbers.getOrDefault(digits.charAt(index), Collections.emptyList());
+        for (char letter : possibleLetters) {
+            builder.append(letter);
+            backtrack(digits, index + 1, result, builder);
+            builder.deleteCharAt(builder.length() - 1);
         }
     }
 }
