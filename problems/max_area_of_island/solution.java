bc614560ac5ff6aca3a81b006f1d32c4eb962d49
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-15 07:09:14 +0000

    Sync LeetCode submission - Max Area of Island (java)

--- problems/max_area_of_island/solution.java
+++ problems/max_area_of_island/solution.java
@@ -1,33 +1,29 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int result = 0;
-        
+        int maxIslandArea = 0;
+
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
                 if (grid[i][j] == 1) {
-                    result = Math.max(result, calculateIslandArea(grid, i, j));
+                    int islandArea = visitIsland(grid, i, j);
+
+                    if (islandArea > maxIslandArea) {
+                        maxIslandArea = islandArea;
+                    }
                 }
             }
         }
-        
-        return result;
+
+        return maxIslandArea;
     }
-    
-    private int calculateIslandArea(int[][] grid, int x, int y) {
-        if (x < 0 || x >= grid.length) {
+
+    private int visitIsland(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[x].length || grid[x][y] == 0) {
             return 0;
         }
-        
-        if (y < 0 || y >= grid[x].length) {
-            return 0;
-        }
-        
-        if (grid[x][y] == 0) {
-            return 0;
-        }
-        
+
         grid[x][y] = 0;
-        
-        return 1 + calculateIslandArea(grid, x - 1, y) + calculateIslandArea(grid, x + 1, y) + calculateIslandArea(grid, x, y - 1) + calculateIslandArea(grid, x, y + 1);
+
+        return 1 + visitIsland(grid, x - 1, y) + visitIsland(grid, x + 1, y) + visitIsland(grid, x, y - 1) + visitIsland(grid, x, y + 1);
     }
 }
