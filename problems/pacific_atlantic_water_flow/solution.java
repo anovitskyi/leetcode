8d03f3ae5b56d1db1e40b6eb6b257f9e3f26258e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-16 07:46:17 +0000

    Sync LeetCode submission - Pacific Atlantic Water Flow (java)

--- problems/pacific_atlantic_water_flow/solution.java
+++ problems/pacific_atlantic_water_flow/solution.java
@@ -1,73 +1,107 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        boolean[][] pacific = findPacificMountains(heights);
-        boolean[][] atlantic = findAtlanticMountains(heights);
-        
+        Set<Point> pacificPoints = findPacificPoints(heights);
+        Set<Point> atlanticPoints = findAtlanticPoints(heights);
+
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < heights.length; ++i) {
-            for (int j = 0; j < heights[i].length; ++j) {
-                if (pacific[i][j] && atlantic[i][j]) {
-                    result.add(List.of(i, j));
-                }
-            }
-        }
-        return result;
-    }
-    
-    private boolean[][] findPacificMountains(int[][] heights) {
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < heights[0].length; ++i) {
-            queue.offer(new int[] {0, i});
-        }
-        for (int i = 1; i < heights.length; ++i) {
-            queue.offer(new int[] {i, 0});
-        }
-        return populateTab(heights, queue);
-    }
-    
-    private boolean[][] findAtlanticMountains(int[][] heights) {
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < heights[heights.length - 1].length; ++i) {
-            queue.offer(new int[] {heights.length - 1, i});
-        }
-        for (int i = 0; i < heights.length - 1; ++i) {
-            queue.offer(new int[] {i, heights[i].length - 1});
-        }
-        return populateTab(heights, queue);
-    }
-    
-    private boolean[][] populateTab(int[][] heights, Queue<int[]> queue) {
-        boolean[][] result = new boolean[heights.length][heights[0].length];
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int x = pos[0];
-            int y = pos[1];
-            
-            if (result[x][y]) {
+        for (Point pacificPoint : pacificPoints) {
+            if (!atlanticPoints.contains(pacificPoint)) {
                 continue;
             }
-            result[x][y] = true;
-            
-            if (x - 1 >= 0 && heights[x - 1][y] >= heights[x][y]) {
-                result[x][y] = true;
-                queue.offer(new int[] {x - 1, y});
-            }
-            
-            if (x + 1 < heights.length && heights[x + 1][y] >= heights[x][y]) {
-                result[x][y] = true;
-                queue.offer(new int[] {x + 1, y}); 
-            }
-            
-            if (y - 1 >= 0 && heights[x][y - 1] >= heights[x][y]) {
-                result[x][y] = true;
-                queue.offer(new int[] {x, y - 1});
-            }
-            
-            if (y + 1 < heights[x].length && heights[x][y + 1] >= heights[x][y]) {
-                result[x][y] = true;
-                queue.offer(new int[] {x, y + 1});
-            }
+
+            result.add(List.of(pacificPoint.x, pacificPoint.y));
         }
         return result;
+    }
+
+    private Set<Point> findPacificPoints(int[][] heights) {
+        Set<Point> pacificPoints = new HashSet<>();
+        Queue<Point> queue = new LinkedList<>();
+        
+        queue.offer(new Point(0, 0));
+        for (int x = 1; x < heights.length; ++x) {
+            queue.offer(new Point(x, 0));
+        }
+        for (int y = 1; y < heights[0].length; ++y) {
+            queue.offer(new Point(0, y));
+        }
+
+        traverseIsland(heights, queue, pacificPoints);
+
+        return pacificPoints;
+    }
+
+    private Set<Point> findAtlanticPoints(int[][] heights) {
+        Set<Point> atlanticPoints = new HashSet<>();
+        Queue<Point> queue = new LinkedList<>();
+
+        int xLast = heights.length - 1;
+        int yLast = heights[0].length - 1;
+        
+        queue.offer(new Point(xLast, yLast));
+        for (int x = xLast - 1; x >= 0; --x) {
+            queue.offer(new Point(x, yLast));
+        }
+        for (int y = yLast - 1; y >= 0; --y) {
+            queue.offer(new Point(xLast, y));
+        }
+
+        traverseIsland(heights, queue, atlanticPoints);
+
+        return atlanticPoints;
+    }
+
+    private void traverseIsland(int[][] heights, Queue<Point> queue, Set<Point> visitedPoints) {
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            if (visitedPoints.contains(point)) {
+                continue;
+            }
+            visitedPoints.add(point);
+
+            int x = point.x;
+            int y = point.y;
+
+            if (x - 1 >= 0 && heights[x - 1][y] >= heights[x][y]) {
+                queue.offer(new Point(x - 1, y));
+            }
+
+            if (x + 1 < heights.length && heights[x + 1][y] >= heights[x][y]) {
+                queue.offer(new Point(x + 1, y));
+            }
+
+            if (y - 1 >= 0 && heights[x][y - 1] >= heights[x][y]) {
+                queue.offer(new Point(x, y - 1));
+            }
+
+            if (y + 1 < heights[x].length && heights[x][y + 1] >= heights[x][y]) {
+                queue.offer(new Point(x, y + 1));
+            }
+        }
+    }
+
+    private static class Point {
+        final int x;
+        final int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            Point oPoint = (Point)other;
+            return this.x == oPoint.x && this.y == oPoint.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        public String toString() {
+            return "[" + x + ", " + y + "]";
+        }
     }
 }
