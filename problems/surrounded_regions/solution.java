958cdb27aec86ccc5545d1c2cf89ff8e784755f8
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-17 07:59:46 +0000

    Sync LeetCode submission - Surrounded Regions (java)

--- problems/surrounded_regions/solution.java
+++ problems/surrounded_regions/solution.java
@@ -1,58 +1,45 @@
 class Solution {
+
+    private static final char WATER = 'X';
+    private static final char LAND = 'O';
+    private static final char BORDER_LAND = 'Q';
+
     public void solve(char[][] board) {
-        for (int col = 0; col < board[0].length; ++col) {
-            if (board[0][col] == 'O') {
-                visitBorderRegion(board, 0, col);
-            }
+        for (int x = 0; x < board.length; ++x) {
+            visitBorderLand(board, x, 0);
+            visitBorderLand(board, x, board[x].length - 1);
         }
-        
-        for (int col = 0; col < board[0].length; ++col) {
-            if (board[board.length - 1][col] == 'O') {
-                visitBorderRegion(board, board.length - 1, col);
-            }
+
+        for (int y = 0; y < board[0].length; ++y) {
+            visitBorderLand(board, 0, y);
+            visitBorderLand(board, board.length - 1, y);
         }
-        
-        for (int row = 0; row < board.length; ++row) {
-            if (board[row][0] == 'O') {
-                visitBorderRegion(board, row, 0);
-            }
-        }
-        
-        for (int row = 0; row < board.length; ++row) {
-            if (board[row][board[0].length - 1] == 'O') {
-                visitBorderRegion(board, row, board[0].length - 1);
-            }
-        }
-        
-        for (int row = 0; row < board.length; ++row) {
-            for (int col = 0; col < board[row].length; ++col) {
-                if (board[row][col] == 'O') {
-                    board[row][col] = 'X';
-                } else if (board[row][col] == 'Q') {
-                    board[row][col] = 'O';
+
+        for (int x = 0; x < board.length; ++x) {
+            for (int y = 0; y < board[x].length; ++y) {
+                if (board[x][y] == BORDER_LAND) {
+                    board[x][y] = LAND;
+                } else if (board[x][y] == LAND) {
+                    board[x][y] = WATER;
                 }
             }
         }
     }
-    
-    private void visitBorderRegion(char[][] board, int x, int y) {
-        if (x < 0 || x >= board.length) {
+
+    private void visitBorderLand(char[][] board, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[x].length) {
             return;
         }
-        
-        if (y < 0 || y >= board[x].length) {
+
+        if (board[x][y] == BORDER_LAND || board[x][y] == WATER) {
             return;
         }
-        
-        if (board[x][y] != 'O') {
-            return;
-        }
-        
-        board[x][y] = 'Q';
-        
-        visitBorderRegion(board, x - 1, y);
-        visitBorderRegion(board, x + 1, y);
-        visitBorderRegion(board, x, y - 1);
-        visitBorderRegion(board, x, y + 1);
+
+        board[x][y] = BORDER_LAND;
+
+        visitBorderLand(board, x - 1, y);
+        visitBorderLand(board, x + 1, y);
+        visitBorderLand(board, x, y - 1);
+        visitBorderLand(board, x, y + 1);
     }
 }
