195aad3cfb2e8979fee449e98dfa3fc7eb648728
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-03-18 07:59:14 +0000

    Sync LeetCode submission - Rotting Oranges (java)

--- problems/rotting_oranges/solution.java
+++ problems/rotting_oranges/solution.java
@@ -1,66 +1,60 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int result = 0;
-        Queue<int[]> rottenOranges = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         int freshOranges = 0;
-        
+
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
                 if (grid[i][j] == 0) {
                     continue;
                 } else if (grid[i][j] == 1) {
                     ++freshOranges;
-                } else {
-                    rottenOranges.offer(new int[] {i, j});
+                } else if (grid[i][j] == 2) {
+                    queue.offer(new int[] {i, j});
                 }
             }
         }
-        
-        while (freshOranges > 0 && !rottenOranges.isEmpty()) {
-            int before = freshOranges;
-            int size = rottenOranges.size();
-            
+
+        int minutes = 0;
+        while (!queue.isEmpty() && freshOranges > 0) {
+            int size = queue.size();
+
             while (size-- > 0) {
-                int[] poll = rottenOranges.poll();
-                int x = poll[0];
-                int y = poll[1];
-                
+                int[] point = queue.poll();
+                int x = point[0];
+                int y = point[1];
+
                 if (x - 1 >= 0 && grid[x - 1][y] == 1) {
-                    --freshOranges;
                     grid[x - 1][y] = 2;
-                    rottenOranges.offer(new int[] {x - 1, y});
-                }
-                
-                if (x + 1 < grid.length && grid[x + 1][y] == 1) {
                     --freshOranges;
+                    queue.offer(new int[] {x - 1, y});
+                }
+
+                if (x + 1 < grid.length && grid[x + 1][y] == 1) {
                     grid[x + 1][y] = 2;
-                    rottenOranges.offer(new int[] {x + 1, y});
+                    --freshOranges;
+                    queue.offer(new int[] {x + 1, y});
                 }
                 
                 if (y - 1 >= 0 && grid[x][y - 1] == 1) {
-                    --freshOranges;
                     grid[x][y - 1] = 2;
-                    rottenOranges.offer(new int[] {x, y - 1});
+                    --freshOranges;
+                    queue.offer(new int[] {x, y - 1});
                 }
                 
                 if (y + 1 < grid[x].length && grid[x][y + 1] == 1) {
-                    --freshOranges;
                     grid[x][y + 1] = 2;
-                    rottenOranges.offer(new int[] {x, y + 1});
+                    --freshOranges;
+                    queue.offer(new int[] {x, y + 1});
                 }
             }
-            
-            if (before == freshOranges) {
-                return -1;
-            }
-            
-            ++result;
+
+            ++minutes;
         }
-        
-        if (freshOranges > 0 && rottenOranges.isEmpty()) {
+
+        if (freshOranges != 0) {
             return -1;
         }
-        
-        return result;
+        return minutes;
     }
 }
