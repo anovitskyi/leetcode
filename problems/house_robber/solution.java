2f3e0c532ac6f2eebd156e38ae3d4f83019b5f84
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2023-04-29 07:03:34 +0000

    Sync LeetCode submission - House Robber (java)

--- problems/house_robber/solution.java
+++ problems/house_robber/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+        for (int i = 0; i < nums.length; ++i) {
+            int prev = i - 1 >= 0 ? nums[i - 1] : 0;
+            int prevprev = i - 2 >= 0 ? nums[i - 2] : 0;
+
+            nums[i] = Math.max(prev, nums[i] + prevprev);
         }
 
-        nums[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < nums.length; ++i) {
-            nums[i] = Math.max(nums[i] + nums[i - 2], nums[i - 1]);
-        }
         return nums[nums.length - 1];
     }
 }
