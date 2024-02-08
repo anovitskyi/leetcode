845b512b3aed35563307633bef693af8ca6f7f8e
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2024-02-08 08:22:33 +0000

    Sync LeetCode submission - Combination Sum III (java)

--- problems/combination_sum_iii/solution.java
+++ problems/combination_sum_iii/solution.java
@@ -1,29 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), k, n, 1);
+        backtrack(result, new ArrayList<>(), k, 0, n, 1);
         return result;
     }
-    
-    private void backtrack(List<List<Integer>> result, List<Integer> curr, int size, int target, int startingIndex) {
-        if (target == 0) {
-            if (curr.size() == size) {
-                result.add(curr);
+
+    private void backtrack(List<List<Integer>> result, List<Integer> curr, int targetSize, int sum, int targetSum, int startNumber) {
+        if (curr.size() == targetSize) {
+            if (sum == targetSum) {
+                result.add(new ArrayList<>(curr));
             }
-            
             return;
         }
-        
-        for (int index = startingIndex; index < 10; ++index) {
-            int newSum = target - index;
-            
-            if (newSum < 0) {
-                continue;
-            }
-            
-            List<Integer> currCopy = new ArrayList<>(curr);
-            currCopy.add(index);
-            backtrack(result, currCopy, size, newSum, index + 1);
+
+        for (int number = startNumber; number <= 9; ++number) {
+            curr.add(number);
+            backtrack(result, curr, targetSize, sum + number, targetSum, number + 1);
+            curr.remove(curr.size() - 1);
         }
     }
 }
