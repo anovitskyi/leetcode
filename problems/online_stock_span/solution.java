6110e481292903077c7aff1c26e2e1db48e77d6c
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2024-02-10 10:54:39 +0000

    Sync LeetCode submission - Online Stock Span (java)

--- problems/online_stock_span/solution.java
+++ problems/online_stock_span/solution.java
@@ -13,3 +13,9 @@ class StockSpanner {
         return counter;
     }
 }
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
