ccc5f4a48a03dc76184ae498e4f59fa5a4b5facf
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2024-02-14 07:48:46 +0000

    Sync LeetCode submission - Search Suggestions System (java)

--- problems/search_suggestions_system/solution.java
+++ problems/search_suggestions_system/solution.java
@@ -1,24 +1,35 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Arrays.sort(products);
-        
-        List<List<String>> result = new LinkedList<>();
-        for (int i = 1; i <= searchWord.length(); ++i) {
-            result.add(findSuggestions(searchWord.substring(0, i), products));
-        }
-        return result;
-    }
-    
-    private List<String> findSuggestions(String word, String[] products) {
-        List<String> result = new LinkedList<>();
+        Node root = new Node();
+
         for (String product : products) {
-            if (product.startsWith(word)) {
-                result.add(product);
-                if (result.size() == 3) {
-                    break;
-                }
+            Node node = root;
+            
+            for (int i = 0; i < product.length(); ++i) {
+                char ch = product.charAt(i);
+                node = node.children.computeIfAbsent(ch, x -> new Node());
+                node.words.offer(product);
             }
         }
+
+        List<List<String>> result = new ArrayList<>();
+    
+        for (int i = 0; i < searchWord.length(); ++i) {
+            char ch = searchWord.charAt(i);
+            root = root.children.computeIfAbsent(ch, x -> new Node());
+
+            List<String> level = new ArrayList<>(3);
+            for (int j = 0; j < 3 && !root.words.isEmpty(); ++j) {
+                level.add(root.words.poll());
+            }
+            result.add(level);
+        }
+
         return result;
+    }
+
+    private static class Node {
+        final Queue<String> words = new PriorityQueue<>();
+        final Map<Character, Node> children = new HashMap<>();
     }
 }
