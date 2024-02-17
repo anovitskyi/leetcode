281b131789eea968931c58b13d8ce752ea7ebd10
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2024-02-17 08:17:24 +0000

    Sync LeetCode submission - N-th Tribonacci Number (java)

--- problems/n-th_tribonacci_number/solution.java
+++ problems/n-th_tribonacci_number/solution.java
@@ -1,28 +1,27 @@
 class Solution {
     public int tribonacci(int n) {
+        int prevprev = 0;
         if (n == 0) {
-            return 0;
-        }
-
-        if (n == 1) {
-            return 1;
-        }
-
-        if (n == 2) {
-            return 1;
+            return prevprev;
         }
 
         int prev = 1;
-        int prevprev = 1;
-        int prevprevprev = 0;
-
-        for (int i = 3; i <= n; ++i) {
-            int sum = prev + prevprev + prevprevprev;
-            prevprevprev = prevprev;
-            prevprev = prev;
-            prev = sum;
+        if (n == 1) {
+            return prev;
         }
 
-        return prev;
+        int curr = 1;
+        if (n == 2) {
+            return curr;
+        }
+
+        for (int i = 3; i <= n; ++i) {
+            int sum = prevprev + prev + curr;
+            prevprev = prev;
+            prev = curr;
+            curr = sum;
+        }
+
+        return curr;
     }
 }
