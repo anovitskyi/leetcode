da86929f9502281d89b51ce9ded7980004c73dcb
Andrii Novitskyi <Andrii.Novitskyi@dataart.com>
2024-02-21 08:15:24 +0000

    Sync LeetCode submission - Decode String (java)

--- problems/decode_string/solution.java
+++ problems/decode_string/solution.java
@@ -1,23 +1,59 @@
 class Solution {
-    public String decodeString(String s) {
-        
-        int lastIndex = s.lastIndexOf('[');
-        if(lastIndex<0) return s;
-        int temp=lastIndex-1;
-        String repeatString="";
-        while(temp>=0){
-            if(Character.isDigit(s.charAt(temp))){
-                temp--;
-            }
-            else break;
-        }
-        
-        int repeatNum=Integer.parseInt(s.substring(temp+1,lastIndex));
-        
-        String rpStr=s.substring(lastIndex+1, s.indexOf(']',lastIndex));
-        rpStr=rpStr.repeat(repeatNum);
-        s=s.replace((s.substring(temp+1,s.indexOf(']',temp+1)+1)),rpStr);
-        return decodeString(s);      
-        
+    
+    public String decodeString(String str) {
+        return decodeString(str, 0).word;
     }
+
+    private DecodeResult decodeString(String str, int index) {
+        StringBuilder builder = new StringBuilder();
+
+        while (index < str.length() && str.charAt(index) != ']') {
+            if (!Character.isDigit(str.charAt(index))) {
+                builder.append(str.charAt(index));
+                ++index;
+                continue;
+            } 
+
+            DecodeResult result = decode(str, index);
+
+            for (int i = 0; i < result.counter; ++i) {
+                builder.append(result.word);
+            }
+
+            index = result.index + 1;
+        }
+
+        return new DecodeResult(index, 1, builder.toString());
+    }
+
+    private DecodeResult decode(String str, int index) {
+        if (index >= str.length()) {
+            return new DecodeResult(index, 0, "");
+        }
+
+        int counter = 0;
+        while (Character.isDigit(str.charAt(index))) {
+            counter *= 10;
+            counter += (int) (str.charAt(index) - '0');
+            ++index;
+        }
+
+        DecodeResult internalResult = decodeString(str, index + 1);
+        return new DecodeResult(internalResult.index, counter, internalResult.word);
+    }
+
+    private static class DecodeResult {
+
+        final int index;
+        final int counter;
+        final String word;
+
+        DecodeResult(int index, int counter, String word) {
+            this.index = index;
+            this.counter = counter;
+            this.word = word;
+        }
+
+    }
+
 }
